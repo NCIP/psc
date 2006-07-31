40b22b67caed81347ac4660167e10a12dc3c6fbd
Padmaja Vedula <padmaja.vedula@semanticbits.com>
2006-07-31 21:05:10 +0000

    tabs changed to spaces

--- src/edu/northwestern/bioinformatics/studycalendar/web/LoginController.java
+++ src/edu/northwestern/bioinformatics/studycalendar/web/LoginController.java
@@ -18,61 +18,58 @@ import org.apache.log4j.Logger;
  */
 public class LoginController extends SimpleFormController {
 	
-	static final Logger log = Logger.getLogger(LoginController.class.getName());
-	static AuthenticationManager authMgr = null;
-	public static final String CSM_STUDYCAL_CONTEXT_NAME = "study_calendar";
-	
+    static final Logger log = Logger.getLogger(LoginController.class.getName());
+    static AuthenticationManager authMgr = null;
+    public static final String CSM_STUDYCAL_CONTEXT_NAME = "study_calendar";
+
     public LoginController() {
     }
-    
+
     protected ModelAndView onSubmit(Object loginData) throws Exception{
-		
-    	LoginCredentials loginCredentials = (LoginCredentials) loginData;
-    	
-    	log.debug("Login ID: " + loginCredentials.getUserId());
-    	log.debug("System Config file is: "
-				+ System.getProperty("gov.nih.nci.security.configFile"));
+	
+        LoginCredentials loginCredentials = (LoginCredentials) loginData;
+        log.debug("Login ID: " + loginCredentials.getUserId());
+        log.debug("System Config file is: "
+                + System.getProperty("gov.nih.nci.security.configFile"));
 
-		//check login credentials using Authentication Manager of CSM
-		boolean loginSuccess = false;
-		try {
-			loginSuccess = getAuthenticationManager().login(
-					loginCredentials.getUserId(), loginCredentials.getPassword());
-		} catch (CSException ex) {
-			loginSuccess = false;
-			log.debug("The user was denied access to the study calendar application.", ex);
-		}
-		if (loginSuccess) {
-			TestUserDetails testUserDetails = new TestUserDetails();
-			return new ModelAndView(getSuccessView(),"testuserdetails",testUserDetails);
-		} else {
-			// have to add an error page or redirect to login page with error msg
-			loginCredentials = new LoginCredentials();
-			return new ModelAndView(getFormView(), "loginCredentials", loginCredentials);
-		}
-	}
-    
+        //check login credentials using Authentication Manager of CSM
+        boolean loginSuccess = false;
+        try {
+            loginSuccess = getAuthenticationManager().login(
+                    loginCredentials.getUserId(), loginCredentials.getPassword());
+        } catch (CSException ex) {
+            loginSuccess = false;
+            log.debug("The user was denied access to the study calendar application." + ex);
+        }
+        if (loginSuccess) {
+            TestUserDetails testUserDetails = new TestUserDetails();
+            return new ModelAndView(getSuccessView(),"testuserdetails",testUserDetails);
+        } else {
+            // have to add an error page or redirect to login page with error msg
+            loginCredentials = new LoginCredentials();
+            return new ModelAndView(getFormView(), "loginCredentials", loginCredentials);
+        }
+    }
+
     /**
-	 * Returns the AuthenticationManager for the csm authentication. This method follows the
-	 * singleton pattern so that only one AuthenticationManager is created for
-	 * the app.
-	 * 
-	 * @return
-	 * @throws CSException
-	 */
-	protected AuthenticationManager getAuthenticationManager()
-			throws CSException {
-		if (authMgr == null) {
-			synchronized (LoginController.class) {
-				if (authMgr == null) {
-					authMgr = SecurityServiceProvider
-							.getAuthenticationManager(CSM_STUDYCAL_CONTEXT_NAME);
-				}
-			}
-		}
-
-		return authMgr;
-
-	}
+     * Returns the AuthenticationManager for the csm authentication. This method follows the
+     * singleton pattern so that only one AuthenticationManager is created for
+     * the app.
+     * 
+     * @return
+     * @throws CSException
+     */
+    protected AuthenticationManager getAuthenticationManager()
+        throws CSException {
+        if (authMgr == null) {
+            synchronized (LoginController.class) {
+                if (authMgr == null) {
+                    authMgr = SecurityServiceProvider
+                    .getAuthenticationManager(CSM_STUDYCAL_CONTEXT_NAME);
+                }
+            }
+        }
+        return authMgr;
+    }
 }
 
