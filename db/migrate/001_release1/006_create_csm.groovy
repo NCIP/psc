4b26939271eaaf6aed38ac5104517be8f6c98a21
Padmaja Vedula <padmaja.vedula@semanticbits.com>
2006-07-31 22:01:58 +0000

    changed file path for CSMPostGresSQL

--- db/migrate/001_release1/006_create_csm.groovy
+++ db/migrate/001_release1/006_create_csm.groovy
@@ -1,16 +1,15 @@
 class CreateCsm extends edu.northwestern.bioinformatics.bering.Migration {
-     void up() {
+    void up() {
         if (databaseMatches('oracle')) {
         } else if (databaseMatches('postgresql')) {
-		String fileName = "CSMPostgreSQL.sql" 
-		File input = new File(fileName) 
-		String content = input.text
-		execute(content)
+	    String fileName = "db/migrate/001_release1/CSMPostgreSQL.sql" 
+	    File input = new File(fileName) 
+	    String content = input.text
+	    execute(content)
         } else {
         }
-
     }
-
+    
     void down() {
     }
 }
