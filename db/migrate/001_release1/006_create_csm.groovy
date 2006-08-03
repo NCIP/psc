c6d83042ca25e2ad90803edfb16e24284801bcc2
Rhett Sutphin <r-sutphin@northwestern.edu>
2006-08-03 19:16:33 +0000

    Make create_csm reversible

--- db/migrate/001_release1/006_create_csm.groovy
+++ db/migrate/001_release1/006_create_csm.groovy
@@ -1,16 +1,26 @@
 class CreateCsm extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
         if (databaseMatches('oracle')) {
+            // TODO: Padmaja has oracle script
         } else if (databaseMatches('postgresql')) {
-	    String fileName = "db/migrate/001_release1/CSMPostgreSQL.sql" 
-	    File input = new File(fileName) 
-	    String content = input.text
-	    execute(content)
+            String fileName = "db/migrate/001_release1/CSMPostgreSQL.sql"
+            File input = new File(fileName)
+            String content = input.text
+            execute(content)
         } else {
+            // TODO: need something for HSQLDB.  PostgreSQL script will probably work.
         }
     }
     
     void down() {
+        if (databaseMatches('oracle')) {
+            // TODO: Padmaja has oracle script
+        } else if (databaseMatches('postgresql')) {
+            File input = new File("db/migrate/001_release1/CSMPostgreSQL-drop.sql")
+            execute(input.text)
+        } else {
+            // TODO: need something for HSQLDB.  PostgreSQL script will probably work.
+        }
     }
 }
 
---
+++ db/migrate/001_release1/CSMPostgreSQL-drop.sql
@@ -0,0 +1,13 @@
+DROP TABLE csm_user_group;
+DROP TABLE csm_pg_pe;
+DROP TABLE csm_user_pe;
+DROP TABLE csm_user_group_role_pg;
+DROP TABLE csm_protection_element;
+DROP TABLE csm_protection_group;
+DROP TABLE csm_role_privilege;
+DROP TABLE csm_user;
+DROP TABLE csm_group;
+DROP TABLE csm_role;
+DROP TABLE csm_privilege;
+DROP TABLE csm_application;
+
