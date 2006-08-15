f08bbf58b4295144c2c1c5e96230655d3a651a12
Jaron Sampson <jsampson@isovera.com>
2006-08-15 15:07:32 +0000

    Removing bad release2 folder

--- db/migrate/002_release2/001_modify_study.groovy
+++
@@ -1,9 +0,0 @@
-class ModifyStudy extends edu.northwestern.bioinformatics.bering.Migration {
-    void up() {
-    	execute('ALTER TABLE studies ADD completed Boolean')
-    }
-
-    void down() {
- 	  	execute('ALTER TABLE studies DROP COLUMN completed')
-     }
-}
