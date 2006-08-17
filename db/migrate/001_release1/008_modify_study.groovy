48f94f8d8cf676ccb630beb65f12008a7c007b1b
Rhett Sutphin <r-sutphin@northwestern.edu>
2006-08-17 21:16:34 +0000

    Use boolean column in the general case, instead of specifically for HSQLDB and PostgreSQL

--- db/migrate/001_release1/008_modify_study.groovy
+++ db/migrate/001_release1/008_modify_study.groovy
@@ -1,16 +1,13 @@
 class ModifyStudy extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
- 		if (databaseMatches('oracle')) {
-     		execute('ALTER TABLE studies ADD completed integer NOT NULL')
-     	} else if (databaseMatches('postgresql')) {
-     		execute('ALTER TABLE studies ADD completed Boolean NOT NULL')     	
-     	} else if (databaseMatches('hsqldb')) {
-     		execute('ALTER TABLE studies ADD completed BOOLEAN NOT NULL')     	
-     	}
-     		
+        if (databaseMatches('oracle')) {
+            execute('ALTER TABLE studies ADD completed integer NOT NULL')
+        } else {
+            execute('ALTER TABLE studies ADD completed BOOLEAN NOT NULL')
+        }
     }
 
     void down() {
- 	  	execute('ALTER TABLE studies DROP COLUMN completed')
-     }
+        execute('ALTER TABLE studies DROP COLUMN completed')
+    }
 }
