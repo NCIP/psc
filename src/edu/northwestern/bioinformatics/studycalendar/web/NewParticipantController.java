33c3a2b8869b44ec8b6fd6990f6f218fab477fdf
Padmaja Vedula <padmaja.vedula@semanticbits.com>
2006-08-22 21:34:37 +0000

    added customdateeditor for date field, the reason for the record not being saved

--- src/edu/northwestern/bioinformatics/studycalendar/web/NewParticipantController.java
+++ src/edu/northwestern/bioinformatics/studycalendar/web/NewParticipantController.java
@@ -1,11 +1,16 @@
 package edu.northwestern.bioinformatics.studycalendar.web;
 
+import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.validation.BindException;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 
 import javax.servlet.http.HttpServletRequest;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -17,15 +22,29 @@ import edu.northwestern.bioinformatics.studycalendar.dao.ParticipantDao;
  */
 public class NewParticipantController extends SimpleFormController {
     private ParticipantDao participantDao;
+    private String pattern = "MM/dd/yyyy";
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
 
     public NewParticipantController() {
         setCommandClass(NewParticipantCommand.class);
         setFormView("createParticipant");
-        setSuccessView("createParticipant");
+        setSuccessView("login");
     }
 
+    protected void initBinder(HttpServletRequest request,
+    		ServletRequestDataBinder binder) throws Exception {
+    		binder.registerCustomEditor(Date.class, null, new CustomDateEditor(
+    		simpleDateFormat, false));
+    		super.initBinder(request, binder);
+    }
+    
     protected Map<String, Object> referenceData(HttpServletRequest httpServletRequest) throws Exception {
         Map<String, Object> refdata = new HashMap<String, Object>();
+        //can be probably loaded from a properties file ??
+        Map<String, String> genders = new HashMap<String, String>();
+        genders.put("Female", "Female");
+        genders.put("male", "male");
+        refdata.put("genders", genders);
         refdata.put("action", "New");
         return refdata;
     }
@@ -33,6 +52,7 @@ public class NewParticipantController extends SimpleFormController {
     protected ModelAndView onSubmit(Object oCommand, BindException errors) throws Exception {
     	NewParticipantCommand participantCommand = (NewParticipantCommand) oCommand;
     	Participant participant = participantCommand.createParticipant();
+    	System.out.println("new participant name : " + participant.getFirstName());
         participantDao.save(participant);
 
         Map<String, Object> model = errors.getModel();
