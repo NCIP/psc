c0b2e4011632f482b5228e7853508ac5f54388bf
Padmaja Vedula <padmaja.vedula@semanticbits.com>
2006-08-23 13:11:48 +0000

    dao for study site

---
+++ src/edu/northwestern/bioinformatics/studycalendar/dao/StudySiteDao.java
@@ -0,0 +1,26 @@
+package edu.northwestern.bioinformatics.studycalendar.dao;
+
+import edu.northwestern.bioinformatics.studycalendar.domain.StudySite;
+
+import java.util.List;
+
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+
+/**
+ * @author Padmaja Vedula
+ */
+public class StudySiteDao extends HibernateDaoSupport {
+    public StudySite getById(int id) {
+        return (StudySite) getHibernateTemplate().get(StudySite.class, new Integer(id));
+    }
+
+    public void save(StudySite studySite) {
+        getHibernateTemplate().saveOrUpdate(studySite);
+    }
+
+    public List<StudySite> getAll() {
+        return getHibernateTemplate().find("from StudySite");
+    }
+}
