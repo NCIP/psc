2dc58091e0b77f56d52411e717e2a5010b70df37
Rhett Sutphin <r-sutphin@northwestern.edu>
2006-08-29 16:29:10 +0000

    Add special cases for HSQLDB

--- db/migrate/002_release2/007_modify_participants.groovy
+++ db/migrate/002_release2/007_modify_participants.groovy
@@ -1,15 +1,17 @@
 class ModifyParticipants extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
-	if (databaseMatches('oracle')) {
-          execute('ALTER TABLE participants RENAME COLUMN social_security_number TO person_id ')
-      } else if (databaseMatches('postgresql') {
-          execute('ALTER TABLE participants RENAME COLUMN social_security_number TO person_id ')
-    	} else {
-	    execute('ALTER TABLE participants ALTER COLUMN social_security_number RENAME TO person_id')
-	}
+        if (databaseMatches('hsqldb')) {
+            execute('ALTER TABLE participants ALTER COLUMN social_security_number RENAME TO person_id')
+        } else {
+            execute('ALTER TABLE participants RENAME COLUMN social_security_number TO person_id')
+        }
     }
 
     void down() {
-        execute('ALTER TABLE participants DROP COLUMN person_id')
+        if (databaseMatches('hsqldb')) {
+            execute('ALTER TABLE participants ALTER COLUMN person_id RENAME TO social_security_number')
+        } else {
+            execute('ALTER TABLE participants RENAME COLUMN person_id TO social_security_number')
+        }
     }
 }
