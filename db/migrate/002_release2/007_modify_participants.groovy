cee5b710e97490406b65a0460d8981c8a3e33b4b
Rhett Sutphin <r-sutphin@northwestern.edu>
2006-08-29 16:14:50 +0000

    Down was not actually the reverse of up

--- db/migrate/002_release2/007_modify_participants.groovy
+++ db/migrate/002_release2/007_modify_participants.groovy
@@ -1,9 +1,9 @@
 class ModifyStudy extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
-          execute('ALTER TABLE participants RENAME COLUMN social_security_number TO person_id ')
+        execute('ALTER TABLE participants RENAME COLUMN social_security_number TO person_id')
     }
 
     void down() {
-        execute('ALTER TABLE participants DROP COLUMN person_id')
+        execute('ALTER TABLE participants RENAME COLUMN person_id TO social_security_number')
     }
 }
