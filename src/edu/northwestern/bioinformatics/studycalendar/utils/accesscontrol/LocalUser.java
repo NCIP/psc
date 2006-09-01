a7f42f48bcbd8a856e882a385d782e7f5195400a
Padmaja Vedula <padmaja.vedula@semanticbits.com>
2006-09-01 14:42:15 +0000

    changed package for accesscontrol

---
+++ src/edu/northwestern/bioinformatics/studycalendar/utils/accesscontrol/LocalUser.java
@@ -0,0 +1,19 @@
+package edu.northwestern.bioinformatics.studycalendar.utils.accesscontrol;
+
+public class LocalUser { 
+	  private static final ThreadLocal<String> threadLocal = new ThreadLocal<String>(); 
+
+	  public static String getInstance() { 
+	    String userName = (String) threadLocal.get(); 
+	    return userName;
+	  } 
+
+	  public static void init(String userName) { 
+	    threadLocal.set(userName); 
+	  } 
+
+	  public static void release() { 
+	    threadLocal.set(null); 
+	  } 
+
+} 
