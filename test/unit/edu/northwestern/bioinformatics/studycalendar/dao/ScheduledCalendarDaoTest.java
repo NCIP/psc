5f49204ecd61a5d01ab64c02485b96a107a9b226
Rhett Sutphin <r-sutphin@northwestern.edu>
2006-09-27 23:30:54 +0000

    Custom naming strategy so that we don't have to explicitly name most tables and FK names

--- test/unit/edu/northwestern/bioinformatics/studycalendar/dao/ScheduledCalendarDaoTest.java
+++ test/unit/edu/northwestern/bioinformatics/studycalendar/dao/ScheduledCalendarDaoTest.java
@@ -1,10 +1,14 @@
 package edu.northwestern.bioinformatics.studycalendar.dao;
 
-import edu.northwestern.bioinformatics.studycalendar.testing.DaoTestCase;
-import edu.northwestern.bioinformatics.studycalendar.testing.StudyCalendarTestCase;
+import edu.nwu.bioinformatics.commons.DateUtils;
+
+import edu.northwestern.bioinformatics.studycalendar.domain.Participant;
 import edu.northwestern.bioinformatics.studycalendar.domain.ScheduledCalendar;
 import edu.northwestern.bioinformatics.studycalendar.domain.ScheduledEvent;
 import edu.northwestern.bioinformatics.studycalendar.domain.ScheduledEventState;
+import edu.northwestern.bioinformatics.studycalendar.domain.Arm;
+import edu.northwestern.bioinformatics.studycalendar.testing.DaoTestCase;
+import edu.northwestern.bioinformatics.studycalendar.testing.StudyCalendarTestCase;
 
 import java.util.Calendar;
 
@@ -14,6 +18,11 @@ import java.util.Calendar;
 public class ScheduledCalendarDaoTest extends DaoTestCase {
     private ScheduledCalendarDao dao
         = (ScheduledCalendarDao) getApplicationContext().getBean("scheduledCalendarDao");
+    private ParticipantDao participantDao
+        = (ParticipantDao) getApplicationContext().getBean("participantDao");
+    private ArmDao armDao
+        = (ArmDao) getApplicationContext().getBean("armDao");
+    private StudySiteDao plannedEventDao;
 
     public void testGetById() throws Exception {
         ScheduledCalendar cal = dao.getById(-20);
@@ -32,5 +41,25 @@ public class ScheduledCalendarDaoTest extends DaoTestCase {
         StudyCalendarTestCase.assertDayOfDate("Wrong actual date", 2006, Calendar.OCTOBER, 25, event.getActualDate());
         assertEquals("Wrong notes", "Boo!", event.getNotes());
         assertEquals("Wrong event state", ScheduledEventState.OCCURRED, event.getState());
+    }
+
+    public void testSave() throws Exception {
+        Participant participant = participantDao.getById(-1);
+
+        ScheduledCalendar calendar = new ScheduledCalendar();
+        calendar.setAssignment(participant.getAssignments().get(0));
+        Arm arm4 = armDao.getById(-4);
+        Arm arm3 = armDao.getById(-3);
+        calendar.addArm(arm4);
+        calendar.addArm(arm3);
+        calendar.addArm(arm4);
+
+        ScheduledEvent event = new ScheduledEvent();
+        event.setIdealDate(DateUtils.createDate(2006, Calendar.SEPTEMBER, 20));
+        event.setActualDate(DateUtils.createDate(2006, Calendar.SEPTEMBER, 22));
+        event.setPlannedEvent(arm3.getPeriods().iterator().next().getPlannedEvents().get(0));
+        event.setState(ScheduledEventState.OCCURRED);
+
+//        calendar.addEvent(event);
     }
 }
