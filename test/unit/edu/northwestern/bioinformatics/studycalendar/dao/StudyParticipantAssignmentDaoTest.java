3c07a4217b86f6c8a709ebbf971818f93549efa3
Rhett Sutphin <r-sutphin@northwestern.edu>
2006-10-02 19:51:20 +0000

    Tests for StudyParticipantAssignmentDao

---
+++ test/unit/edu/northwestern/bioinformatics/studycalendar/dao/StudyParticipantAssignmentDaoTest.java
@@ -0,0 +1,22 @@
+package edu.northwestern.bioinformatics.studycalendar.dao;
+
+import edu.nwu.bioinformatics.commons.testing.CoreTestCase;
+
+import edu.northwestern.bioinformatics.studycalendar.domain.StudyParticipantAssignment;
+
+import java.util.Calendar;
+
+/**
+ * @author Rhett Sutphin
+ */
+public class StudyParticipantAssignmentDaoTest extends ContextDaoTestCase<StudyParticipantAssignmentDao> {
+    public void testGetById() throws Exception {
+        StudyParticipantAssignment assignment = getDao().getById(-10);
+
+        assertEquals("Wrong id", -10, (int) assignment.getId());
+        CoreTestCase.assertDayOfDate("Wrong start date", 2003, Calendar.FEBRUARY, 1,
+            assignment.getStartDateEpoch());
+        assertEquals("Wrong participant", -20, (int) assignment.getParticipant().getId());
+        assertEquals("Wrong study site", -15, (int) assignment.getStudySite().getId());
+    }
+}
