f494a29b92af11b351798d7faedcc04a5993e7e3
Padmaja Vedula <padmaja.vedula@semanticbits.com>
2006-10-06 16:37:15 +0000

    added a DI for studycalendarauthorizationmanager

--- src/edu/northwestern/bioinformatics/studycalendar/web/ParticipantCoordinatorController.java
+++ src/edu/northwestern/bioinformatics/studycalendar/web/ParticipantCoordinatorController.java
@@ -1,13 +1,16 @@
 package edu.northwestern.bioinformatics.studycalendar.web;
 
-import edu.northwestern.bioinformatics.studycalendar.dao.StudyDao;
-import edu.northwestern.bioinformatics.studycalendar.domain.Study;
-import edu.northwestern.bioinformatics.studycalendar.utils.accesscontrol.StudyCalendarAuthorizationManager;
-import gov.nih.nci.security.authorization.domainobjects.User;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.beans.propertyeditors.CustomCollectionEditor;
-import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.bind.ServletRequestUtils;
@@ -15,14 +18,10 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 import org.springframework.web.servlet.view.RedirectView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import edu.northwestern.bioinformatics.studycalendar.dao.StudyDao;
+import edu.northwestern.bioinformatics.studycalendar.domain.Study;
+import edu.northwestern.bioinformatics.studycalendar.utils.accesscontrol.StudyCalendarAuthorizationManager;
+import gov.nih.nci.security.AuthenticationManager;
 
 /**
  * @author Padmaja Vedula
@@ -30,6 +29,7 @@ import java.util.Set;
 public class ParticipantCoordinatorController extends SimpleFormController {
 	private static final String GROUP_NAME = "PARTICIPANT_COORDINATOR";
 	private StudyDao studyDao;
+	private StudyCalendarAuthorizationManager authorizationManager;
 
     public ParticipantCoordinatorController() {
         setCommandClass(ParticipantCoordinatorCommand.class);
@@ -46,7 +46,7 @@ public class ParticipantCoordinatorController extends SimpleFormController {
     protected Map<String, Object> referenceData(HttpServletRequest httpServletRequest) throws Exception {
         Map<String, Object> refdata = new HashMap<String, Object>();
         Study study = studyDao.getById(ServletRequestUtils.getRequiredIntParameter(httpServletRequest, "id"));
-        HashMap<String, List> userLists = StudyCalendarAuthorizationManager.getUsers(GROUP_NAME, study.getClass().getName()+"."+study.getId());
+        HashMap<String, List> userLists = authorizationManager.getUsers(GROUP_NAME, study.getClass().getName()+"."+study.getId());
         refdata.put("study", study);
         refdata.put("assignedUsers", userLists.get(StudyCalendarAuthorizationManager.ASSIGNED_USERS));
         refdata.put("availableUsers", userLists.get(StudyCalendarAuthorizationManager.AVAILABLE_USERS));
@@ -57,7 +57,7 @@ public class ParticipantCoordinatorController extends SimpleFormController {
     protected ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response, Object oCommand, BindException errors) throws Exception {
     	ParticipantCoordinatorCommand assignCommand = (ParticipantCoordinatorCommand) oCommand;
     	Study assignedStudy = studyDao.getById(assignCommand.getStudyId());
-        StudyCalendarAuthorizationManager.assignProtectionElementsToUsers((ArrayList)assignCommand.getAssignedCoordinators(), assignedStudy.getClass().getName()+"."+assignedStudy.getId());
+        authorizationManager.assignProtectionElementsToUsers((ArrayList)assignCommand.getAssignedCoordinators(), assignedStudy.getClass().getName()+"."+assignedStudy.getId());
 
         return new ModelAndView(new RedirectView(getSuccessView()), "id", ServletRequestUtils.getIntParameter(request, "id"));
     }
@@ -72,6 +72,11 @@ public class ParticipantCoordinatorController extends SimpleFormController {
     @Required
     public void setStudyDao(StudyDao studyDao) {
         this.studyDao = studyDao;
+    }
+    
+    @Required
+    public void setStudyCalendarAuthorizationManager(StudyCalendarAuthorizationManager authorizationManager) {
+        this.authorizationManager = authorizationManager;
     }
 
 }
