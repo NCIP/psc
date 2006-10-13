3130f78d94571d1c7cdec28d9f9f0a8c6e6e621d
Rhett Sutphin <r-sutphin@northwestern.edu>
2006-10-13 20:24:50 +0000

    Create insertHtml tag, factoring commonalities out of replaceHtml along the way

--- src/edu/northwestern/bioinformatics/studycalendar/web/taglibs/jsgenerator/ReplaceHtml.java
+++ src/edu/northwestern/bioinformatics/studycalendar/web/taglibs/jsgenerator/ReplaceHtml.java
@@ -9,48 +9,8 @@ import java.io.IOException;
 /**
  * @author Rhett Sutphin
  */
-public class ReplaceHtml extends BodyTagSupport {
-    private String targetElement;
-
-    public void setTargetElement(String targetElement) {
-        this.targetElement = targetElement;
-    }
-
-    public int doStartTag() throws JspException {
-        return EVAL_BODY_BUFFERED;
-    }
-
-    public int doEndTag() throws JspException {
-        try {
-            pageContext.getOut().write(call("Element.update", targetElement, getBodyContent().getString()));
-        } catch (IOException e) {
-            throw new JspException("", e);
-        }
-        return EVAL_PAGE;
-    }
-
-    private String call(String method, Object... parameters) {
-        StringBuffer call = new StringBuffer(method).append('(');
-        for (Object parameter : parameters) {
-            call.append(createJavascriptLiteralRepresentation(parameter))
-                .append(", ");
-        }
-        call.setLength(call.length() - 2);
-        call.append(')');
-        return call.toString();
-    }
-
-    private String createJavascriptLiteralRepresentation(Object value) {
-        if (value == null) {
-            return "null";
-        } else if (value instanceof String) {
-            return jsString((String) value);
-        } else {
-            return value.toString();
-        }
-    }
-
-    private static String jsString(String html) {
-        return '"' + StringEscapeUtils.escapeJavaScript(html) + '"';
+public class ReplaceHtml extends ElementUpdatingGenerator {
+    protected void writeJavascript() throws JspException {
+        writeCall("Element.update", getTargetElement(), getBodyContent().getString());
     }
 }
--- test/unit/edu/northwestern/bioinformatics/studycalendar/web/taglibs/jsgenerator/ReplaceHtmlTest.java
+++ test/unit/edu/northwestern/bioinformatics/studycalendar/web/taglibs/jsgenerator/ReplaceHtmlTest.java
@@ -1,59 +1,34 @@
 package edu.northwestern.bioinformatics.studycalendar.web.taglibs.jsgenerator;
 
-import edu.northwestern.bioinformatics.studycalendar.testing.MockPageContext;
-import edu.northwestern.bioinformatics.studycalendar.testing.StudyCalendarTestCase;
-import static org.easymock.classextension.EasyMock.expect;
-
-import javax.servlet.jsp.JspWriter;
-import javax.servlet.jsp.tagext.BodyContent;
 import javax.servlet.jsp.tagext.BodyTag;
 import javax.servlet.jsp.tagext.Tag;
+import javax.servlet.jsp.JspException;
 
 
 /**
  * @author Rhett Sutphin
  */
-public class ReplaceHtmlTest extends StudyCalendarTestCase {
-    private static final String TARGET_ELEMENT = "target";
+public class ReplaceHtmlTest extends JsGeneratorTestCase<ReplaceHtml> {
 
-    private ReplaceHtml tag;
-    private JspWriter writer;
-    private MockPageContext pageContext;
-    private BodyContent bodyContent;
-
-    protected void setUp() throws Exception {
-        super.setUp();
-        tag = new ReplaceHtml();
-        writer = registerMockFor(JspWriter.class);
-        pageContext = new MockPageContext(writer);
-        bodyContent = registerMockFor(BodyContent.class);
-
-        tag.setPageContext(pageContext);
-        tag.setTargetElement(TARGET_ELEMENT);
-        tag.setBodyContent(bodyContent);
-    }
+    protected ReplaceHtml createTag() { return new ReplaceHtml(); }
 
     public void testStart() throws Exception {
         replayMocks();
-        assertEquals(BodyTag.EVAL_BODY_BUFFERED, tag.doStartTag());
+        assertEquals(BodyTag.EVAL_BODY_BUFFERED, getTag().doStartTag());
         verifyMocks();
     }
 
     public void testEnd() throws Exception {
-        expect(bodyContent.getString()).andReturn("content");
-        writer.write("Element.update(\"" + TARGET_ELEMENT + "\", \"content\")");
+        expectBody("content");
+        expectWrite("Element.update(\"" + TARGET_ELEMENT + "\", \"content\")");
 
-        replayMocks();
-        assertEquals(Tag.EVAL_PAGE, tag.doEndTag());
-        verifyMocks();
+        doEndTag();
     }
 
     public void testEndWithComplexContent() throws Exception {
-        expect(bodyContent.getString()).andReturn("<div id=\"foo\">\n  <strong>Bad</strong>\n</div>");
-        writer.write("Element.update(\"" + TARGET_ELEMENT + "\", \"<div id=\\\"foo\\\">\\n  <strong>Bad</strong>\\n</div>\")");
+        expectBody("<div id=\"foo\">\n  <strong>Bad</strong>\n</div>");
+        expectWrite("Element.update(\"" + TARGET_ELEMENT + "\", \"<div id=\\\"foo\\\">\\n  <strong>Bad</strong>\\n</div>\")");
 
-        replayMocks();
-        assertEquals(Tag.EVAL_PAGE, tag.doEndTag());
-        verifyMocks();
+        doEndTag();
     }
 }
