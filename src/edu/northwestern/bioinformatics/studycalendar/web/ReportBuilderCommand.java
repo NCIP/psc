878b0a2dbc40cbc833d5353d24462f8bddd7c4d2
Yufang Wang <ywang@akazaresearch.com>
2006-11-10 21:26:47 +0000

    Command object for report builder form

---
+++ src/edu/northwestern/bioinformatics/studycalendar/web/ReportBuilderCommand.java
@@ -0,0 +1,21 @@
+package edu.northwestern.bioinformatics.studycalendar.web;
+
+/*
+ * @author Yufang Wang
+ * @author Jaron Sampson 
+ */
+
+public class ReportBuilderCommand {
+	
+	private Integer studyId;
+
+	public Integer getStudyId() {
+		return studyId;
+	}
+
+	public void setStudyId(Integer siteId) {
+		this.studyId = siteId;
+	}
+	
+
+}
