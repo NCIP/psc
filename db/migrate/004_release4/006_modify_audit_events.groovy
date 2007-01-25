c5a83c355c2a90bd38dd6cf627a25b9b86864956
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-01-25 16:33:13 +0000

    Modify so that down() will run

--- db/migrate/004_release4/006_modify_audit_events.groovy
+++ db/migrate/004_release4/006_modify_audit_events.groovy
@@ -1,5 +1,8 @@
 class ModifyAuditEvents extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
+        // have to delete current contents b/c changing nullabilities
+        execute("DELETE FROM audit_events")
+
         renameColumn("audit_events", "user_name", "username");
         renameColumn("audit_events", "class_name", "object_class");
         renameColumn("audit_event_values", "new_value", "current_value");
@@ -15,12 +18,15 @@ class ModifyAuditEvents extends edu.northwestern.bioinformatics.bering.Migration
     }
 
     void down() {
+        // have to delete current contents b/c changing nullabilities
+        execute("DELETE FROM audit_events")
+
         renameColumn("audit_events", "username", "user_name");
         renameColumn("audit_events", "object_class", "class_name");
         renameColumn("audit_event_values", "current_value", "new_value");
 
         dropColumn("audit_events", "object_id")
         addColumn("audit_events", "object_id", "string")
-        setNullable("audit_events", "object_id", false)
+        // setNullable("audit_events", "object_id", false)
     }
 }
