f6aa4f6da9af17f0a6ca9e95d15bbaed25624256
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-03-01 19:41:54 +0000

    Parameterize the date

--- ctmsi/mark-all-past-occurred.sql
+++ ctmsi/mark-all-past-occurred.sql
@@ -1,13 +1,15 @@
 --
 -- This script marks any ScheduledEvent (for any participant) as "occurred" if its
--- date is before March 8, 2007.
+-- date is before "filter_date" (set below).
 --
--- Note that this script will only work on a PostgreSQL-backed PSC deployment.  It lacks the
--- explicit sequence refs that Oracle requires.
---
--- Note also that this script bypasses the PSC's application-level database update auditing.
+-- n.b., this script:
+--   ... will only work with psql
+--   ... bypasses the PSC's application-level database update auditing
 
 BEGIN TRANSACTION;
+
+-- The date before which to mark events occurred
+\set filter_date 'DATE \'2007-03-08\''
 
 -- Move the current state to the history
 INSERT INTO scheduled_event_states (scheduled_event_id, mode_id, actual_date, reason, list_index)
@@ -15,10 +17,10 @@ INSERT INTO scheduled_event_states (scheduled_event_id, mode_id, actual_date, re
     COALESCE(
       (SELECT MAX(list_index) + 1 FROM scheduled_event_states ses WHERE ses.scheduled_event_id=se.id), 0
     )
-  FROM scheduled_events se WHERE se.current_state_mode_id=1 AND se.current_state_date < DATE '2007-03-08';
+  FROM scheduled_events se WHERE se.current_state_mode_id=1 AND se.current_state_date < :filter_date;
 
 -- Update with new mode & reason
-UPDATE scheduled_events SET current_state_mode_id=2, current_state_reason='Batch changed for demo'
-  WHERE current_state_mode_id=1 AND current_state_date < DATE '2007-03-08';
+UPDATE scheduled_events SET current_state_mode_id=2, current_state_reason='Batch change: demo data'
+  WHERE current_state_mode_id=1 AND current_state_date < :filter_date;
 
 COMMIT;
