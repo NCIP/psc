ff2eb05ad3985d112ac45d5085d8562a509d5ac0
Nataliya Shurupova <n-shurupova@northwestern.edu>
2007-07-06 20:05:17 +0000

    changed the syntax for adding and removing the column, just so it runs on oracle db as well as on psql

--- db/migrate/006_Phase2_Elaboration/004_modify_holiday_table.groovy
+++ db/migrate/006_Phase2_Elaboration/004_modify_holiday_table.groovy
@@ -1,10 +1,9 @@
 class ModifyHolidayTable extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
-        execute('ALTER TABLE holidays ADD COLUMN number_of_week integer')
-
+        addColumn("holidays", "number_of_week", "integer")
     }
 
     void down() {
-        execute('ALTER TABLE holidays DROP COLUMN number_of_week')
+        dropColumn("holidays", "number_of_week");
     }
 }
