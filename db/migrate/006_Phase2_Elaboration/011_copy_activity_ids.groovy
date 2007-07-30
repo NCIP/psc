ce1021aab0be482d90fa687374532ca895950551
John Dzak <j-dzak@northwestern.edu>
2007-07-30 22:11:08 +0000

    Fix sql type for oracle

--- db/migrate/006_Phase2_Elaboration/011_copy_activity_ids.groovy
+++ db/migrate/006_Phase2_Elaboration/011_copy_activity_ids.groovy
@@ -5,8 +5,8 @@ class CopyActivtyIdsAndDetails extends edu.northwestern.bioinformatics.bering.Mi
             execute('UPDATE scheduled_events SET activity_id=p.activity_id FROM planned_events p INNER JOIN scheduled_events s ON s.planned_event_id=p.id where scheduled_events.id = s.id')
             execute('UPDATE scheduled_events SET details=p.details FROM planned_events p INNER JOIN scheduled_events s ON s.planned_event_id=p.id where scheduled_events.id = s.id')
         } else {
-            execute('UPDATE scheduled_events SET activity_id=(SELECT p.activity_id FROM planned_events p INNER JOIN scheduled_events s ON s.planned_event_id=p._id where scheduled_events.id=s.id)')
-            execute('UPDATE scheduled_events SET details=(SELECT p.details FROM planned_events p INNER JOIN scheduled_events s ON s.planned_event_id=p._id where scheduled_events.id=s.id)')
+            execute('UPDATE scheduled_events SET activity_id=(SELECT p.activity_id FROM planned_events p INNER JOIN scheduled_events s ON s.planned_event_id=p.id where scheduled_events.id=s.id)')
+            execute('UPDATE scheduled_events SET details=(SELECT p.details FROM planned_events p INNER JOIN scheduled_events s ON s.planned_event_id=p.id where scheduled_events.id=s.id)')
         }
     }
 
