a2e023a2cdff1bb3d10f1b6f254c1afe82be6b02
Nataliya Shurupova <n-shurupova@northwestern.edu>
2007-08-02 14:38:16 +0000

    added a new test.

--- test/unit/edu/northwestern/bioinformatics/studycalendar/web/template/ManagePeriodEventsControllerTest.java
+++ test/unit/edu/northwestern/bioinformatics/studycalendar/web/template/ManagePeriodEventsControllerTest.java
@@ -3,18 +3,24 @@ package edu.northwestern.bioinformatics.studycalendar.web.template;
 import edu.northwestern.bioinformatics.studycalendar.dao.ActivityDao;
 import edu.northwestern.bioinformatics.studycalendar.dao.PeriodDao;
 import edu.northwestern.bioinformatics.studycalendar.dao.PlannedEventDao;
-import edu.northwestern.bioinformatics.studycalendar.domain.Activity;
-import edu.northwestern.bioinformatics.studycalendar.domain.Epoch;
 import static edu.northwestern.bioinformatics.studycalendar.domain.Fixtures.*;
-import edu.northwestern.bioinformatics.studycalendar.domain.Period;
-import edu.northwestern.bioinformatics.studycalendar.domain.PlannedCalendar;
-import edu.northwestern.bioinformatics.studycalendar.domain.Study;
+import edu.northwestern.bioinformatics.studycalendar.domain.*;
 import edu.northwestern.bioinformatics.studycalendar.web.ControllerTestCase;
+import edu.northwestern.bioinformatics.studycalendar.web.ControllerTools;
+import edu.northwestern.bioinformatics.studycalendar.utils.DomainObjectTools;
 import static org.easymock.classextension.EasyMock.expect;
+import org.easymock.classextension.EasyMock;
+import org.easymock.IArgumentMatcher;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingErrorProcessor;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.ServletRequestUtils;
 
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 
 /**
  * @author Rhett Sutphin
@@ -46,6 +52,7 @@ public class ManagePeriodEventsControllerTest extends ControllerTestCase {
 
         request.setMethod("GET");
         request.addParameter("id", "15");
+
         expect(periodDao.getById(15)).andReturn(period).anyTimes();
         expect(activityDao.getAll()).andReturn(activities).anyTimes();
 
@@ -60,6 +67,37 @@ public class ManagePeriodEventsControllerTest extends ControllerTestCase {
         verifyMocks();
         assertTrue(command instanceof ManagePeriodEventsCommand);
         assertSame(period, ((ManagePeriodEventsCommand) command).getPeriod());
+    }
+
+    public void testFormProcessing() throws Exception {
+
+
+        request.addParameter("grid[0].addition","true");
+        request.addParameter("grid[0].columnNumber","3");
+        request.addParameter("grid[0].conditionalCheckbox",	"false");
+        request.addParameter("grid[0].conditionalDetails","");
+        request.addParameter("grid[0].conditionalUpdated",	"false" );
+        request.addParameter("grid[0].details",	"abc");
+        request.addParameter("grid[0].eventIds[0]",	"646" );
+        request.addParameter("grid[0].eventIds[1]",	"-1" );
+        request.addParameter("grid[0].eventIds[2]",	"645" );
+        request.addParameter("grid[0].eventIds[3]",	"-1" );
+        request.addParameter("grid[0].rowNumber",	"2" );
+        request.addParameter("grid[0].updated",	"true" );
+        plannedEventDao.save(eqPlannedEvent(new PlannedEvent()));
+
+        periodDao.evict(period);
+
+        replayMocks();
+
+        ManagePeriodEventsCommand command
+            = (ManagePeriodEventsCommand) controller.handleRequest(request, response).getModel().get("command");
+        ModelAndView model = controller.processFormSubmission(request, response, command, null);
+        verifyMocks();
+        assertEquals("Periods are not the same" , period, model.getModel().get("period"));
+        assertEquals("Parameter rowNumber is not the same", 2, model.getModel().get("rowNumber"));
+        assertEquals("Parameter columnNumber is not the same", 3, model.getModel().get("columnNumber"));
+        assertEquals("Study is not the same", period.getArm().getEpoch().getPlannedCalendar().getStudy(), model.getModel().get("study"));
     }
 
     public void testBindingGridDetails() throws Exception {
@@ -147,11 +185,37 @@ public class ManagePeriodEventsControllerTest extends ControllerTestCase {
         verifyMocks();
     }
 
+
     public void testReferenceDataEmptyIfNoNewActivity() throws Exception {
         request.removeParameter("selectedActivity");
         replayMocks();
 
         assertNotContains(controller.referenceData(request, command, null).keySet(), "selectedActivity");
         verifyMocks();
+    }
+
+    public static PlannedEvent eqPlannedEvent(PlannedEvent event) {
+        EasyMock.reportMatcher(new PlannedEventMatcher(event));
+        return null;
+    }
+
+    private static class PlannedEventMatcher implements IArgumentMatcher {
+        private PlannedEvent expectedPlannedEvent;
+
+        public PlannedEventMatcher(PlannedEvent expectedPlannedEvent) {
+            this.expectedPlannedEvent = expectedPlannedEvent;
+        }
+
+        public boolean matches(Object object) {
+            if(!(object instanceof PlannedEvent)) {
+                return false;
+            }
+            //we don't care if the events are not the same for this test
+            return true;
+        }
+
+        public void appendTo(StringBuffer sb) {
+            sb.append("PlannedEvent activity=").append(expectedPlannedEvent.getActivity());
+        }
     }
 }
