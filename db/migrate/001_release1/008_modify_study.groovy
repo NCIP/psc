8ef49fb22349331d893499c1240c64034aef8580
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-08-07 22:39:58 +0000

    Add default values in case there are already values in the table

--- db/migrate/001_release1/008_modify_study.groovy
+++ db/migrate/001_release1/008_modify_study.groovy
@@ -1,9 +1,9 @@
 class ModifyStudy extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
         if (databaseMatches('oracle')) {
-            execute('ALTER TABLE studies ADD completed integer NOT NULL')
+            execute('ALTER TABLE studies ADD completed integer NOT NULL DEFAULT \'0\'')
         } else {
-            execute('ALTER TABLE studies ADD completed BOOLEAN NOT NULL')
+            execute('ALTER TABLE studies ADD completed BOOLEAN NOT NULL DEFAULT false')
         }
     }
 
