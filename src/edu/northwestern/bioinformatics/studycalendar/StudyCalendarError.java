2802b29d94f7d0e29b926e943fe3badc382febd6
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-08-15 23:18:16 +0000

    Add message formatting to PSC exceptions

--- src/edu/northwestern/bioinformatics/studycalendar/StudyCalendarError.java
+++ src/edu/northwestern/bioinformatics/studycalendar/StudyCalendarError.java
@@ -1,5 +1,7 @@
 package edu.northwestern.bioinformatics.studycalendar;
 
+import static java.lang.String.*;
+
 /**
  * PSC-specific error for fatal problems.  In general these will be caused by unrecoverable
  * configuration errors or incorrect assumptions.
@@ -7,11 +9,11 @@ package edu.northwestern.bioinformatics.studycalendar;
  * @author Rhett Sutphin
  */
 public class StudyCalendarError extends Error {
-    public StudyCalendarError(String message) {
-        super(message);
+    public StudyCalendarError(String message, Object... messageParameters) {
+        super(format(message, messageParameters));
     }
 
-    public StudyCalendarError(String message, Throwable cause) {
-        super(message, cause);
+    public StudyCalendarError(String message, Throwable cause, Object... messageParameters) {
+        super(format(message, messageParameters), cause);
     }
 }
--- src/edu/northwestern/bioinformatics/studycalendar/StudyCalendarSystemException.java
+++ src/edu/northwestern/bioinformatics/studycalendar/StudyCalendarSystemException.java
@@ -1,5 +1,7 @@
 package edu.northwestern.bioinformatics.studycalendar;
 
+import static java.lang.String.format;
+
 /**
  * Exception for unexpected, unhandlable runtime conditions.  E.g, an InvocationTargetException
  * when it is not expected that the invoked method will throw an exception.
@@ -7,12 +9,12 @@ package edu.northwestern.bioinformatics.studycalendar;
  * @author Rhett Sutphin
  */
 public class StudyCalendarSystemException extends RuntimeException {
-    public StudyCalendarSystemException(String message) {
-        super(message);
+    public StudyCalendarSystemException(String message, Object... messageParameters) {
+        super(format(message, messageParameters));
     }
 
-    public StudyCalendarSystemException(String message, Throwable cause) {
-        super(message, cause);
+    public StudyCalendarSystemException(String message, Throwable cause, Object... messageParameters) {
+        super(format(message, messageParameters), cause);
     }
 
     public StudyCalendarSystemException(Throwable cause) {
