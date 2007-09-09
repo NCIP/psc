b5d7fe42653cdc83dad13ab03bb8ad1a0c0e5d18
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-09-09 01:21:49 +0000

    Remove unused/untested methods; deprecate & de-support getStudyById because that field's about to disappear

--- src/edu/northwestern/bioinformatics/studycalendar/dao/delta/AmendmentDao.java
+++ src/edu/northwestern/bioinformatics/studycalendar/dao/delta/AmendmentDao.java
@@ -1,23 +1,16 @@
 package edu.northwestern.bioinformatics.studycalendar.dao.delta;
 
-import org.springframework.transaction.annotation.Transactional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import edu.northwestern.bioinformatics.studycalendar.domain.delta.Amendment;
 import edu.northwestern.bioinformatics.studycalendar.dao.StudyCalendarDao;
+import edu.northwestern.bioinformatics.studycalendar.domain.delta.Amendment;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 /**
- * Created by IntelliJ IDEA.
- * User: nshurupova
- * Date: Aug 29, 2007
- * Time: 3:41:38 PM
- * To change this template use File | Settings | File Templates.
+ * @author Nataliya Shurupova
  */
 public class AmendmentDao  extends StudyCalendarDao<Amendment> {
-        private static final Logger log = LoggerFactory.getLogger(AmendmentDao.class.getName());
-
+    @Override
     public Class<Amendment> domainClass() {
         return Amendment.class;
     }
@@ -31,21 +24,8 @@ public class AmendmentDao  extends StudyCalendarDao<Amendment> {
         getHibernateTemplate().saveOrUpdate(amendment);
     }
 
-    public List<Amendment> getByName (String name) {
-        return getHibernateTemplate().find("from Amendment a where a.name= ?", name);
-    }
-
-    public List<Amendment> getByDate(String date) {
-        return getHibernateTemplate().find("from Amendment a where a.date= ?", date);
-    }
-
-    public Amendment getByPreviousAmendmentId(Integer previousAmendmentId) {
-        List<Amendment> results = getHibernateTemplate().find("from Amendment a where a.previousAmendment= ?", previousAmendmentId);
-        return results.get(0);
-    }
-
+    @Deprecated
     public Amendment getByStudyId(Integer studyId) {
-        List<Amendment> results = getHibernateTemplate().find("from Amendment a where a.studyId= ?", studyId);
-        return results.get(0);
+        throw new UnsupportedOperationException("Deprecated");
     }
 }
