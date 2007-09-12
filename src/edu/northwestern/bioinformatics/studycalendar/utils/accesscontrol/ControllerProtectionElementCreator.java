7055d0f8962bb10d37cb03d73db85357a36f0fc8
John Dzak <j-dzak@northwestern.edu>
2007-09-12 21:10:26 +0000

    Setup csm authorization to support other study calendar users.

---
+++ src/edu/northwestern/bioinformatics/studycalendar/utils/accesscontrol/ControllerProtectionElementCreator.java
@@ -0,0 +1,68 @@
+package edu.northwestern.bioinformatics.studycalendar.utils.accesscontrol;
+
+import gov.nih.nci.cabig.ctms.tools.spring.ControllerUrlResolver;
+import gov.nih.nci.cabig.ctms.tools.spring.ResolvedControllerReference;
+//import org.apache.commons.logging.Log;
+//import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.core.Ordered;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Rhett Sutphin
+ */
+public class ControllerProtectionElementCreator implements BeanFactoryPostProcessor, Ordered {
+//    private static Log log = LogFactory.getLog(ControllerProtectionElementCreator.class);
+    private static Logger log = LoggerFactory.getLogger(ControllerProtectionElementCreator.class);    
+
+    private ControllerUrlResolver urlResolver;
+    private StudyCalendarAuthorizationManager studyCalendarAuthorizationManager;
+
+    // Must occur after BeanNameControllerUrlResolver
+    public int getOrder() { return 3; }
+
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        String[] controllerNames = beanFactory.getBeanNamesForType(Controller.class, false, false);
+        for (String controllerName : controllerNames) {
+            ResolvedControllerReference controller = urlResolver.resolve(controllerName);
+            List<String> groupNames = getRequiredProtectionGroupNames(controller);
+            if (groupNames != null) {
+                studyCalendarAuthorizationManager.registerUrl(controller.getUrl(true), groupNames);
+            }
+        }
+    }
+
+    private List<String> getRequiredProtectionGroupNames(ResolvedControllerReference controller) {
+        Class<? extends Controller> clazz = controller.getControllerClass();
+        AccessControl ac = clazz.getAnnotation(AccessControl.class);
+        if (ac == null) {
+            log.warn("No AccessControl annotations on " + clazz.getName() + ".  CSM will not be automatically configured for this controller.");
+            return null;
+        }
+        List<String> pgNames = new ArrayList<String>(ac.protectionGroups().length);
+        for (StudyCalendarProtectionGroup protectionGroup : ac.protectionGroups()) {
+            pgNames.add(protectionGroup.csmName());
+        }
+        return pgNames;
+    }
+
+    ////// CONFIGURATION
+
+    @Required
+    public void setStudyCalendarAuthorizationManager(StudyCalendarAuthorizationManager studyCalendarAuthorizationManager) {
+        this.studyCalendarAuthorizationManager = studyCalendarAuthorizationManager;
+    }
+
+    @Required
+    public void setUrlResolver(ControllerUrlResolver urlResolver) {
+        this.urlResolver = urlResolver;
+    }
+}
