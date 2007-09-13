4bbb72e9993a5c3429dd33b401b16ae355263821
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-09-13 02:25:58 +0000

    Mask ctms-common's commons-logging logger with an slf4j logger

--- src/edu/northwestern/bioinformatics/studycalendar/dao/StudyCalendarDao.java
+++ src/edu/northwestern/bioinformatics/studycalendar/dao/StudyCalendarDao.java
@@ -2,9 +2,12 @@ package edu.northwestern.bioinformatics.studycalendar.dao;
 
 import gov.nih.nci.cabig.ctms.dao.AbstractDomainObjectDao;
 import gov.nih.nci.cabig.ctms.domain.DomainObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Rhett Sutphin
  */
 public abstract class StudyCalendarDao<T extends DomainObject> extends AbstractDomainObjectDao<T> {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
 }
