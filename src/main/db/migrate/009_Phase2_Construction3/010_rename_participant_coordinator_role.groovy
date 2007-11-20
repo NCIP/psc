ba2997a82c8deb3026a7b1abbb9321f1da8369fa
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-11-20 15:16:47 +0000

    Correct down()

--- src/main/db/migrate/009_Phase2_Construction3/010_rename_participant_coordinator_role.groovy
+++ src/main/db/migrate/009_Phase2_Construction3/010_rename_participant_coordinator_role.groovy
@@ -13,10 +13,9 @@ class RenameParticipantCoordinatorRole extends edu.northwestern.bioinformatics.b
         execute("UPDATE csm_role SET role_name='PARTICIPANT_COORDINATOR' WHERE role_name='SUBJECT_COORDINATOR'");
         execute("UPDATE csm_role SET role_description='participant coordinator' WHERE role_description='subject coordinator'");
 
-        execute("UPDATE user_roles SET csm_group_name='PARTICIPANT_COORDINATOR' WHERE role_name='SUBJECT_COORDINATOR'");
+        execute("UPDATE user_roles SET csm_group_name='PARTICIPANT_COORDINATOR' WHERE csm_group_name='SUBJECT_COORDINATOR'");
 
-        execute("UPDATE csm_group SET group_name='SUBJECT_COORDINATOR' WHERE group_name='PARTICIPANT_COORDINATOR'");
+        execute("UPDATE csm_group SET group_name='PARTICIPANT_COORDINATOR' WHERE group_name='SUBJECT_COORDINATOR'");
         execute("UPDATE csm_group SET group_desc='participant coordinator group' WHERE group_desc='subject coordinator group'");
-
     }
 }
