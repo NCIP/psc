835b9e8e66425ea17003dd7392a5d9d94b24db60
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-11-27 14:23:31 +0000

    Drop and recreate fk_delta_node_types constraint so that existing data can be preserved when updating the node_type table

--- src/main/db/migrate/009_Phase2_Construction3/012_rename_arms_table.groovy
+++ src/main/db/migrate/009_Phase2_Construction3/012_rename_arms_table.groovy
@@ -6,11 +6,16 @@ class RenameArmsTable extends edu.northwestern.bioinformatics.bering.Migration {
         renameColumn("periods", "arm_id", "study_segment_id")
         renameColumn("scheduled_activities", "scheduled_arm_id", "scheduled_study_segment_id")
 
+        // Changing the delta_node_types TLU will violate an FK no matter which order we do it,
+        // so defer checking until commit.
+        execute("ALTER TABLE deltas DROP CONSTRAINT fk_delta_node_type");
+
         execute("UPDATE delta_node_types SET node_type='segmnt' WHERE node_type='arm'");
         execute("UPDATE delta_node_types SET node_table='study_segments' WHERE node_table='arms'");
-
         execute("UPDATE deltas SET node_type='segmnt' WHERE node_type='arm'");
 
+        // Reenable checking
+        execute("ALTER TABLE deltas ADD CONSTRAINT fk_delta_node_type FOREIGN KEY (node_type) REFERENCES delta_node_types");
     }
 
     void down() {
@@ -20,9 +25,15 @@ class RenameArmsTable extends edu.northwestern.bioinformatics.bering.Migration {
         renameColumn("periods", "study_segment_id", "arm_id")
         renameColumn("scheduled_activities", "scheduled_study_segment_id", "scheduled_arm_id")
 
+        // Changing the delta_node_types TLU will violate an FK no matter which order we do it,
+        // so defer checking until commit.
+        execute("ALTER TABLE deltas DROP CONSTRAINT fk_delta_node_type");
+
         execute("UPDATE delta_node_types SET node_type='arm' WHERE node_type='segmnt'");
         execute("UPDATE delta_node_types SET node_table='arms' WHERE node_table='study_segments'");
-
         execute("UPDATE deltas SET node_type='arm' WHERE node_type='segmnt'");
+
+        // Reenable checking
+        execute("ALTER TABLE deltas ADD CONSTRAINT fk_delta_node_type FOREIGN KEY (node_type) REFERENCES delta_node_types");
     }
 }
