ce7d0c40a959c99d69df3dd921749ad67cfa5a86
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-11-29 19:04:52 +0000

    Clean up whitespace

--- src/main/java/edu/northwestern/bioinformatics/studycalendar/dao/StudyDao.java
+++ src/main/java/edu/northwestern/bioinformatics/studycalendar/dao/StudyDao.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 import edu.northwestern.bioinformatics.studycalendar.StudyCalendarSystemException;
 import edu.northwestern.bioinformatics.studycalendar.domain.Study;
 import edu.northwestern.bioinformatics.studycalendar.domain.StudySubjectAssignment;
+import edu.nwu.bioinformatics.commons.CollectionUtils;
 
 /**
  * @author Rhett Sutphin
@@ -14,74 +15,65 @@ import edu.northwestern.bioinformatics.studycalendar.domain.StudySubjectAssignme
 @Transactional(readOnly = true)
 public class StudyDao extends StudyCalendarMutableDomainObjectDao<Study> {
 
-	@Override
-	public Class<Study> domainClass() {
-		return Study.class;
-	}
+    @Override
+    public Class<Study> domainClass() {
+        return Study.class;
+    }
 
-	@SuppressWarnings( { "unchecked" })
-	public List<Study> getAll() {
-		return getHibernateTemplate().find("from Study");
-	}
+    @SuppressWarnings( { "unchecked" })
+    public List<Study> getAll() {
+        return getHibernateTemplate().find("from Study");
+    }
 
-	@SuppressWarnings( { "unchecked" })
-	public List<StudySubjectAssignment> getAssignmentsForStudy(final Integer studyId) {
-		return getHibernateTemplate()
-				.find(
-						"select a from StudySubjectAssignment a inner join a.studySite ss inner join a.subject p where ss.study.id = ? order by p.lastName, p.firstName",
-						studyId);
-	}
+    @SuppressWarnings( { "unchecked" })
+    public List<StudySubjectAssignment> getAssignmentsForStudy(final Integer studyId) {
+        return getHibernateTemplate()
+            .find("select a from StudySubjectAssignment a inner join a.studySite ss inner join a.subject p where ss.study.id = ? order by p.lastName, p.firstName",
+                studyId);
+    }
 
-	public Study getByAssignedIdentifier(final String assignedIdentifier) {
-		List<Study> results = getHibernateTemplate().find("from Study where assignedIdentifier= ?", assignedIdentifier);
-		if (!results.isEmpty()) {
+    public Study getByAssignedIdentifier(final String assignedIdentifier) {
+        List<Study> results = getHibernateTemplate().find("from Study where assignedIdentifier= ?", assignedIdentifier);
+        return CollectionUtils.firstElement(results);
+    }
 
-			Study study = results.get(0);
-			return study;
-		}
-		return null;
-	}
+    public void commitInProgressStudy(final String ccIdentifier) {
+        Object studyId = getSession().createSQLQuery(
+            "select id from studies where load_status=0 and assigned_identifier='" + ccIdentifier + "'")
+            .uniqueResult();
 
-	public void commitInProgressStudy(final String ccIdentifier) {
-		Object studyId = getSession().createSQLQuery(
-				"select id from studies where load_status=0 and assigned_identifier='" + ccIdentifier + "'")
-				.uniqueResult();
+        if (studyId == null) {
+            String msg = "Either no study exist with ccIdentifier :" + ccIdentifier
+                + " or study is already been commited";
+            logger.error(msg);
+            throw new StudyCalendarSystemException(msg);
+        }
+        logger.info("For cc identifier:" + ccIdentifier + " found study:id-" + studyId);
 
-		if (studyId == null) {
-			String msg = "Either no study exist with ccIdentifier :" + ccIdentifier
-					+ " or study is already been commited";
-			logger.error(msg);
-			throw new StudyCalendarSystemException(msg);
-		}
-		logger.info("For cc identifier:" + ccIdentifier + " found study:id-" + studyId);
+        // update load status of the study
+        getSession().createSQLQuery("update studies set load_status=1 where id=" + studyId.toString()).executeUpdate();
+        logger.info("commited study:id:" + studyId);
+    }
 
-		// update load status of the study
-		getSession().createSQLQuery("update studies set load_status=1 where id=" + studyId.toString()).executeUpdate();
-		logger.info("commited study:id:" + studyId);
+    public void deleteInprogressStudy(final String ccIdentifier) {
+        Object studyId = getSession().createSQLQuery(
+            "select id from studies where load_status=0 and assigned_identifier='" + ccIdentifier + "'")
+            .uniqueResult();
 
-	}
+        if (studyId == null) {
+            String message = "Either no study exist with ccIdentifier : " + ccIdentifier
+                + " or study is already commited";
+            logger.error(message);
+            throw new StudyCalendarSystemException(message);
+        }
+        logger.info("For cc identifier:" + ccIdentifier + " found study:id=" + studyId);
 
-	public void deleteInprogressStudy(final String ccIdentifier) {
-		Object studyId = getSession().createSQLQuery(
-				"select id from studies where load_status=0 and assigned_identifier='" + ccIdentifier + "'")
-				.uniqueResult();
+        // delete study
+        getSession().createSQLQuery("delete from planned_calendars where study_id=" + studyId.toString())
+            .executeUpdate();
+        getSession().createSQLQuery("delete from study_sites where study_id=" + studyId.toString()).executeUpdate();
 
-		if (studyId == null) {
-			String message = "Either no study exist with ccIdentifier : " + ccIdentifier
-					+ " or study is already commited";
-			logger.error(message);
-			throw new StudyCalendarSystemException(message);
-		}
-		logger.info("For cc identifier:" + ccIdentifier + " found study:id=" + studyId);
-
-		// delete study
-		getSession().createSQLQuery("delete from planned_calendars where study_id=" + studyId.toString())
-				.executeUpdate();
-		getSession().createSQLQuery("delete from study_sites where study_id=" + studyId.toString()).executeUpdate();
-
-		getSession().createSQLQuery("delete from studies where id=" + studyId.toString()).executeUpdate();
-		logger.info("deleted study:id:" + studyId);
-
-	}
-
+        getSession().createSQLQuery("delete from studies where id=" + studyId.toString()).executeUpdate();
+        logger.info("deleted study:id:" + studyId);
+    }
 }
