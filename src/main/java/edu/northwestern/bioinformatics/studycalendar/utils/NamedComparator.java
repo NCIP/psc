c8f6caf5ff11cb6d428353b3c2561699b22b9f29
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-11-30 17:15:29 +0000

    Add static singleton instance

--- src/main/java/edu/northwestern/bioinformatics/studycalendar/utils/NamedComparator.java
+++ src/main/java/edu/northwestern/bioinformatics/studycalendar/utils/NamedComparator.java
@@ -5,6 +5,8 @@ import edu.northwestern.bioinformatics.studycalendar.domain.Named;
 import java.util.Comparator;
 
 public class NamedComparator implements Comparator<Named> {
+    public static final NamedComparator INSTANCE = new NamedComparator();
+    
     public int compare(Named named, Named named1) {
         return named.getName().compareTo(named1.getName());
     }
