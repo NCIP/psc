077a2815f538cc14aef61bd861da9f16a1b62e62
Rhett Sutphin <r-sutphin@northwestern.edu>
2007-12-05 14:34:24 +0000

    Add javadoc, particularly for the fact that the setName operation is optional

--- src/main/java/edu/northwestern/bioinformatics/studycalendar/domain/Named.java
+++ src/main/java/edu/northwestern/bioinformatics/studycalendar/domain/Named.java
@@ -2,8 +2,19 @@ package edu.northwestern.bioinformatics.studycalendar.domain;
 
 /**
  * @author Rhett Sutphin
+ * @see edu.northwestern.bioinformatics.studycalendar.utils.NamedComparator
  */
 public interface Named {
+    /**
+     * Get the name for this instance, either set explicitly or computed.
+     * @return
+     */
     String getName();
+
+    /**
+     * Set the name.  This is an optional operation; if an implementor uses a computed
+     * name, it should throw {@link UnsupportedOperationException}.
+     * @param name
+     */
     void setName(String name);
 }
