28e9e9cdce0d543fd58c20a6403a93f8e1fd69a5
Saurabh Agrawal <saurabh.agrawal@semanticbits.com>
2007-12-06 17:40:23 +0000

    removing commit and rollback methods from dao..these methods were added for psc grid services.  we don't need these method now because grid service will commit immediately.

--- src/main/java/edu/northwestern/bioinformatics/studycalendar/dao/StudyDao.java
+++ src/main/java/edu/northwestern/bioinformatics/studycalendar/dao/StudyDao.java
@@ -1,13 +1,15 @@
 package edu.northwestern.bioinformatics.studycalendar.dao;
 
-import java.util.List;
-
-import org.springframework.transaction.annotation.Transactional;
-
-import edu.northwestern.bioinformatics.studycalendar.StudyCalendarSystemException;
 import edu.northwestern.bioinformatics.studycalendar.domain.Study;
 import edu.northwestern.bioinformatics.studycalendar.domain.StudySubjectAssignment;
 import edu.nwu.bioinformatics.commons.CollectionUtils;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.springframework.orm.hibernate3.HibernateCallback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+import java.util.List;
 
 /**
  * @author Rhett Sutphin
@@ -32,48 +34,29 @@ public class StudyDao extends StudyCalendarMutableDomainObjectDao<Study> {
                 studyId);
     }
 
-    public Study getByAssignedIdentifier(final String assignedIdentifier) {
+    public Study getStudyByAssignedIdentifier(final String assignedIdentifier) {
         List<Study> results = getHibernateTemplate().find("from Study where assignedIdentifier= ?", assignedIdentifier);
         return CollectionUtils.firstElement(results);
     }
 
-    public void commitInProgressStudy(final String ccIdentifier) {
-        Object studyId = getSession().createSQLQuery(
-            "select id from studies where load_status=0 and assigned_identifier='" + ccIdentifier + "'")
-            .uniqueResult();
+    public String getStudyIdByAssignedIdentifier(final String assignedIdentifier) {
+        Object studyId = getHibernateTemplate().execute(new HibernateCallback() {
+            public Object doInHibernate(Session session) throws HibernateException, SQLException {
+                return session.createSQLQuery(
+                        "select max(id) from studies where  assigned_identifier='" + assignedIdentifier + "'")
+                        .uniqueResult();
+            }
+        });
 
-        if (studyId == null) {
-            String msg = "Either no study exist with ccIdentifier :" + ccIdentifier
-                + " or study is already been commited";
-            logger.error(msg);
-            throw new StudyCalendarSystemException(msg);
-        }
-        logger.info("For cc identifier:" + ccIdentifier + " found study:id-" + studyId);
 
-        // update load status of the study
-        getSession().createSQLQuery("update studies set load_status=1 where id=" + studyId.toString()).executeUpdate();
-        logger.info("commited study:id:" + studyId);
+        if (studyId != null)
+            return studyId.toString();
+        return null;
     }
 
-    public void deleteInprogressStudy(final String ccIdentifier) {
-        Object studyId = getSession().createSQLQuery(
-            "select id from studies where load_status=0 and assigned_identifier='" + ccIdentifier + "'")
-            .uniqueResult();
 
-        if (studyId == null) {
-            String message = "Either no study exist with ccIdentifier : " + ccIdentifier
-                + " or study is already commited";
-            logger.error(message);
-            throw new StudyCalendarSystemException(message);
-        }
-        logger.info("For cc identifier:" + ccIdentifier + " found study:id=" + studyId);
-
-        // delete study
-        getSession().createSQLQuery("delete from planned_calendars where study_id=" + studyId.toString())
-            .executeUpdate();
-        getSession().createSQLQuery("delete from study_sites where study_id=" + studyId.toString()).executeUpdate();
-
-        getSession().createSQLQuery("delete from studies where id=" + studyId.toString()).executeUpdate();
-        logger.info("deleted study:id:" + studyId);
+    @Transactional(readOnly = false)
+    public void delete(Study study) {
+        getHibernateTemplate().delete(study);
     }
 }
