6510164b2fd31e3f6fe7631b2e5ab25be6cd5f8d
John Dzak <j-dzak@northwestern.edu>
2007-12-13 20:17:36 +0000

    Cleanup and expand template writer tests and schema.

--- src/main/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/TemplateWriter.java
+++ src/main/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/TemplateWriter.java
@@ -1,4 +1,50 @@
 package edu.northwestern.bioinformatics.studycalendar.xml.writers;
 
+import edu.northwestern.bioinformatics.studycalendar.domain.Study;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class TemplateWriter {
+    public static String ROOT_START =
+             "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+             "<study xmlns=\"http://bioinformatics.northwestern.edu/ns/psc/template.xsd\"\n" +
+             "     xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
+             "     xsi:schemaLocation=\"http://bioinformatics.northwestern.edu/ns/psc/template.xsd\">\n";
+
+    public static String PLANNDED_CALENDAR = "<planned-calendar/>";
+    public static String AMENDMENT_START = "<amendment>";
+    public static String DELTA_START = "<delta>";
+    public static String ADD = "<add/>";
+
+    public static String DELTA_END = "</delta>";
+    public static String AMENDMENT_END = "</amendment>";
+    public static String ROOT_END = "</study>";
+
+    public byte[] writeTemplate() throws Exception {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        PrintStream p = new PrintStream(os);
+        p.println(ROOT_START);
+        p.println(PLANNDED_CALENDAR);
+        p.println(AMENDMENT_START);
+        p.println(DELTA_START);
+        p.println(ADD);
+        p.println(DELTA_END);
+        p.println(AMENDMENT_END);
+        p.println(ROOT_END);
+
+        return os.toByteArray();
+    }
+
+    public byte[] writeTemplate(Study study) {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        PrintStream p = new PrintStream(os);
+
+        p.println(ROOT_START);
+        p.println(PLANNDED_CALENDAR);
+
+        p.println(ROOT_END);
+
+        return os.toByteArray();
+    }
 }
--- src/test/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/TemplateWriterTest.java
+++ src/test/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/TemplateWriterTest.java
@@ -5,30 +5,57 @@ import static org.springframework.validation.ValidationUtils.invokeValidator;
 import edu.northwestern.bioinformatics.studycalendar.testing.StudyCalendarTestCase;
 import edu.northwestern.bioinformatics.studycalendar.xml.validators.XmlValidator;
 import edu.northwestern.bioinformatics.studycalendar.xml.validators.Schema;
+import edu.northwestern.bioinformatics.studycalendar.domain.Fixtures;
+import edu.northwestern.bioinformatics.studycalendar.domain.Study;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.BindException;
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayInputStream;
 
 public class TemplateWriterTest extends StudyCalendarTestCase {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+    private Study study;
+    private TemplateWriter witer;
 
     protected void setUp() throws Exception {
         super.setUp();
+
+        witer = new TemplateWriter();
+
+        study = Fixtures.createBlankTemplate();
     }
 
-    public void testWriteTemplate() {
-        String s =  "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-                    "<template xmlns=\"http://bioinformatics.northwestern.edu/ns/psc/template.xsd\"\n" +
-                    "     xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" +
-                    "     xsi:schemaLocation=\"http://bioinformatics.northwestern.edu/ns/psc/template.xsd\">\n";
+    public void testContainsRoot() throws Exception {
+        String output = createAndValidateXml(study);
 
-        s +=        "<amendment><delta><add></add></delta></amendment></template>";
+        assertContains(output, TemplateWriter.ROOT_START);
+        assertContains(output, TemplateWriter.ROOT_END);
+    }
 
-        BindException errors = new BindException(s, StringUtils.EMPTY);
-        invokeValidator(TEMPLATE_VALIDATOR_INSTANCE, new ByteArrayInputStream(s.getBytes()), errors);
 
+    /* Test Helpers */
+
+    public String createAndValidateXml(Study study) {
+        byte[] byteOutput = witer.writeTemplate(study);
+
+        validate(byteOutput);
+
+        return convertToString(byteOutput);
+    }
+
+    private void validate(byte[] byteOutput) {
+        BindException errors = new BindException(byteOutput, StringUtils.EMPTY);
+        invokeValidator(TEMPLATE_VALIDATOR_INSTANCE, new ByteArrayInputStream(byteOutput), errors);
 
         assertFalse("Template xml should be error free", errors.hasErrors());
+    }
+
+    private String convertToString(byte[] byteOutput) {
+        String output = new String(byteOutput);
+        log.debug("XML: {}", output);
+        return output;
     }
 }
