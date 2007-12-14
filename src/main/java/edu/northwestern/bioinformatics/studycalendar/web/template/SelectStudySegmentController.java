7ed21583bde0650326c335dd0a142e003183d2b6
Nataliya Shurupova <n-shurupova@northwestern.edu>
2007-12-14 21:31:55 +0000

    Fixing the ticket #312: Renaming a segment doesn't cause "Release this template" button to show anymore

--- src/main/java/edu/northwestern/bioinformatics/studycalendar/web/template/SelectStudySegmentController.java
+++ src/main/java/edu/northwestern/bioinformatics/studycalendar/web/template/SelectStudySegmentController.java
@@ -3,6 +3,7 @@ package edu.northwestern.bioinformatics.studycalendar.web.template;
 import edu.northwestern.bioinformatics.studycalendar.web.ControllerTools;
 import edu.northwestern.bioinformatics.studycalendar.domain.StudySegment;
 import edu.northwestern.bioinformatics.studycalendar.domain.Study;
+import edu.northwestern.bioinformatics.studycalendar.domain.Epoch;
 import edu.northwestern.bioinformatics.studycalendar.dao.StudySegmentDao;
 import edu.northwestern.bioinformatics.studycalendar.service.TemplateService;
 import edu.northwestern.bioinformatics.studycalendar.service.DeltaService;
@@ -10,11 +11,14 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.beans.factory.annotation.Required;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author Rhett Sutphin
@@ -24,6 +28,7 @@ public class SelectStudySegmentController implements Controller {
     private DeltaService deltaService;
     private ControllerTools controllerTools;
     private StudySegmentDao studySegmentDao;
+    private static final Logger log = LoggerFactory.getLogger(SelectStudySegmentController.class.getName());
 
     @SuppressWarnings({ "unchecked" })
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -36,6 +41,9 @@ public class SelectStudySegmentController implements Controller {
             model.put("developmentRevision", study.getDevelopmentAmendment());
         }
         controllerTools.addHierarchyToModel(studySegment, model);
+        Study theRevisedStudy = deltaService.revise(study, study.getDevelopmentAmendment());
+        List<Epoch> epochs = theRevisedStudy.getPlannedCalendar().getEpochs();
+        model.put("epochs", epochs);
         model.put("studySegment", new StudySegmentTemplate(studySegment));
         return new ModelAndView("template/ajax/selectStudySegment", model);
     }
