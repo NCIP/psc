f4ca14d1a4bd6365113e15dd59503ce79f677516
John Dzak <j-dzak@northwestern.edu>
2007-12-27 03:13:02 +0000

    StudyXMLReader reads planned calendars.

--- src/main/java/edu/northwestern/bioinformatics/studycalendar/xml/readers/StudyXMLReader.java
+++ src/main/java/edu/northwestern/bioinformatics/studycalendar/xml/readers/StudyXMLReader.java
@@ -1,12 +1,17 @@
 package edu.northwestern.bioinformatics.studycalendar.xml.readers;
 
+import static edu.northwestern.bioinformatics.studycalendar.xml.writers.StudyXMLWriter.PLANNDED_CALENDAR;
 import edu.northwestern.bioinformatics.studycalendar.dao.StudyDao;
+import edu.northwestern.bioinformatics.studycalendar.dao.PlannedCalendarDao;
 import edu.northwestern.bioinformatics.studycalendar.domain.Study;
 import edu.northwestern.bioinformatics.studycalendar.domain.PlannedCalendar;
 import static edu.northwestern.bioinformatics.studycalendar.xml.writers.StudyXMLWriter.ASSIGNED_IDENTIFIER;
 import static edu.northwestern.bioinformatics.studycalendar.xml.writers.StudyXMLWriter.ID;
+import edu.northwestern.bioinformatics.studycalendar.xml.writers.StudyXMLWriter;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -15,9 +20,11 @@ import java.io.InputStream;
 
 public class StudyXMLReader  {
     private StudyDao studyDao;
+    private PlannedCalendarDao plannedCalendarDao;
 
-    public StudyXMLReader(StudyDao studyDao) {
+    public StudyXMLReader(StudyDao studyDao, PlannedCalendarDao plannedCalendarDao) {
         this.studyDao = studyDao;
+        this.plannedCalendarDao = plannedCalendarDao;
     }
 
     public Study read(InputStream dataFile) throws Exception {
@@ -41,14 +48,24 @@ public class StudyXMLReader  {
             study = new Study();
             study.setGridId(gridId);
         }
-
+        //TODO: Use amendments to figure out which study is newer and use that assigned identifier
         study.setAssignedIdentifier(element.getAttribute(ASSIGNED_IDENTIFIER));
-
+                                Node e = element.getFirstChild();
         return study;
     }
 
-    protected PlannedCalendar parsePlannedCalendar(Document dom) {
-        return null;
+    protected PlannedCalendar parsePlannedCalendar(Document doc) {
+        NodeList nodes = doc.getElementsByTagName(PLANNDED_CALENDAR);
+
+        Element element = ((Element)nodes.item(0));
+        String gridId = element.getAttribute(ID);
+        PlannedCalendar calendar = plannedCalendarDao.getByGridId(gridId);
+        if (calendar == null) {
+            calendar = new PlannedCalendar();
+            calendar.setGridId(gridId);
+        }
+
+        return calendar;
     }
 
 
--- src/test/java/edu/northwestern/bioinformatics/studycalendar/xml/readers/StudyXMLReaderTest.java
+++ src/test/java/edu/northwestern/bioinformatics/studycalendar/xml/readers/StudyXMLReaderTest.java
@@ -1,8 +1,10 @@
 package edu.northwestern.bioinformatics.studycalendar.xml.readers;
 
 import edu.northwestern.bioinformatics.studycalendar.dao.StudyDao;
+import edu.northwestern.bioinformatics.studycalendar.dao.PlannedCalendarDao;
 import static edu.northwestern.bioinformatics.studycalendar.domain.Fixtures.createNamedInstance;
 import edu.northwestern.bioinformatics.studycalendar.domain.Study;
+import edu.northwestern.bioinformatics.studycalendar.domain.PlannedCalendar;
 import edu.northwestern.bioinformatics.studycalendar.testing.StudyCalendarTestCase;
 import static edu.northwestern.bioinformatics.studycalendar.xml.writers.StudyXMLWriter.*;
 import static org.easymock.EasyMock.expect;
@@ -16,12 +18,14 @@ import static java.text.MessageFormat.format;
 public class StudyXMLReaderTest extends StudyCalendarTestCase {
     private StudyXMLReader reader;
     private StudyDao studyDao;
+    private PlannedCalendarDao plannedCalendarDao;
 
     protected void setUp() throws Exception {
         super.setUp();
 
         studyDao = registerDaoMockFor(StudyDao.class);
-        reader = new StudyXMLReader(studyDao);
+        plannedCalendarDao = registerDaoMockFor(PlannedCalendarDao.class);
+        reader = new StudyXMLReader(studyDao, plannedCalendarDao);
     }
 
     public void testReadNewStudy() throws Exception {
@@ -46,17 +50,16 @@ public class StudyXMLReaderTest extends StudyCalendarTestCase {
     public void testReadExistingStudy() throws Exception {
         StringBuffer buf = new StringBuffer();
         buf.append(       "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-           .append(       "<study assigned-identifier=\"Study A\" id=\"grid2\" \n")
+           .append(       "<study assigned-identifier=\"Study A\" id=\"grid1\" \n")
            .append(format("       {0}=\"{1}\" \n"     , SCHEMA_NAMESPACE_ATTRIBUTE, SCHEMA_NAMESPACE))
            .append(format("       {0}=\"{1}\" \n"     , SCHEMA_LOCATION_ATTRIBUTE, SCHEMA_LOCATION))
            .append(format("       {0}=\"{1}\" >\n"    , XML_SCHEMA_ATTRIBUTE, XML_SCHEMA))
            .append(       "</study>");
 
         Study study = createNamedInstance("Study A", Study.class);
-        study.setGridId("grid2");
-        study.setAssignedIdentifier("Study A");
+        study.setGridId("grid1");
         
-        expect(studyDao.getByGridId("grid2")).andReturn(study);
+        expect(studyDao.getByGridId("grid1")).andReturn(study);
         replayMocks();
         
         Study actual = reader.parseStudy(parse(buf));
@@ -64,8 +67,44 @@ public class StudyXMLReaderTest extends StudyCalendarTestCase {
 
         assertSame("Studies should be same", study, actual);
         assertEquals("Wrong Study Identifier", "Study A", actual.getAssignedIdentifier());
-        assertEquals("Wrong Grid Id", "grid2", actual.getGridId());
+        assertEquals("Wrong Grid Id", "grid1", actual.getGridId());
     }
+
+    public void testReadNewPlannedCalendar() throws Exception {
+        StringBuffer buf = new StringBuffer();
+        buf.append(       "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+           .append(       "<planned-calendar id=\"grid1\" />\n");
+
+        PlannedCalendar calendar = new PlannedCalendar();
+        calendar.setGridId("grid1");
+
+        expect(plannedCalendarDao.getByGridId("grid1")).andReturn(calendar);
+        replayMocks();
+        
+        PlannedCalendar actual = reader.parsePlannedCalendar(parse(buf));
+        verifyMocks();
+
+        assertSame("Studies should be same", calendar, actual);
+        assertEquals("Wrong Grid Id", "grid1", actual.getGridId());
+    }
+
+    public void testReadExistingPlannedCalendar() throws Exception {
+        StringBuffer buf = new StringBuffer();
+        buf.append(       "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+           .append(       "<planned-calendar id=\"grid1\" />\n");
+
+        PlannedCalendar calendar = new PlannedCalendar();
+        calendar.setGridId("grid1");
+
+        expect(plannedCalendarDao.getByGridId("grid1")).andReturn(null);
+        replayMocks();
+
+        PlannedCalendar actual = reader.parsePlannedCalendar(parse(buf));
+        verifyMocks();
+
+        assertEquals("Wrong Grid Id", "grid1", actual.getGridId());
+    }
+
 
     /* Test Helpers */
     private Document parse(StringBuffer buf) throws Exception {
