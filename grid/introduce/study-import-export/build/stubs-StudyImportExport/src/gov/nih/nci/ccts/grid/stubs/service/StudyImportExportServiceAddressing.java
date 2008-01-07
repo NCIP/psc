c361be70c528c39b1efc41c646470958ac12c646
Saurabh Agrawal <saurabh.agrawal@semanticbits.com>
2008-01-07 20:58:37 +0000

    adding study-import-export-grid service..

---
+++ grid/introduce/study-import-export/build/stubs-StudyImportExport/src/gov/nih/nci/ccts/grid/stubs/service/StudyImportExportServiceAddressing.java
@@ -0,0 +1,14 @@
+/**
+ * StudyImportExportServiceAddressing.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.2RC2 Apr 28, 2006 (12:42:00 EDT) WSDL2Java emitter.
+ */
+
+package gov.nih.nci.ccts.grid.stubs.service;
+
+public interface StudyImportExportServiceAddressing extends gov.nih.nci.ccts.grid.stubs.service.StudyImportExportService {
+    public gov.nih.nci.ccts.grid.stubs.StudyImportExportPortType getStudyImportExportPortTypePort(org.apache.axis.message.addressing.EndpointReferenceType reference) throws javax.xml.rpc.ServiceException;
+
+
+}
