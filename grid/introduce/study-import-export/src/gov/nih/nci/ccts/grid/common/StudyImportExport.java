0ec53a250ba164fb8b2e42960c05a63a6374fdc1
Saurabh Agrawal <saurabh.agrawal@semanticbits.com>
2008-01-11 15:04:21 +0000

    implementing importStudy method..

--- grid/introduce/study-import-export/src/gov/nih/nci/ccts/grid/common/StudyImportExport.java
+++ grid/introduce/study-import-export/src/gov/nih/nci/ccts/grid/common/StudyImportExport.java
@@ -8,10 +8,14 @@ import java.rmi.RemoteException;
 public interface StudyImportExport {
     /**
      * Search study by coordinating center identifier and returns xml string of corresponding study
+     *
      * @param coordinatingCenterIdentifier
-     * @return  study xml string.
+     * @return study xml string.
      * @throws RemoteException if no study found or for any other exception occured
      */
     public String exportStudyByCoordinatingCenterIdentifier(String coordinatingCenterIdentifier) throws RemoteException;
+
+    public void importStudy(java.lang.String string) throws RemoteException;
+
 
 }
