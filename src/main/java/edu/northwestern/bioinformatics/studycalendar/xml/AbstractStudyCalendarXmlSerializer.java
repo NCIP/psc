450ab741952cbfe98edaba0a0e69bbd889b166de
John Dzak <j-dzak@northwestern.edu>
2008-01-15 20:48:28 +0000

    Changed study serializer and abstract seralizer to have correct functionality.

--- src/main/java/edu/northwestern/bioinformatics/studycalendar/xml/AbstractStudyCalendarXmlSerializer.java
+++ src/main/java/edu/northwestern/bioinformatics/studycalendar/xml/AbstractStudyCalendarXmlSerializer.java
@@ -4,17 +4,36 @@ package edu.northwestern.bioinformatics.studycalendar.xml;
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
+import static org.apache.commons.lang.StringUtils.EMPTY;
 
 import java.io.Reader;
+
+import edu.northwestern.bioinformatics.studycalendar.xml.writers.StudyXMLWriter;
 
 /**
  * @author Rhett Sutphin
  * @author John Dzak
  */
 public abstract class AbstractStudyCalendarXmlSerializer<R> implements StudyCalendarXmlSerializer<R> {
+    public static final String XML_NS = StudyXMLWriter.XML_NS;
+    public static final String XSI_NS = StudyXMLWriter.XSI_NS;
+    public static final String PSC_NS = StudyXMLWriter.PSC_NS;
+    public static final String SCHEMA_LOCATION  = StudyXMLWriter.SCHEMA_LOCATION;
+
+    public static final String SCHEMA_LOCATION_ATTRIBUTE  = "schemaLocation";
+    public static final String XML_SCHEMA_ATTRIBUTE       = "xsi";
 
     public Document createDocument(R root) {
-        throw new UnsupportedOperationException("TODO");
+        Document document = DocumentHelper.createDocument();
+        Element element = createElement(root);
+
+        element.addNamespace(EMPTY, PSC_NS)
+                .addNamespace(XML_SCHEMA_ATTRIBUTE, XSI_NS)
+                .addNamespace(SCHEMA_LOCATION_ATTRIBUTE, PSC_NS + ' ' + SCHEMA_LOCATION);
+
+        document.add(element);
+        
+        return document;
     }
 
     public String createDocumentString(R root) {
@@ -33,8 +52,4 @@ public abstract class AbstractStudyCalendarXmlSerializer<R> implements StudyCale
 
     public abstract R readElement(Element element);
 
-    // Create Instance of Document
-    protected Document createDocumentInstance() {
-        return DocumentHelper.createDocument();
-    }
 }
--- src/main/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/StudyXmlSerializer.java
+++ src/main/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/StudyXmlSerializer.java
@@ -2,38 +2,21 @@ package edu.northwestern.bioinformatics.studycalendar.xml.writers;
 
 import edu.northwestern.bioinformatics.studycalendar.domain.Study;
 import edu.northwestern.bioinformatics.studycalendar.xml.AbstractStudyCalendarXmlSerializer;
-import static org.apache.commons.lang.StringUtils.EMPTY;
-import org.dom4j.Document;
+import org.dom4j.DocumentFactory;
 import org.dom4j.Element;
 
-public class StudyXmlSerializer extends AbstractStudyCalendarXmlSerializer<Study>
-{
-    public static final String XML_NS = StudyXMLWriter.XML_NS;
-    public static final String XSI_NS = StudyXMLWriter.XSI_NS;
-    public static final String PSC_NS = StudyXMLWriter.PSC_NS;
-    public static final String SCHEMA_LOCATION  = StudyXMLWriter.SCHEMA_LOCATION;
-
-    public static final String SCHEMA_LOCATION_ATTRIBUTE  = "schemaLocation";
-    public static final String XML_SCHEMA_ATTRIBUTE       = "xsi";
-
+public class StudyXmlSerializer extends AbstractStudyCalendarXmlSerializer<Study> {
     public static final String STUDY = "study";
     public static final String ASSIGNED_IDENTIFIER = "assigned-identifier";
     public static final String ID = "id";
 
-    public Document createDocument(Study study) {
-        Document document = createDocumentInstance();
-        Element element = createElement(document, study);
-
-        element.addNamespace(EMPTY, PSC_NS)
-               .addNamespace(XML_SCHEMA_ATTRIBUTE, XSI_NS)
-               .addNamespace(SCHEMA_LOCATION_ATTRIBUTE, PSC_NS + ' ' + SCHEMA_LOCATION);
-
-        return document;
-    }
-
     public Element createElement(Study study) {
-        Document document = createDocumentInstance();
-        return createElement(document, study);
+        Element element = DocumentFactory.getInstance().createElement(STUDY);
+
+        element.addAttribute(ID, study.getGridId())
+               .addAttribute(ASSIGNED_IDENTIFIER, study.getAssignedIdentifier());
+
+        return element;
     }
 
     public Study readElement(Element element) {
@@ -41,12 +24,5 @@ public class StudyXmlSerializer extends AbstractStudyCalendarXmlSerializer<Study
         study.setGridId(element.attributeValue(ID));
         study.setAssignedIdentifier(element.attributeValue(ASSIGNED_IDENTIFIER));
         return study;
-    }
-
-    // Create Element without namespace
-    protected Element createElement(Document document, Study study) {
-        return document.addElement(STUDY)
-                .addAttribute(ID, study.getGridId())
-                .addAttribute(ASSIGNED_IDENTIFIER, study.getAssignedIdentifier());
     }
 }
