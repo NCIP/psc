9ce7c7c90ae61544c525ad6754623276b9924677
Rhett Sutphin <r-sutphin@northwestern.edu>
2008-01-17 16:34:50 +0000

    Add mechanism to view HTML version of embedded WADL

--- src/main/java/edu/northwestern/bioinformatics/studycalendar/restlets/PscWadlRepresentation.java
+++ src/main/java/edu/northwestern/bioinformatics/studycalendar/restlets/PscWadlRepresentation.java
@@ -2,7 +2,12 @@ package edu.northwestern.bioinformatics.studycalendar.restlets;
 
 import freemarker.template.Configuration;
 import org.restlet.data.MediaType;
+import org.restlet.data.Request;
 import org.restlet.ext.freemarker.TemplateRepresentation;
+import org.restlet.resource.Representation;
+
+import java.util.Map;
+import java.util.Collections;
 
 /**
  * @author Rhett Sutphin
@@ -10,7 +15,20 @@ import org.restlet.ext.freemarker.TemplateRepresentation;
 public class PscWadlRepresentation extends TemplateRepresentation {
     public static final String PSC_WADL_PATH = "restlets/psc.wadl";
 
-    public PscWadlRepresentation(Configuration config, Object dataModel) {
+    protected PscWadlRepresentation(Configuration config, Object dataModel) {
         super(PSC_WADL_PATH, config, dataModel, MediaType.APPLICATION_WADL_XML);
     }
+
+    public static Representation create(Configuration configuration, Request request) {
+        return new PscWadlRepresentation(configuration, createDataModel(request));
+    }
+
+    private static Map<String, String> createDataModel(Request request) {
+        return Collections.singletonMap("baseUri", determineBaseUri(request));
+    }
+
+    private static String determineBaseUri(Request request) {
+        return request.getRootRef().toString();
+    }
+
 }
--- src/main/java/edu/northwestern/bioinformatics/studycalendar/restlets/WadlResource.java
+++ src/main/java/edu/northwestern/bioinformatics/studycalendar/restlets/WadlResource.java
@@ -1,43 +1,28 @@
 package edu.northwestern.bioinformatics.studycalendar.restlets;
 
 import org.restlet.Context;
-import org.restlet.data.MediaType;
 import org.restlet.data.Request;
 import org.restlet.data.Response;
 import org.restlet.resource.Resource;
-import org.restlet.resource.Variant;
-import org.restlet.resource.Representation;
-import org.restlet.resource.ResourceException;
 import org.springframework.beans.factory.annotation.Required;
 import freemarker.template.Configuration;
-
-import java.util.Collections;
-import java.util.Map;
 
 /**
  * @author Rhett Sutphin
  */
 public class WadlResource extends Resource {
-    private Configuration configuration;
+    private Configuration freemarkerConfiguration;
 
     @Override
     public void init(Context context, Request request, Response response) {
         super.init(context, request, response);
-        getVariants().add(new PscWadlRepresentation(configuration, createDataModel()));
-    }
-
-    private Map<String, String> createDataModel() {
-        return Collections.singletonMap("baseUri", determineBaseUri());
-    }
-
-    private String determineBaseUri() {
-        return getRequest().getRootRef().toString();
+        getVariants().add(PscWadlRepresentation.create(freemarkerConfiguration, request));
     }
 
     ////// CONFIGURATION
 
     @Required
     public void setFreemarkerConfiguration(Configuration configuration) {
-        this.configuration = configuration;
+        this.freemarkerConfiguration = configuration;
     }
 }
