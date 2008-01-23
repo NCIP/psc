43622175bdee63a7c0b3e35e9cd7bafd153e64b2
Rhett Sutphin <r-sutphin@northwestern.edu>
2008-01-23 21:54:06 +0000

    Fix test so it doesn't interfere with other DAO tests.  Theory for cause: AbstractTransactionalDataSourceSpringContextTests's separate loading of the application context was interfering with the various thread-bound data access objects (sessions, transactions).

--- src/test/java/edu/northwestern/bioinformatics/studycalendar/service/UserServiceIntegratedTest.java
+++ src/test/java/edu/northwestern/bioinformatics/studycalendar/service/UserServiceIntegratedTest.java
@@ -1,49 +1,50 @@
 package edu.northwestern.bioinformatics.studycalendar.service;
 
-import org.springframework.test.AbstractTransactionalDataSourceSpringContextTests;
-import edu.northwestern.bioinformatics.studycalendar.testing.ContextTools;
+import edu.northwestern.bioinformatics.studycalendar.testing.StudyCalendarDbTestCase;
 import edu.northwestern.bioinformatics.studycalendar.domain.User;
 import edu.northwestern.bioinformatics.studycalendar.domain.Fixtures;
 import edu.northwestern.bioinformatics.studycalendar.domain.Role;
 import edu.northwestern.bioinformatics.studycalendar.dao.UserDao;
 
-import java.util.Date;
-
-import gov.nih.nci.cabig.ctms.audit.domain.DataAuditInfo;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 
 /**
  * @author Rhett Sutphin
  */
-public class UserServiceIntegratedTest extends AbstractTransactionalDataSourceSpringContextTests {
-    public UserServiceIntegratedTest() {
-        setAutowireMode(AUTOWIRE_BY_NAME);
+public class UserServiceIntegratedTest extends StudyCalendarDbTestCase {
+    private TransactionStatus transaction;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        transaction = getTransactionManager().getTransaction(
+            new DefaultTransactionDefinition(TransactionDefinition.PROPAGATION_REQUIRED));
     }
 
     @Override
-    protected String[] getConfigLocations() {
-        return ContextTools.DEPLOYED_CONFIG_LOCATIONS;
+    protected void tearDown() throws Exception {
+        getTransactionManager().rollback(transaction);
+        // CSM seems to use its own transaction, or something
+        getJdbcTemplate().execute("DELETE from csm_user");
+        super.tearDown();
+    }
+
+    private PlatformTransactionManager getTransactionManager() {
+        return (PlatformTransactionManager) getApplicationContext().getBean("transactionManager");
     }
 
     @Override
-    protected void onSetUpBeforeTransaction() throws Exception {
-        super.onSetUpBeforeTransaction();
-        DataAuditInfo.setLocal(new DataAuditInfo("admin", "127.0.0.8", new Date(), "/etc/etc"));
-    }
-
-    @Override
-    protected void onTearDownAfterTransaction() throws Exception {
-        DataAuditInfo.setLocal(null);
-        // CSM seems to do its own COMMIT, so we need to clean up after it 
-        deleteFromTables(new String[] { "csm_user" });
-        super.onTearDownAfterTransaction();
+    protected String getTestDataFileName() {
+        return "testdata/none.xml";
     }
 
     // this test is primarily to ensure that saving users all the way through to the database works,
     // including the mirror into CSM
     public void testUserCreationWorks() throws Exception {
-        // TODO: temporarily disable while working out why this test interferes with ConfigurationTest
-/*
         {
             User user = Fixtures.createUser("joe", Role.STUDY_ADMIN, Role.SITE_COORDINATOR);
             getUserService().saveUser(user, "alfalfa");
@@ -51,7 +52,6 @@ public class UserServiceIntegratedTest extends AbstractTransactionalDataSourceSp
 
         User reloaded = getUserDao().getByName("joe");
         assertNotNull(reloaded);
-*/
     }
 
     private UserDao getUserDao() {
