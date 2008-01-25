61c7b445d8b0d5381e8ddc0eb750799e3716bbf4
Rhett Sutphin <r-sutphin@northwestern.edu>
2008-01-25 18:25:53 +0000

    Remove unused imports

--- src/main/java/edu/northwestern/bioinformatics/studycalendar/restlets/ActivitySourceResource.java
+++ src/main/java/edu/northwestern/bioinformatics/studycalendar/restlets/ActivitySourceResource.java
@@ -2,18 +2,7 @@ package edu.northwestern.bioinformatics.studycalendar.restlets;
 
 import edu.northwestern.bioinformatics.studycalendar.dao.SourceDao;
 import edu.northwestern.bioinformatics.studycalendar.domain.Source;
-import edu.northwestern.bioinformatics.studycalendar.xml.readers.ActivityXMLReader;
-import org.restlet.Context;
-import org.restlet.data.MediaType;
-import org.restlet.data.Reference;
 import org.restlet.data.Request;
-import org.restlet.data.Response;
-import org.restlet.resource.Representation;
-import org.restlet.resource.Resource;
-import org.restlet.resource.ResourceException;
-import org.restlet.resource.Variant;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Required;
 
 /**
@@ -24,6 +13,7 @@ import org.springframework.beans.factory.annotation.Required;
 public class ActivitySourceResource extends AbstractStorableDomainObjectResource<Source> {
     private SourceDao sourceDao;
 
+    @Override
     protected Source loadRequestedObject(Request request) {
         String sourceName = UriTemplateParameters.SOURCE_NAME.extractFrom(request);
         return sourceDao.getByName(sourceName);
