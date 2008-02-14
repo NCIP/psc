e02d9c2932a1944f3833927148ac1f142a1f8a1d
John Dzak <j-dzak@northwestern.edu>
2008-02-14 17:19:22 +0000

    Renaming misnamed test.

--- src/test/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/ScheduledCalendarXmlSerializerTest.java
+++ src/test/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/ScheduledCalendarXmlSerializerTest.java
@@ -1,18 +1,23 @@
 package edu.northwestern.bioinformatics.studycalendar.xml.writers;
 
+import edu.northwestern.bioinformatics.studycalendar.domain.ScheduledCalendar;
 import edu.northwestern.bioinformatics.studycalendar.testing.StudyCalendarXmlTestCase;
 
 /**
  * @author John Dzak
  */
-public class ScheduledStudyXmlSerializerTest extends StudyCalendarXmlTestCase {
+public class ScheduledCalendarXmlSerializerTest extends StudyCalendarXmlTestCase {
+    private ScheduledCalendar schedule;
 
     protected void setUp() throws Exception {
         super.setUp();
+
+        schedule = new ScheduledCalendar();
     }
 
     public void testCreateElement() {
         assertTrue(true);
+//        serializer.createElement(schedule);
     }
 
     public void testReadElement() {
