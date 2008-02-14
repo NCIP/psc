07d7613118aa8b8ae9aa2b71b2d3edb247913767
John Dzak <j-dzak@northwestern.edu>
2008-02-14 16:57:12 +0000

    Create initial tests for ScheduledStudXmlSerializer.

---
+++ src/test/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/ScheduledStudyXmlSerializerTest.java
@@ -0,0 +1,21 @@
+package edu.northwestern.bioinformatics.studycalendar.xml.writers;
+
+import edu.northwestern.bioinformatics.studycalendar.testing.StudyCalendarXmlTestCase;
+
+/**
+ * @author John Dzak
+ */
+public class ScheduledStudyXmlSerializerTest extends StudyCalendarXmlTestCase {
+
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testCreateElement() {
+        assertTrue(true);
+    }
+
+    public void testReadElement() {
+        assertTrue(true);
+    }
+}
