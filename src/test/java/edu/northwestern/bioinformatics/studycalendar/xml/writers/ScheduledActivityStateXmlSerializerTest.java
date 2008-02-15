704cbbf5cd4ea412c290c3cf25878975274b3068
John Dzak <j-dzak@northwestern.edu>
2008-02-15 21:50:56 +0000

    1) Add scheduled activity state schema definition 2) Create skeleton for scheduled activity state serializer test

---
+++ src/test/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/ScheduledActivityStateXmlSerializerTest.java
@@ -0,0 +1,20 @@
+package edu.northwestern.bioinformatics.studycalendar.xml.writers;
+
+import edu.northwestern.bioinformatics.studycalendar.testing.StudyCalendarXmlTestCase;
+
+/**
+ * @author John Dzak
+ */
+public class ScheduledActivityStateXmlSerializerTest extends StudyCalendarXmlTestCase {
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testCreateElement() {
+        assertTrue(true);
+    }
+
+    public void testReadElement() {
+        assertTrue(true);
+    }
+}
