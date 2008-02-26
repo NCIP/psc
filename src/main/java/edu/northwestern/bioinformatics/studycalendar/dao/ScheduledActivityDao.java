f1f9a7d6bcfa3cd8661ebae7113cc171db1863d2
John Dzak <j-dzak@northwestern.edu>
2008-02-26 17:50:35 +0000

    Add javadoc to scheduled activity dao.

--- src/main/java/edu/northwestern/bioinformatics/studycalendar/dao/ScheduledActivityDao.java
+++ src/main/java/edu/northwestern/bioinformatics/studycalendar/dao/ScheduledActivityDao.java
@@ -1,13 +1,13 @@
 package edu.northwestern.bioinformatics.studycalendar.dao;
 
+import edu.northwestern.bioinformatics.studycalendar.domain.PlannedActivity;
 import edu.northwestern.bioinformatics.studycalendar.domain.ScheduledActivity;
 import edu.northwestern.bioinformatics.studycalendar.domain.ScheduledCalendar;
-import edu.northwestern.bioinformatics.studycalendar.domain.PlannedActivity;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
-import java.util.ArrayList;
 
 /**
  * @author Rhett Sutphin
@@ -15,6 +15,14 @@ import java.util.ArrayList;
 public class ScheduledActivityDao extends StudyCalendarMutableDomainObjectDao<ScheduledActivity> {
     @Override public Class<ScheduledActivity> domainClass() { return ScheduledActivity.class; }
 
+    /**
+    * Finds all the scheduled activities for a given calendar by the start date and end date given.
+    *
+    * @param  calendar the calendar to retrieve the scheduled activities from
+    * @param  start the start date of the time span search
+    * @param  end the end date of the time span search
+    * @return      a list of scheduled activities for the criteria specified
+    */
     @SuppressWarnings({ "unchecked" })
     public Collection<ScheduledActivity> getEventsByDate(ScheduledCalendar calendar, Date start, Date end) {
         StringBuilder builder = new StringBuilder("from ScheduledActivity e where e.scheduledStudySegment.scheduledCalendar = ?");
@@ -31,6 +39,13 @@ public class ScheduledActivityDao extends StudyCalendarMutableDomainObjectDao<Sc
         return getHibernateTemplate().find(builder.toString(), params.toArray());
     }
 
+    /**
+    * Finds all the scheduled activities for the given calendar and planned activity
+    *
+    * @param  source the planned activity to search for in the scheduled activities
+    * @param  calendar the calendar to search through for all the scheduled activities
+    * @return      a list of all the scheduled activities that meet the criteria specified
+    */
     @SuppressWarnings({ "unchecked" })
     public Collection<ScheduledActivity> getEventsFromPlannedActivity(
         PlannedActivity source, ScheduledCalendar calendar
