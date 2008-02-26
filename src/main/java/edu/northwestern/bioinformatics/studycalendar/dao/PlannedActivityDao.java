54d579ed832a49f1a22dfb2388da24f67f884a9e
John Dzak <j-dzak@northwestern.edu>
2008-02-26 17:28:52 +0000

    Add javadoc to planned activity dao.

--- src/main/java/edu/northwestern/bioinformatics/studycalendar/dao/PlannedActivityDao.java
+++ src/main/java/edu/northwestern/bioinformatics/studycalendar/dao/PlannedActivityDao.java
@@ -15,15 +15,23 @@ public class PlannedActivityDao extends StudyCalendarMutableDomainObjectDao<Plan
         return PlannedActivity.class;
     }
 
+    /**
+    * Deletes a planned activity
+    *
+    * @param  event the planned activity to delete
+    */
     @Transactional(readOnly=false)
     public void delete(PlannedActivity event) {
         getHibernateTemplate().delete(event);
     }
 
+    /**
+    * Finds all planned activities for a activity id
+    *
+    * @param  activityId the activity id to search with
+    * @return      a list of planned activities with the activity id passed in
+    */
     public List<PlannedActivity> getPlannedActivitiesForAcivity(Integer activityId) {
-
         return (List<PlannedActivity>) getHibernateTemplate().find("select pa from PlannedActivity as pa where pa.activity.id=?",activityId);
     }
-
-   
 }
