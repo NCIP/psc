787c2439c7588409745e2fd52048ba531396b72a
Nataliya Shurupova <n-shurupova@northwestern.edu>
2008-03-03 20:41:37 +0000

    Fix for bug # 364 - "Enter" key now works for "Activities for the next X days". Clicking outside the textbox also updates the information, if Enter wasn't pressed.

--- src/main/webapp/js/dashboards.js
+++ src/main/webapp/js/dashboards.js
@@ -8,7 +8,17 @@ SC.updateCurrentActivities = function(href) {
 SC.registerCurrentActivitiesUpdaters = function(href) {
     Event.observe(window, 'load', function() {
         var handler = function() { SC.updateCurrentActivities(href) }
+
         $('toDate').observe('change', handler)
+        var browser=navigator.appName;
+        if (browser=="Microsoft Internet Explorer") {
+            $('toDate').observe('keypress', function(e) {
+                if(e.keyCode == 13) {
+                    SC.updateCurrentActivities(href)
+                }
+            })
+        }
+
         $$('input.activity-type').each(function(input) {
             input.observe('click', handler)
         })
