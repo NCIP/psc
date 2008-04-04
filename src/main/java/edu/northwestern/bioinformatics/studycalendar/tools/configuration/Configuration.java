4680f1769d4d5ee8f2a519873b89a83b63cd9dd6
Saurabh Agrawal <saurabh.agrawal@semanticbits.com>
2008-04-04 14:23:20 +0000

    [396] adding @Transactional annotation on Configuration.java

--- src/main/java/edu/northwestern/bioinformatics/studycalendar/tools/configuration/Configuration.java
+++ src/main/java/edu/northwestern/bioinformatics/studycalendar/tools/configuration/Configuration.java
@@ -1,62 +1,61 @@
 package edu.northwestern.bioinformatics.studycalendar.tools.configuration;
 
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.annotation.Propagation;
-import org.restlet.util.Template;
-
-import java.util.List;
-import java.util.Map;
-
+import gov.nih.nci.cabig.ctms.tools.configuration.ConfigurationEntry;
 import gov.nih.nci.cabig.ctms.tools.configuration.ConfigurationProperties;
 import gov.nih.nci.cabig.ctms.tools.configuration.ConfigurationProperty;
 import gov.nih.nci.cabig.ctms.tools.configuration.DatabaseBackedConfiguration;
-import gov.nih.nci.cabig.ctms.tools.configuration.ConfigurationEntry;
+import org.restlet.util.Template;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Rhett Sutphin
  */
 public class Configuration extends DatabaseBackedConfiguration {
     public static final ConfigurationProperties PROPERTIES
-        = new ConfigurationProperties(new ClassPathResource("details.properties", Configuration.class));
+            = new ConfigurationProperties(new ClassPathResource("details.properties", Configuration.class));
 
     public static final ConfigurationProperty<String>
-        DEPLOYMENT_NAME = PROPERTIES.add(new ConfigurationProperty.Text("deploymentName"));
+            DEPLOYMENT_NAME = PROPERTIES.add(new ConfigurationProperty.Text("deploymentName"));
     public static final ConfigurationProperty<String>
-        MAIL_REPLY_TO = PROPERTIES.add(new ConfigurationProperty.Text("replyTo"));
+            MAIL_REPLY_TO = PROPERTIES.add(new ConfigurationProperty.Text("replyTo"));
     public static final ConfigurationProperty<List<String>>
-        MAIL_EXCEPTIONS_TO = PROPERTIES.add(new ConfigurationProperty.Csv("mailExceptionsTo"));
+            MAIL_EXCEPTIONS_TO = PROPERTIES.add(new ConfigurationProperty.Csv("mailExceptionsTo"));
     public static final ConfigurationProperty<String>
-        SMTP_HOST = PROPERTIES.add(new ConfigurationProperty.Text("smtpHost"));
+            SMTP_HOST = PROPERTIES.add(new ConfigurationProperty.Text("smtpHost"));
     public static final ConfigurationProperty<Integer>
-        SMTP_PORT = PROPERTIES.add(new ConfigurationProperty.Int("smtpPort"));
+            SMTP_PORT = PROPERTIES.add(new ConfigurationProperty.Int("smtpPort"));
     public static final ConfigurationProperty<Boolean>
-        SHOW_DEBUG_INFORMATION = PROPERTIES.add(new ConfigurationProperty.Bool("showDebugInformation"));
+            SHOW_DEBUG_INFORMATION = PROPERTIES.add(new ConfigurationProperty.Bool("showDebugInformation"));
 
     public static final ConfigurationProperty<Template>
-        CAAERS_BASE_URL = PROPERTIES.add(new TemplateConfigurationProperty("caAERSBaseUrl"));
+            CAAERS_BASE_URL = PROPERTIES.add(new TemplateConfigurationProperty("caAERSBaseUrl"));
     public static final ConfigurationProperty<Template>
-        LABVIEWER_BASE_URL = PROPERTIES.add(new TemplateConfigurationProperty("labViewerBaseUrl"));
+            LABVIEWER_BASE_URL = PROPERTIES.add(new TemplateConfigurationProperty("labViewerBaseUrl"));
 
 
     public static final ConfigurationProperty<Template>
-        PATIENT_PAGE_URL = PROPERTIES.add(new TemplateConfigurationProperty("patientPageUrl"));
+            PATIENT_PAGE_URL = PROPERTIES.add(new TemplateConfigurationProperty("patientPageUrl"));
     public static final ConfigurationProperty<String>
-        CTMS_NAME= PROPERTIES.add(new ConfigurationProperty.Text("ctmsName"));
+            CTMS_NAME = PROPERTIES.add(new ConfigurationProperty.Text("ctmsName"));
     public static final ConfigurationProperty<String>
-        BASE_CTMS_URL = PROPERTIES.add(new ConfigurationProperty.Text("ctmsUrl"));
+            BASE_CTMS_URL = PROPERTIES.add(new ConfigurationProperty.Text("ctmsUrl"));
 
 
     public static final ConfigurationProperty<Template>
-        STUDY_PAGE_URL = PROPERTIES.add(new TemplateConfigurationProperty("studyPageUrl"));
-    
+            STUDY_PAGE_URL = PROPERTIES.add(new TemplateConfigurationProperty("studyPageUrl"));
+
 
     ////// PSC-SPECIFIC LOGIC
-
     public boolean getExternalAppsConfigured() {
         return get(CAAERS_BASE_URL) != null || get(LABVIEWER_BASE_URL) != null || get(PATIENT_PAGE_URL) != null;
     }
 
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
     public boolean getCtmsConfigured() {
         return get(CTMS_NAME) != null && get(BASE_CTMS_URL) != null;
     }
