8a4111b81b52683164b9d8c370736f7cf403203f
Saurabh Agrawal <saurabh.agrawal@semanticbits.com>
2008-04-14 18:44:29 +0000

    #301 fixing breadcrumb issues in create/import activity..

--- src/main/java/edu/northwestern/bioinformatics/studycalendar/web/template/ImportActivitiesController.java
+++ src/main/java/edu/northwestern/bioinformatics/studycalendar/web/template/ImportActivitiesController.java
@@ -1,27 +1,35 @@
 package edu.northwestern.bioinformatics.studycalendar.web.template;
 
-import edu.northwestern.bioinformatics.studycalendar.utils.accesscontrol.AccessControl;
-import edu.northwestern.bioinformatics.studycalendar.web.PscSimpleFormController;
+import edu.northwestern.bioinformatics.studycalendar.dao.PeriodDao;
 import edu.northwestern.bioinformatics.studycalendar.domain.Role;
 import edu.northwestern.bioinformatics.studycalendar.service.ImportActivitiesService;
+import edu.northwestern.bioinformatics.studycalendar.utils.accesscontrol.AccessControl;
+import edu.northwestern.bioinformatics.studycalendar.utils.breadcrumbs.BreadcrumbContext;
+import edu.northwestern.bioinformatics.studycalendar.utils.breadcrumbs.DefaultCrumb;
+import edu.northwestern.bioinformatics.studycalendar.web.PscSimpleFormController;
 import edu.nwu.bioinformatics.commons.spring.ValidatableValidator;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindException;
-import org.springframework.beans.factory.annotation.Required;
+import org.springframework.validation.Errors;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
 import java.util.Map;
+import java.util.HashMap;
 
 @AccessControl(roles = Role.STUDY_COORDINATOR)
 public class ImportActivitiesController extends PscSimpleFormController {
     private ImportActivitiesService importActivitiesService;
+    private PeriodDao periodDao;
 
     public ImportActivitiesController() {
         setCommandClass(ImportActivitiesCommand.class);
         setValidator(new ValidatableValidator());
         setFormView("template/importActivities");
         setSuccessView("redirectToStudyList");
+        setCrumb(new Crumb());
     }
 
     protected ModelAndView onSubmit(Object o, BindException errors) throws Exception {
@@ -36,15 +44,46 @@ public class ImportActivitiesController extends PscSimpleFormController {
         }
     }
 
+
     protected ImportActivitiesCommand formBackingObject(HttpServletRequest httpServletRequest) throws Exception {
         ImportActivitiesCommand command = new ImportActivitiesCommand();
         command.setImportActivitiesService(importActivitiesService);
         return command;
     }
 
+    @Override
+    protected Map referenceData(HttpServletRequest request, Object command, Errors errors) throws Exception {
+        Map<String, Object> refdata = new HashMap<String,Object>();
+
+        Integer periodId = ((ImportActivitiesCommand) command).getReturnToPeriodId();
+        if (periodId != null) {
+            getControllerTools().addHierarchyToModel(periodDao.getById(periodId), refdata);
+        }
+        return refdata;
+    }
+
     //// Field setters
     @Required
     public void setImportActivitiesService(ImportActivitiesService importActivitiesService) {
         this.importActivitiesService = importActivitiesService;
+    }
+
+
+    private static class Crumb extends DefaultCrumb {
+        @Override
+        public String getName(BreadcrumbContext context) {
+            return "Import activities";
+
+        }
+
+        @Override
+        public Map<String, String> getParameters(BreadcrumbContext context) {
+            return Collections.singletonMap("returnToPeriodId", context.getPeriod().getId().toString());
+        }
+    }
+
+    @Required
+    public void setPeriodDao(final PeriodDao periodDao) {
+        this.periodDao = periodDao;
     }
 }
--- src/main/java/edu/northwestern/bioinformatics/studycalendar/web/template/NewActivityController.java
+++ src/main/java/edu/northwestern/bioinformatics/studycalendar/web/template/NewActivityController.java
@@ -1,25 +1,30 @@
 package edu.northwestern.bioinformatics.studycalendar.web.template;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.bind.ServletRequestDataBinder;
-import org.springframework.validation.BindException;
-import org.springframework.beans.factory.annotation.Required;
-import org.springframework.ui.ModelMap;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
-import java.util.HashMap;
-
 import edu.northwestern.bioinformatics.studycalendar.dao.ActivityDao;
+import edu.northwestern.bioinformatics.studycalendar.dao.PeriodDao;
 import edu.northwestern.bioinformatics.studycalendar.dao.SourceDao;
-import edu.northwestern.bioinformatics.studycalendar.domain.*;
-import edu.northwestern.bioinformatics.studycalendar.utils.editors.ControlledVocabularyEditor;
+import edu.northwestern.bioinformatics.studycalendar.domain.Activity;
+import edu.northwestern.bioinformatics.studycalendar.domain.ActivityType;
+import edu.northwestern.bioinformatics.studycalendar.domain.Role;
 import edu.northwestern.bioinformatics.studycalendar.utils.accesscontrol.AccessControl;
+import edu.northwestern.bioinformatics.studycalendar.utils.breadcrumbs.BreadcrumbContext;
+import edu.northwestern.bioinformatics.studycalendar.utils.breadcrumbs.DefaultCrumb;
+import edu.northwestern.bioinformatics.studycalendar.utils.editors.ControlledVocabularyEditor;
 import edu.northwestern.bioinformatics.studycalendar.web.PscSimpleFormController;
 import edu.nwu.bioinformatics.commons.spring.ValidatableValidator;
-import gov.nih.nci.cabig.ctms.editors.DaoBasedEditor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindException;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.ServletRequestDataBinder;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Jaron Sampson
@@ -29,6 +34,7 @@ public class NewActivityController extends PscSimpleFormController {
     public static final String PSC_CREATE_NEW_ACTIVITY_SOURCE_NAME = "PSC - Manual Activity Creation";
 
     private ActivityDao activityDao;
+    private PeriodDao periodDao;
 
     private static final Logger log = LoggerFactory.getLogger(NewActivityController.class.getName());
     private SourceDao sourceDao;
@@ -39,20 +45,29 @@ public class NewActivityController extends PscSimpleFormController {
         setBindOnNewForm(true);
         setFormView("editActivity");
         setSuccessView("viewActivity");
+        setCrumb(new Crumb());
     }
 
-    protected Map<String, Object> referenceData(HttpServletRequest httpServletRequest) throws Exception {
+
+    @Override
+    protected Map referenceData(HttpServletRequest request, Object command, Errors errors) throws Exception {
         Map<String, Object> refdata = new HashMap<String, Object>();
         refdata.put("activityTypes", ActivityType.values());
         refdata.put("action", "New");
         refdata.put("sourceName", PSC_CREATE_NEW_ACTIVITY_SOURCE_NAME);
+
+        Integer periodId = ((NewActivityCommand) command).getReturnToPeriodId();
+
+        if (periodId != null) {
+            getControllerTools().addHierarchyToModel(periodDao.getById(periodId), refdata);
+        }
         return refdata;
     }
 
     protected void initBinder(HttpServletRequest request, ServletRequestDataBinder binder) throws Exception {
         super.initBinder(request, binder);
         binder.registerCustomEditor(ActivityType.class, "activityType",
-            new ControlledVocabularyEditor(ActivityType.class));
+                new ControlledVocabularyEditor(ActivityType.class));
     }
 
     protected ModelAndView onSubmit(Object oCommand, BindException errors) throws Exception {
@@ -68,7 +83,7 @@ public class NewActivityController extends PscSimpleFormController {
             return new ModelAndView(getSuccessView(), model);
         } else {
             ModelMap model = new ModelMap("id", command.getReturnToPeriodId())
-                .addObject("selectedActivity", activity.getId());
+                    .addObject("selectedActivity", activity.getId());
             return new ModelAndView("redirectToManagePeriod", model);
         }
     }
@@ -76,6 +91,18 @@ public class NewActivityController extends PscSimpleFormController {
     protected Object formBackingObject(HttpServletRequest request) throws Exception {
         NewActivityCommand command = new NewActivityCommand(activityDao);
         return command;
+    }
+
+
+    private static class Crumb extends DefaultCrumb {
+        public Crumb() {
+            super("Add activity");
+        }
+
+        @Override
+        public Map<String, String> getParameters(BreadcrumbContext context) {
+            return Collections.singletonMap("returnToPeriodId", context.getPeriod().getId().toString());
+        }
     }
 
     ////// CONFIGURATION
@@ -87,5 +114,10 @@ public class NewActivityController extends PscSimpleFormController {
 
     public void setSourceDao(SourceDao sourceDao) {
         this.sourceDao = sourceDao;
+    }
+
+    @Required
+    public void setPeriodDao(final PeriodDao periodDao) {
+        this.periodDao = periodDao;
     }
 }
