4e097bb0591ed481c73da1bdabe3779ff112462e
Saurabh Agrawal <saurabh.agrawal@semanticbits.com>
2008-07-17 17:32:18 +0000

    #436 adding ant script for packaging grid services into a single war

--- src/main/java/edu/northwestern/bioinformatics/studycalendar/tools/PscDataSourceSelfDiscoveringPropertiesFactoryBean.java
+++ src/main/java/edu/northwestern/bioinformatics/studycalendar/tools/PscDataSourceSelfDiscoveringPropertiesFactoryBean.java
@@ -14,8 +14,19 @@ public class PscDataSourceSelfDiscoveringPropertiesFactoryBean extends DataSourc
     private static final String WEBSSO_SERVER_URL_PROPERTY = "webSSO.server.baseUrl";
     private static final String WEBSSO_SERVER_CERT_PROPERTY = "webSSO.server.trustCert";
     private static final String WEBSSO_CAS_URL_PROPERTY = "webSSO.cas.acegi.security.url";
+
+    //grid service related configuration
+    private static final String GRID_REGISTRATION_CONSUMER_URL = "grid.registrationconsumer.url";
+
+    private static final String GRID_REGISTRATION_CONSUMER_VALUE = "/psc-wsrf/services/cagrid/RegistrationConsumer";
+
+    private static final String GRID_STUDY_CONSUMER_URL = "grid.studyconsumer.url";
+
+    private static final String GRID_STUDY_CONSUMER_VALUE = "/psc-wsrf/services/cagrid/StudyConsumer";
+
+    //grid.registrationconsumer.url=/wsrf/services/cagrid/RegistrationConsumer
     private static final String[] WEBSSO_PROPERTIES = {
-        WEBSSO_CAS_URL_PROPERTY, WEBSSO_SERVER_URL_PROPERTY, WEBSSO_SERVER_CERT_PROPERTY
+            WEBSSO_CAS_URL_PROPERTY, WEBSSO_SERVER_URL_PROPERTY, WEBSSO_SERVER_CERT_PROPERTY
     };
 
     @Override
@@ -25,6 +36,10 @@ public class PscDataSourceSelfDiscoveringPropertiesFactoryBean extends DataSourc
         for (String webssoProperty : WEBSSO_PROPERTIES) {
             setPropertyDefault(webssoProperty, "If you are using CCTS-SSO, please set " + webssoProperty);
         }
+
+        setPropertyDefault(GRID_STUDY_CONSUMER_URL, GRID_STUDY_CONSUMER_VALUE);
+        setPropertyDefault(GRID_REGISTRATION_CONSUMER_URL, GRID_REGISTRATION_CONSUMER_VALUE);
+
     }
 
     private void setPropertyDefault(String propertyName, String defaultValue) {
