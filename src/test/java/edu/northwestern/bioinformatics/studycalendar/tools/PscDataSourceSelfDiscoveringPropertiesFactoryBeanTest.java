1260c1b5592fbf2fb133ca20867f09c1bc5dff0b
Saurabh Agrawal <saurabh.agrawal@semanticbits.com>
2008-07-25 20:14:16 +0000

    #436 changing the default url of grid services from "psc-wsrf" to "wsrf-psc"

--- src/test/java/edu/northwestern/bioinformatics/studycalendar/tools/PscDataSourceSelfDiscoveringPropertiesFactoryBeanTest.java
+++ src/test/java/edu/northwestern/bioinformatics/studycalendar/tools/PscDataSourceSelfDiscoveringPropertiesFactoryBeanTest.java
@@ -26,10 +26,10 @@ public class PscDataSourceSelfDiscoveringPropertiesFactoryBeanTest extends TestC
 
 
         assertNotNull("registration consumer grid url can not be null", properties.getProperty("grid.registrationconsumer.url"));
-        assertEquals("registration consumer grid url value can not be null", "/psc-wsrf/services/cagrid/RegistrationConsumer", properties.getProperty("grid.registrationconsumer.url"));
+        assertEquals("registration consumer grid url value can not be null", "/wsrf-psc/services/cagrid/RegistrationConsumer", properties.getProperty("grid.registrationconsumer.url"));
 
         assertNotNull("study consumer grid url can not be null", properties.getProperty("grid.studyconsumer.url"));
-        assertEquals("study consumer grid url value can not be null", "/psc-wsrf/services/cagrid/StudyConsumer", properties.getProperty("grid.studyconsumer.url"));
+        assertEquals("study consumer grid url value can not be null", "/wsrf-psc/services/cagrid/StudyConsumer", properties.getProperty("grid.studyconsumer.url"));
 
 
     }
