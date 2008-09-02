91073b989eda958a38024a292d8a3ef5be95b7c0
Rhett Sutphin <r-sutphin@northwestern.edu>
2008-09-02 16:13:21 +0000

    Remove unused, incorrectly implemented getDayRange method

--- src/main/java/edu/northwestern/bioinformatics/studycalendar/domain/Period.java
+++ src/main/java/edu/northwestern/bioinformatics/studycalendar/domain/Period.java
@@ -1,18 +1,28 @@
 package edu.northwestern.bioinformatics.studycalendar.domain;
 
+import edu.northwestern.bioinformatics.studycalendar.StudyCalendarValidationException;
 import edu.northwestern.bioinformatics.studycalendar.utils.DayRange;
 import edu.northwestern.bioinformatics.studycalendar.utils.DefaultDayRange;
-import edu.northwestern.bioinformatics.studycalendar.StudyCalendarValidationException;
 import edu.nwu.bioinformatics.commons.ComparisonUtils;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
 
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * @author Moses Hohman
@@ -86,19 +96,6 @@ public class Period extends PlanTreeOrderedInnerNode<StudySegment, PlannedActivi
     @Transient
     public String getDisplayNameWithActivities() {
         return getDisplayName()+ " ("+getChildren().size()+" activities)";
-    }
-
-    @Transient
-    public List<DayRange> getDayRanges() {
-        List<DayRange> ranges = new ArrayList<DayRange>(getRepetitions());
-        while (ranges.size() < getRepetitions()) {
-            int rep = ranges.size();
-            Integer dayCount = getDuration().getDays();
-            int repStartDay = getStartDay() + rep * dayCount;
-            int durationUnit = getDuration().getUnit().inDays();
-            ranges.add(new DefaultDayRange(repStartDay, repStartDay + dayCount - durationUnit));
-        }
-        return ranges;
     }
 
     @Transient
