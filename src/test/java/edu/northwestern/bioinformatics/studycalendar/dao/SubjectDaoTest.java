2303b09a6353b555b74b2500f95e2635cd599eb7
Jalpa Patel <jalpa-patel@northwestern.edu>
2008-09-05 16:19:55 +0000

    Add Tests for Delete Subject

--- src/test/java/edu/northwestern/bioinformatics/studycalendar/dao/SubjectDaoTest.java
+++ src/test/java/edu/northwestern/bioinformatics/studycalendar/dao/SubjectDaoTest.java
@@ -16,6 +16,7 @@ public class SubjectDaoTest extends ContextDaoTestCase<SubjectDao> {
     private SiteDao siteDao = (SiteDao) getApplicationContext().getBean("siteDao");
     private StudyDao studyDao = (StudyDao) getApplicationContext().getBean("studyDao");
     private AmendmentDao amendmentDao = (AmendmentDao) getApplicationContext().getBean("amendmentDao");
+    private StudySubjectAssignmentDao ssaDao = (StudySubjectAssignmentDao) getApplicationContext().getBean("studySubjectAssignmentDao");
 
     public void testGetAll() throws Exception {
         List<Subject> actual = getDao().getAll();
@@ -161,4 +162,21 @@ public class SubjectDaoTest extends ContextDaoTestCase<SubjectDao> {
         assertNotNull("No subject matched", subject);
         assertEquals("Wrong subject matched", -101, (int) subject.getId());
     }
+    // Becuase of Constraint @Cascade(value = {CascadeType.ALL, CascadeType.DELETE_ORPHAN}) in Subject,
+    // the deletion deletes subject and it's referred children
+    public void testDeleteSubjectWithSubjectAssigment() throws Exception {
+        Subject subject = getDao().getById(-100);
+        getDao().delete(subject);
+        Subject subject1 = getDao().getById(-100);
+        assertNull("Subject is null, althouth it shoudln't be ", subject1);
+        
+    }
+    public void testDeleteSubject() throws Exception {
+        Subject subject = getDao().getById(-101);
+        getDao().delete(subject);
+        Subject subject1 = getDao().getById(-101);
+        assertNull("Subject is not null, althouth it shoudl be ", subject1);
+    }
+
+  
 }
