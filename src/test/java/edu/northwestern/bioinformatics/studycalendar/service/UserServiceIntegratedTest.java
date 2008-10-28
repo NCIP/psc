8ce7a6fca2dfb615075dac0b6140a89a5967a77b
Rhett Sutphin <r-sutphin@northwestern.edu>
2008-10-28 21:48:16 +0000

    Ensure that creating a user with roles & site associations all at once works.  Related to #516.

--- src/test/java/edu/northwestern/bioinformatics/studycalendar/service/UserServiceIntegratedTest.java
+++ src/test/java/edu/northwestern/bioinformatics/studycalendar/service/UserServiceIntegratedTest.java
@@ -1,16 +1,18 @@
 package edu.northwestern.bioinformatics.studycalendar.service;
 
-import edu.northwestern.bioinformatics.studycalendar.testing.StudyCalendarDbTestCase;
-import edu.northwestern.bioinformatics.studycalendar.domain.User;
+import edu.northwestern.bioinformatics.studycalendar.dao.SiteDao;
+import edu.northwestern.bioinformatics.studycalendar.dao.UserDao;
 import edu.northwestern.bioinformatics.studycalendar.domain.Fixtures;
 import edu.northwestern.bioinformatics.studycalendar.domain.Role;
-import edu.northwestern.bioinformatics.studycalendar.dao.UserDao;
-
+import edu.northwestern.bioinformatics.studycalendar.domain.Site;
+import edu.northwestern.bioinformatics.studycalendar.domain.User;
+import edu.northwestern.bioinformatics.studycalendar.testing.StudyCalendarDbTestCase;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
+import java.util.Set;
 
 /**
  * @author Rhett Sutphin
@@ -30,6 +32,7 @@ public class UserServiceIntegratedTest extends StudyCalendarDbTestCase {
         getTransactionManager().rollback(transaction);
         // CSM seems to use its own transaction, or something
         getJdbcTemplate().execute("DELETE from csm_user");
+        getJdbcTemplate().execute("DELETE from csm_protection_group");
         super.tearDown();
     }
 
@@ -54,11 +57,42 @@ public class UserServiceIntegratedTest extends StudyCalendarDbTestCase {
         assertNotNull(reloaded);
     }
 
+    public void testUserCreationWithSiteBasedRoleWorks() throws Exception {
+        {
+            Integer siteId;
+            Site site = Fixtures.createSite("NU");
+            getSiteService().createOrUpdateSite(site);
+            siteId = site.getId();
+            assertNotNull(siteId);
+
+            Site nu = getSiteDao().getById(siteId);
+            User user = Fixtures.createUser("joe", Role.SUBJECT_COORDINATOR);
+            user.getUserRole(Role.SUBJECT_COORDINATOR).addSite(nu);
+            getUserService().saveUser(user, "alfalfa", "user@email.com");
+        }
+
+        User reloaded = getUserDao().getByName("joe");
+        assertNotNull(reloaded);
+        assertNotNull(reloaded.getUserRole(Role.SUBJECT_COORDINATOR));
+        Set<Site> actualSubjCoordSites = reloaded.getUserRole(Role.SUBJECT_COORDINATOR).getSites();
+        assertEquals("Site not assoc. with role", 1, actualSubjCoordSites.size());
+        assertEquals("Wrong site assoc. with role", "NU",
+            actualSubjCoordSites.iterator().next().getName());
+    }
+
     private UserDao getUserDao() {
         return (UserDao) getApplicationContext().getBean("userDao");
     }
 
     private UserService getUserService() {
         return (UserService) getApplicationContext().getBean("userService");
+    }
+
+    private SiteDao getSiteDao() {
+        return (SiteDao) getApplicationContext().getBean("siteDao");
+    }
+
+    private SiteService getSiteService() {
+        return (SiteService) getApplicationContext().getBean("siteService");
     }
 }
