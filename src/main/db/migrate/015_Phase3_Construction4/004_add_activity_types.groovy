3ee3aba03ebd26128bda02351c7b2e881ab69f62
Rhett Sutphin <r-sutphin@northwestern.edu>
2008-11-11 16:49:16 +0000

    Ensure that database remains consistent when migrating down and then back up in the presence of custom activity types

--- src/main/db/migrate/015_Phase3_Construction4/004_add_activity_types.groovy
+++ src/main/db/migrate/015_Phase3_Construction4/004_add_activity_types.groovy
@@ -1,13 +1,13 @@
 class AddActivityTypes extends edu.northwestern.bioinformatics.bering.Migration {
-	void up() {
-	    addColumn("activity_types", "grid_id", "string", limit: 255)
+    void up() {
+        addColumn("activity_types", "grid_id", "string", limit: 255)
 
         execute("ALTER TABLE activity_types ADD CONSTRAINT un_activity_types_grid_id UNIQUE (grid_id)")
-		execute("insert into activity_types (id, version, name, grid_id) values ('1', '0', 'Disease Measure', '1')");
-		execute("insert into activity_types (id, version, name, grid_id) values ('2', '0', 'Intervention', '2')");
-		execute("insert into activity_types (id, version, name, grid_id) values ('3', '0', 'Lab Test', '3')");
-		execute("insert into activity_types (id, version, name, grid_id) values ('4', '0', 'Procedure', '4')");
-		execute("insert into activity_types (id, version, name, grid_id) values ('5', '0', 'Other', '5')");
+        execute("insert into activity_types (id, version, name, grid_id) values ('1', '0', 'Disease Measure', '1')");
+        execute("insert into activity_types (id, version, name, grid_id) values ('2', '0', 'Intervention', '2')");
+        execute("insert into activity_types (id, version, name, grid_id) values ('3', '0', 'Lab Test', '3')");
+        execute("insert into activity_types (id, version, name, grid_id) values ('4', '0', 'Procedure', '4')");
+        execute("insert into activity_types (id, version, name, grid_id) values ('5', '0', 'Other', '5')");
 
 
         if (databaseMatches('oracle')) {
@@ -19,11 +19,12 @@ class AddActivityTypes extends edu.northwestern.bioinformatics.bering.Migration 
             execute("SELECT setval('activity_types_id_seq', 5)");
         }
 
+        execute("UPDATE activities SET activity_type_id=5 WHERE activity_type_id > 5")
         execute("ALTER TABLE activities ADD CONSTRAINT fk_activ_type_id FOREIGN KEY (activity_type_id) REFERENCES activity_types")
-	}
+    }
 
-	void down() {
-	    execute("ALTER TABLE activities DROP CONSTRAINT fk_activ_type_id")
+    void down() {
+        execute("ALTER TABLE activities DROP CONSTRAINT fk_activ_type_id")
         if (databaseMatches('oracle')) {
             execute("""\
                 alter sequence seq_activity_types_id increment by -5;
@@ -31,8 +32,8 @@ class AddActivityTypes extends edu.northwestern.bioinformatics.bering.Migration 
         } else if (databaseMatches('postgresql')){
             execute("SELECT setval('activity_types_id_seq', 1)");
         }
-	    execute("alter table activity_types DROP CONSTRAINT un_activity_types_grid_id")
-	    removeColumn("activity_types", "grid_id")
-		execute("delete from activity_types");
-	}
+        execute("alter table activity_types DROP CONSTRAINT un_activity_types_grid_id")
+        removeColumn("activity_types", "grid_id")
+        execute("delete from activity_types");
+    }
 }
