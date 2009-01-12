948dee4999de39101d2d04b3cdc077f3f3ec1c59
Nataliya Shurupova <n-shurupova@northwestern.edu>
2009-01-12 20:47:44 +0000

    Backport revision 3399:  Fix for bug #578 - Making space an illegal character for population's abbreviation property. Handled for Add/Edit population page and in .xml for importing the template.

--- src/main/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/PopulationXmlSerializer.java
+++ src/main/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/PopulationXmlSerializer.java
@@ -4,12 +4,15 @@ import edu.northwestern.bioinformatics.studycalendar.dao.PopulationDao;
 import edu.northwestern.bioinformatics.studycalendar.domain.Population;
 import edu.northwestern.bioinformatics.studycalendar.domain.Study;
 import edu.northwestern.bioinformatics.studycalendar.xml.AbstractStudyCalendarXmlSerializer;
+import edu.northwestern.bioinformatics.studycalendar.StudyImportException;
 import org.dom4j.Element;
+import org.apache.commons.lang.StringUtils;
 
 public class PopulationXmlSerializer extends AbstractStudyCalendarXmlSerializer<Population> implements StatefulTemplateXmlSerializer<Population> {
     public static final String ABBREVIATION = "abbreviation";
     public static final String NAME = "name";
     public static final String POPULATION = "population";
+    private static final String POPULATION_ABBREVIATION_IS_INVALID_MESSAGE = "Population's abbreviation '%s' is invalid. Please make sure abbreviation don't contain spaces and less then 5 characters long.";
 
     private PopulationDao populationDao;
     private Study study;
@@ -22,6 +25,7 @@ public class PopulationXmlSerializer extends AbstractStudyCalendarXmlSerializer<
 
     public Population readElement(Element element) {
         String abbreviation = element.attributeValue(ABBREVIATION);
+        validateElement(abbreviation);
         Population population = load(abbreviation);
         if (population == null) {
             population = new Population();
@@ -47,4 +51,22 @@ public class PopulationXmlSerializer extends AbstractStudyCalendarXmlSerializer<
     public void setStudy(Study study) {
         this.study = study;
     }
+
+    private void validateElement(String abbreviation) {
+        StringBuffer errorMessageBuffer = new StringBuffer("");
+
+        if (abbreviation != null && (abbreviation.contains(" ") || abbreviation.length() > 5)) {
+            String errorMessage = String.format(POPULATION_ABBREVIATION_IS_INVALID_MESSAGE, abbreviation);
+            errorMessageBuffer.append(errorMessage);
+        }
+
+        if (!StringUtils.isEmpty(errorMessageBuffer.toString())) {
+            log.error(errorMessageBuffer.toString());
+
+            StudyImportException studyImportException = new StudyImportException(errorMessageBuffer.toString());
+
+            throw studyImportException;
+        }
+    }
+
 }
