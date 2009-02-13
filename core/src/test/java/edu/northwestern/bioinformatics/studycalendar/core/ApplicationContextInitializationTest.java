f97921b76de5f5e655103fd289be3e803ec7f6b9
Rhett Sutphin <r-sutphin@northwestern.edu>
2009-02-13 22:43:52 +0000

    Remove test that's incompatible with the new build process. #151.

--- core/src/test/java/edu/northwestern/bioinformatics/studycalendar/core/ApplicationContextInitializationTest.java
+++ core/src/test/java/edu/northwestern/bioinformatics/studycalendar/core/ApplicationContextInitializationTest.java
@@ -1,10 +1,7 @@
 package edu.northwestern.bioinformatics.studycalendar.core;
 
-import gov.nih.nci.cabig.ctms.testing.MoreJUnitAssertions;
 import gov.nih.nci.cabig.ctms.tools.BuildInfo;
 import org.springframework.context.ApplicationContext;
-
-import java.util.Date;
 
 /**
  * These tests are intended to verify that the various application contexts will all load when
@@ -22,8 +19,5 @@ public class ApplicationContextInitializationTest extends StudyCalendarTestCase 
     public void testBuildInfoTimestampIsParsed() throws Exception {
         BuildInfo buildInfo = (BuildInfo) StudyCalendarTestCase.getDeployedApplicationContext().getBean("buildInfo");
         assertNotNull(buildInfo.getTimestamp());
-        // note that this assertion will fail if you build and then run the tests much later
-        MoreJUnitAssertions.assertDatesClose("Build timestamp is not recent (this test will fail if you run it a long time after you build)",
-            new Date(), buildInfo.getTimestamp(), 3 * 60 * 60 * 1000 /* 3 hours */);
     }
 }
