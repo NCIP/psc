22d38e68788cd2f127356966f97654bb83bf259c
Nataliya Shurupova <n-shurupova@northwestern.edu>
2009-02-25 17:26:17 +0000

    Fixing invalid import for AmendmentXmlSerializerTest

--- src/test/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/AmendmentXmlSerializerTest.java
+++ src/test/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/AmendmentXmlSerializerTest.java
@@ -2,7 +2,7 @@ package edu.northwestern.bioinformatics.studycalendar.xml.writers;
 
 import edu.northwestern.bioinformatics.studycalendar.dao.delta.AmendmentDao;
 import edu.northwestern.bioinformatics.studycalendar.domain.Epoch;
-import static edu.northwestern.bioinformatics.studycalendar.core.Fixtures.*;
+import static edu.northwestern.bioinformatics.studycalendar.test.Fixtures.*;
 import edu.northwestern.bioinformatics.studycalendar.domain.PlannedCalendar;
 import edu.northwestern.bioinformatics.studycalendar.domain.Study;
 import edu.northwestern.bioinformatics.studycalendar.domain.delta.Add;
