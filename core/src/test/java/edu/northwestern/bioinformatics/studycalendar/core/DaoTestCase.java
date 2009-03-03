4dc998e8a49507f68abca2929bec6574cad66597
Rhett Sutphin <r-sutphin@northwestern.edu>
2009-03-03 20:09:47 +0000

    Pull dumpResults debugging method up from DaoTestCase to StudyCalendarDbTestCase

--- core/src/test/java/edu/northwestern/bioinformatics/studycalendar/core/DaoTestCase.java
+++ core/src/test/java/edu/northwestern/bioinformatics/studycalendar/core/DaoTestCase.java
@@ -1,24 +1,17 @@
 package edu.northwestern.bioinformatics.studycalendar.core;
 
-import edu.nwu.bioinformatics.commons.StringUtils;
 import edu.northwestern.bioinformatics.studycalendar.database.StudyCalendarDbTestCase;
-import org.springframework.jdbc.core.ColumnMapRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.context.ApplicationContext;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.orm.hibernate3.support.OpenSessionInViewInterceptor;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.context.ApplicationContext;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author Rhett Sutphin
@@ -93,46 +86,6 @@ public abstract class DaoTestCase extends StudyCalendarDbTestCase {
         List<OpenSessionInViewInterceptor> interceptors = interceptors();
         Collections.reverse(interceptors);
         return interceptors;
-    }
-
-    @SuppressWarnings({ "unchecked" })
-    protected final void dumpResults(String sql) {
-        List<Map<String, String>> rows = new JdbcTemplate(getDataSource()).query(
-            sql, new ColumnMapRowMapper() {
-            @Override
-            protected Object getColumnValue(ResultSet rs, int index) throws SQLException {
-                Object value = super.getColumnValue(rs, index);
-                return value == null ? "null" : value.toString();
-            }
-        }
-        );
-        StringBuffer dump = new StringBuffer(sql).append('\n');
-        if (rows.size() > 0) {
-            Map<String, Integer> colWidths = new HashMap<String, Integer>();
-            for (String colName : rows.get(0).keySet()) {
-                colWidths.put(colName, colName.length());
-                for (Map<String, String> row : rows) {
-                    colWidths.put(colName, Math.max(colWidths.get(colName), row.get(colName).length()));
-                }
-            }
-
-            for (String colName : rows.get(0).keySet()) {
-                StringUtils.appendWithPadding(colName, colWidths.get(colName), false, dump);
-                dump.append("   ");
-            }
-            dump.append('\n');
-
-            for (Map<String, String> row : rows) {
-                for (String colName : row.keySet()) {
-                    StringUtils.appendWithPadding(row.get(colName), colWidths.get(colName), false, dump);
-                    dump.append(" | ");
-                }
-                dump.append('\n');
-            }
-        }
-        dump.append("  ").append(rows.size()).append(" row").append(rows.size() != 1 ? "s\n" : "\n");
-
-        System.out.print(dump);
     }
 
     @Override
--- database/src/test/java/edu/northwestern/bioinformatics/studycalendar/database/StudyCalendarDbTestCase.java
+++ database/src/test/java/edu/northwestern/bioinformatics/studycalendar/database/StudyCalendarDbTestCase.java
@@ -1,5 +1,6 @@
 package edu.northwestern.bioinformatics.studycalendar.database;
 
+import edu.nwu.bioinformatics.commons.StringUtils;
 import edu.nwu.bioinformatics.commons.testing.DbTestCase;
 import edu.nwu.bioinformatics.commons.testing.HsqlDataTypeFactory;
 import gov.nih.nci.cabig.ctms.audit.domain.DataAuditInfo;
@@ -8,14 +9,18 @@ import org.dbunit.dataset.datatype.IDataTypeFactory;
 import org.dbunit.ext.oracle.OracleDataTypeFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ColumnMapRowMapper;
 import org.springframework.jdbc.core.ConnectionCallback;
+import org.springframework.jdbc.core.JdbcTemplate;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Rhett Sutphin
@@ -53,5 +58,51 @@ public abstract class StudyCalendarDbTestCase extends DbTestCase {
         else {
             return new DefaultDataTypeFactory();
         }
+    }
+
+    /**
+     * Prints the (tabular) result of executing the given SELECT at the point
+     * where it is invoked.  Useful for debugging apparently data-related problems
+     * with your tests, since dbunit wipes out data post execution.
+     * @param sql
+     */
+    @SuppressWarnings({ "unchecked" })
+    protected final void dumpResults(String sql) {
+        List<Map<String, String>> rows = new JdbcTemplate(getDataSource()).query(
+            sql, new ColumnMapRowMapper() {
+            @Override
+            protected Object getColumnValue(ResultSet rs, int index) throws SQLException {
+                Object value = super.getColumnValue(rs, index);
+                return value == null ? "null" : value.toString();
+            }
+        }
+        );
+        StringBuffer dump = new StringBuffer(sql).append('\n');
+        if (rows.size() > 0) {
+            Map<String, Integer> colWidths = new HashMap<String, Integer>();
+            for (String colName : rows.get(0).keySet()) {
+                colWidths.put(colName, colName.length());
+                for (Map<String, String> row : rows) {
+                    colWidths.put(colName, Math.max(colWidths.get(colName), row.get(colName).length()));
+                }
+            }
+
+            for (String colName : rows.get(0).keySet()) {
+                StringUtils.appendWithPadding(colName, colWidths.get(colName), false, dump);
+                dump.append("   ");
+            }
+            dump.append('\n');
+
+            for (Map<String, String> row : rows) {
+                for (String colName : row.keySet()) {
+                    StringUtils.appendWithPadding(row.get(colName), colWidths.get(colName), false, dump);
+                    dump.append(" | ");
+                }
+                dump.append('\n');
+            }
+        }
+        dump.append("  ").append(rows.size()).append(" row").append(rows.size() != 1 ? "s\n" : "\n");
+
+        System.out.print(dump);
     }
 }
