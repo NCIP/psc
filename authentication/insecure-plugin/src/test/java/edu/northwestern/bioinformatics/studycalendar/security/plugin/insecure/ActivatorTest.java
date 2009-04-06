b8883031dfb15fecabebf8319c7d14ae6a59a444
Rhett Sutphin <r-sutphin@northwestern.edu>
2009-04-06 20:06:41 +0000

    Unit test for insecure-plugin activator.  #597.

---
+++ authentication/insecure-plugin/src/test/java/edu/northwestern/bioinformatics/studycalendar/security/plugin/insecure/ActivatorTest.java
@@ -0,0 +1,74 @@
+package edu.northwestern.bioinformatics.studycalendar.security.plugin.insecure;
+
+import edu.northwestern.bioinformatics.studycalendar.security.plugin.AuthenticationSystem;
+import junit.framework.TestCase;
+import org.osgi.framework.ServiceFactory;
+import org.osgi.framework.ServiceRegistration;
+import org.springframework.osgi.mock.MockBundleContext;
+
+import java.util.Dictionary;
+
+/**
+ * @author Rhett Sutphin
+ */
+@SuppressWarnings({ "RawUseOfParameterizedType" })
+public class ActivatorTest extends TestCase {
+    private ServiceRegistrationRecordingBundleContext bundleContext;
+    private Activator activator;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        bundleContext = new ServiceRegistrationRecordingBundleContext();
+        activator = new Activator();
+    }
+
+    public void testRegisteredServiceIsAFactory() throws Exception {
+        activator.start(bundleContext);
+        assertTrue("Service not a factory", bundleContext.getLastService() instanceof ServiceFactory);
+        assertTrue("Factory is for wrong type",
+            ((ServiceFactory) bundleContext.getLastService()).getService(null, null) instanceof InsecureAuthenticationSystem);
+    }
+
+    public void testRegisteredServicePropertiesIncludeName() throws Exception {
+        activator.start(bundleContext);
+        assertEquals(
+            "Wrong name",
+            bundleContext.getLastServiceProperties().get(AuthenticationSystem.ServiceKeys.NAME),
+            "Insecure");
+    }
+
+    public void testRegisteredServicePropertiesIncludeBehavior() throws Exception {
+        activator.start(bundleContext);
+        assertEquals(
+            "Wrong behavior",
+            bundleContext.getLastServiceProperties().get(AuthenticationSystem.ServiceKeys.BEHAVIOR_DESCRIPTION),
+            new InsecureAuthenticationSystem().behaviorDescription());
+    }
+
+    private static class ServiceRegistrationRecordingBundleContext extends MockBundleContext {
+        private String lastServiceClass;
+        private Object lastService;
+        private Dictionary lastServiceProperties;
+
+        @Override
+        public ServiceRegistration registerService(String interfaceClass, Object service, Dictionary properties) {
+            this.lastServiceClass = interfaceClass;
+            this.lastService = service;
+            this.lastServiceProperties = properties;
+            return super.registerService(interfaceClass, service, properties);
+        }
+
+        public String getLastServiceClass() {
+            return lastServiceClass;
+        }
+
+        public Object getLastService() {
+            return lastService;
+        }
+
+        public Dictionary getLastServiceProperties() {
+            return lastServiceProperties;
+        }
+    }
+}
