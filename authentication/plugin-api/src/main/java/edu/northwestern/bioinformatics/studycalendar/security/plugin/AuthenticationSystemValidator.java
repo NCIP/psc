81c7fe124823330fe49e170ae570f0f112c5144b
Rhett Sutphin <r-sutphin@northwestern.edu>
2009-04-06 20:54:24 +0000

    Move required field validation into a static utility class so it can be used elsewhere.  #597.

---
+++ authentication/plugin-api/src/main/java/edu/northwestern/bioinformatics/studycalendar/security/plugin/AuthenticationSystemValidator.java
@@ -0,0 +1,40 @@
+package edu.northwestern.bioinformatics.studycalendar.security.plugin;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * @author Rhett Sutphin
+*/
+public class AuthenticationSystemValidator {
+    public static void validateRequiredElementsCreated(
+        AuthenticationSystem system
+    ) throws AuthenticationSystemInitializationFailure {
+        List<String> missing = new ArrayList<String>(3);
+        if (system.authenticationManager() == null) {
+            missing.add("authenticationManager()");
+        }
+        if (system.entryPoint() == null) {
+            missing.add("entryPoint()");
+        }
+        if (system.logoutFilter() == null) {
+            missing.add("logoutFilter()");
+        }
+        if (!missing.isEmpty()) {
+            String list = missing.get(missing.size() - 1);
+            if (missing.size() >= 2) {
+                list = String.format(
+                    "%s or %s",
+                    StringUtils.join(missing.subList(0, missing.size() - 1).iterator(), ", "),
+                    list);
+            }
+            throw new AuthenticationSystemInitializationFailure(
+                "%s must not return null from %s", system.getClass().getSimpleName(), list);
+        }
+    }
+
+    // static class
+    private AuthenticationSystemValidator() { }
+}
