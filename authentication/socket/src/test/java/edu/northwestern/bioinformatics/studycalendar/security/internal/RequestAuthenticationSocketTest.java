5e3dccc69e771bf90eb62d97b35511b623e1237c
Jalpa Patel <jalpa-patel@northwestern.edu>
2009-06-30 17:06:21 +0000

    Add RequestAuthenticationSocket for attaching authentication to the request instead of using SecurityConextHolerAwareRequestFilter which wraps the request. Related #682.

---
+++ authentication/socket/src/test/java/edu/northwestern/bioinformatics/studycalendar/security/internal/RequestAuthenticationSocketTest.java
@@ -0,0 +1,32 @@
+package edu.northwestern.bioinformatics.studycalendar.security.internal;
+
+import edu.northwestern.bioinformatics.studycalendar.core.StudyCalendarTestCase;
+import edu.northwestern.bioinformatics.studycalendar.core.accesscontrol.SecurityContextHolderTestHelper;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import javax.servlet.FilterChain;
+
+/**
+ * @author Jalpa Patel
+ */
+public class RequestAuthenticationSocketTest extends StudyCalendarTestCase {
+    private RequestAuthenticationSocket filter;
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+    private FilterChain filterChain;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        request = new MockHttpServletRequest();
+        response = new MockHttpServletResponse();
+        filterChain = registerMockFor(FilterChain.class);
+        filter = new RequestAuthenticationSocket();
+    }
+
+    public void testAddAuthenticationToRequest() throws Exception {
+        SecurityContextHolderTestHelper.setSecurityContext("user" , "user");
+        filter.doFilter(request, response, filterChain);
+        assertNotNull(request.getAttribute("authentication"));
+    }
+}
