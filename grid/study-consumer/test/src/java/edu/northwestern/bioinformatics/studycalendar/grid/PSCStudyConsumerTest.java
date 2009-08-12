aacd5a7afeeb5d9f33ba1c0347d7e6f967fec1a6
Kruttik Aggarwal <kruttik.aggarwal@semanticbits.com>
2009-08-12 22:27:29 +0000

    ->changed the testing inheritance. Extended the DaoTestCase instead of DBTestCase. This enabled the test case to run the general testing framework. ->Commented the init methods. ->Needs to be rewritten or refactored to function correctly. ->refactored the way data set was being fetched. Using the classpath to resolve the file instead of hard code absolute path.

--- grid/study-consumer/test/src/java/edu/northwestern/bioinformatics/studycalendar/grid/PSCStudyConsumerTest.java
+++ grid/study-consumer/test/src/java/edu/northwestern/bioinformatics/studycalendar/grid/PSCStudyConsumerTest.java
@@ -20,13 +20,15 @@ import org.dbunit.operation.DatabaseOperation;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import edu.northwestern.bioinformatics.studycalendar.core.DaoTestCase;
+
 import java.io.*;
 import java.util.Date;
 
 /**
  * @author <a href="mailto:joshua.phillips@semanticbits.com>Joshua Phillips</a>
  */
-public class PSCStudyConsumerTest extends DBTestCase {
+public class PSCStudyConsumerTest extends DaoTestCase {
 
 
     private String clientConfigFile;
@@ -42,21 +44,21 @@ public class PSCStudyConsumerTest extends DBTestCase {
 
     private ApplicationContext applicationContext;
 
-    private void init() {
+//    private void init() {
 
-        applicationContext = new ClassPathXmlApplicationContext(new String[]{
-                // "classpath:applicationContext.xml",
-                "classpath:applicationContext-studyConsumer-grid.xml"});
-        pscStudyConsumer = (PSCStudyConsumer) applicationContext.getBean("studyConsumer");
-
-        clientConfigFile = System.getProperty("psc.test.clientConfigFile",
-                "gov/nih/nci/ccts/grid/client/client-config.wsdd");
-
-        regFile = System.getProperty("psc.test.sampleStudyFile",
-                "grid/study-consumer/test/resources/SampleStudyMessage.xml");
-
-        serviceUrl = System.getProperty("psc.test.serviceUrl",
-                "http://localhost:8080/wsrf/services/cagrid/StudyConsumer");
+//        applicationContext = new ClassPathXmlApplicationContext(new String[]{
+//                // "classpath:applicationContext.xml",
+//                "classpath:applicationContext-studyConsumer-grid.xml"});
+//        pscStudyConsumer = (PSCStudyConsumer) applicationContext.getBean("studyConsumer");
+//
+//        clientConfigFile = System.getProperty("psc.test.clientConfigFile",
+//                "gov/nih/nci/ccts/grid/client/client-config.wsdd");
+//
+//        regFile = System.getProperty("psc.test.sampleStudyFile",
+//                "grid/study-consumer/test/resources/SampleStudyMessage.xml");
+//
+//        serviceUrl = System.getProperty("psc.test.serviceUrl",
+//                "http://localhost:8080/wsrf/services/cagrid/StudyConsumer");
 
 //        serviceUrl = System.getProperty("psc.test.serviceUrl",
 //                "http://10.10.10.2:9012/wsrf/services/cagrid/StudyConsumer");
@@ -65,46 +67,47 @@ public class PSCStudyConsumerTest extends DBTestCase {
 //                "http://cbvapp-d1017.nci.nih.gov:18080/psc-wsrf/services/cagrid/StudyConsumer");
 
 
-        String driver = System.getProperty("psc.test.db.driver", "org.postgresql.Driver");
+//        String driver = System.getProperty("psc.test.db.driver", "org.postgresql.Driver");
 
 //          String url = System.getProperty("psc.test.db.url", "jdbc:postgresql://cbiovdev5004.nci.nih.gov:5455/psc");
 //        String usr = System.getProperty("psc.test.db.usr", "pscdev");
 //        String pwd = System.getProperty("psc.test.db.pwd", "devpsc1234");
 
-        String url = System.getProperty("psc.test.db.url", "jdbc:postgresql:psc-test");
+//        String url = System.getProperty("psc.test.db.url", "jdbc:postgresql:psc-test");
 //        //  String url = System.getProperty("psc.test.db.url", "jdbc:postgresql://10.10.10.2:5432/psc");
-        String usr = System.getProperty("psc.test.db.usr", "psc");
-        String pwd = System.getProperty("psc.test.db.pwd", "psc");
-
-        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, driver);
-        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, url);
-        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, usr);
-        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, pwd);
-    }
+//        String usr = System.getProperty("psc.test.db.usr", "psc");
+//        String pwd = System.getProperty("psc.test.db.pwd", "psc");
+//
+//        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, driver);
+//        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, url);
+//        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, usr);
+//        System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, pwd);
+//    }
 
     @Override
     public void setUp() throws Exception {
         super.setUp();
-        DataAuditInfo.setLocal(new gov.nih.nci.cabig.ctms.audit.domain.DataAuditInfo("test", "localhost", new Date(), "/wsrf/services/cagrid/StudyConsumer"));
+        regFile = System.getProperty("psc.test.sampleRegistrationFile");
+        //DataAuditInfo.setLocal(new gov.nih.nci.cabig.ctms.audit.domain.DataAuditInfo("test", "localhost", new Date(), "/wsrf/services/cagrid/StudyConsumer"));
     }
+//
+//    @Override
+//    public void tearDown() throws Exception {
+//        super.tearDown();
+//        DataAuditInfo.setLocal(null);
+//
+//    }
 
-    @Override
-    public void tearDown() throws Exception {
-        super.tearDown();
-        DataAuditInfo.setLocal(null);
-
-    }
-
-    public PSCStudyConsumerTest() {
-        super();
-        init();
-
-    }
-
-    public PSCStudyConsumerTest(final String name) {
-        super(name);
-        init();
-    }
+//    public PSCStudyConsumerTest() {
+//        super();
+//        init();
+//
+//    }
+//
+//    public PSCStudyConsumerTest(final String name) {
+//        super(name);
+//        init();
+//    }
 
     @Override
     protected DatabaseOperation getSetUpOperation() throws Exception {
@@ -174,9 +177,9 @@ public class PSCStudyConsumerTest extends DBTestCase {
 
     }
 
-    public static void main(final String[] args) {
-        junit.textui.TestRunner.run(suite());
-    }
+//    public static void main(final String[] args) {
+//        junit.textui.TestRunner.run(suite());
+//    }
 
     public void testCommitRemote() throws Exception {
         try {
@@ -223,7 +226,13 @@ public class PSCStudyConsumerTest extends DBTestCase {
         try {
             InputStream config = Thread.currentThread().getContextClassLoader().getResourceAsStream(
                     "gov/nih/nci/ccts/grid/client/client-config.wsdd");
-            Reader reader = new FileReader(regFile);
+            Reader reader = null;
+            if (regFile != null){
+            	reader = new FileReader(regFile);
+            }else{
+            	reader = new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream(
+                    "SampleStudyMessage.xml"));
+            }
             studyDTO = (Study) gov.nih.nci.cagrid.common.Utils.deserializeObject(reader, Study.class, config);
         }
         catch (Exception e) {
@@ -234,8 +243,8 @@ public class PSCStudyConsumerTest extends DBTestCase {
         return studyDTO;
     }
 
-    public static Test suite() {
-        TestSuite suite = new TestSuite();
+//    public static Test suite() {
+//        TestSuite suite = new TestSuite();
 
         /*
            * NOTE: These tests CANNOT be run in succession because it will cause the maximum number of connections to be exceeded.
@@ -245,19 +254,22 @@ public class PSCStudyConsumerTest extends DBTestCase {
         // suite.addTest(new PSCStudyConsumerTest("testRollbackStudyLocal"));
         //suite.addTest(new PSCStudyConsumerTest("testCreateStudyRemote"));
 
-        suite.addTest(new PSCStudyConsumerTest("testCreateStudyLocal"));
-        return suite;
-    }
+//        suite.addTest(new PSCStudyConsumerTest("testCreateStudyLocal"));
+//        return suite;
+//    }
 
     @Override
     protected IDataSet getDataSet() throws Exception {
-        String fileName = "grid/study-consumer/test/resources/test_data.xml";
-        File testFile = new File(fileName);
-        if (!testFile.exists()) {
-            throw new RuntimeException(fileName + " not found.");
-        }
-
-        return new FlatXmlDataSet(new FileInputStream(testFile));
+//        String fileName = "grid/study-consumer/test/resources/test_data.xml";
+//        File testFile = new File(fileName);
+//        if (!testFile.exists()) {
+//            throw new RuntimeException(fileName + " not found.");
+//        }
+//
+//        return new FlatXmlDataSet(new FileInputStream(testFile));
+        
+        String fileName = "test_data.xml";
+        return new FlatXmlDataSet(Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName));
     }
 
 
