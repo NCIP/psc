3e67414c83491c1656fa058dfb348da8fc5bd90f
Jalpa Patel <jalpa-patel@northwestern.edu>
2009-08-25 01:37:57 +0000

    Add tests for ics calendar representation. Related to #738.

--- test/restful-api/src/spec/ruby/subject_centric_schedule_spec.rb
+++ test/restful-api/src/spec/ruby/subject_centric_schedule_spec.rb
@@ -1,59 +1,40 @@
 describe "/subjects/{subject-identifier}/schedules" do
   before do
-    begin
     #create site
     @site = PscTest::Fixtures.createSite("TestSite", "site")
     application_context['siteDao'].save( @site)
 
-    #create two studies with an amendment
-    @studies = [
-      @study1 = PscTest::Fixtures.createSingleEpochStudy("NU480", "Treatment", ["segment_A", "segment_B"].to_java(:String)), 
-      @study2 = PscTest::Fixtures.createSingleEpochStudy("ECOG170", "FollowUp", ["segment_A", "segment_B"].to_java(:String))
-    ]    
-    @amendment1 = PscTest::Fixtures.createAmendment("am1", PscTest.createDate(2008, 12, 10)) 
-    @amendment2 = PscTest::Fixtures.createAmendment("am2", PscTest.createDate(2008, 12, 15))
-    @study1.amendment = @amendment1 
-    @study2.amendment = @amendment2
-    @studies.each do |s|
-      application_context['studyService'].save(s)
+    # The released version of NU480
+    @nu480 = create_study 'NU480' do |s|
+      s.planned_calendar do |cal|
+        cal.epoch "Treatment" do |e|
+          e.study_segment "A" do |a|
+            a.period "P1", :start_day => 2, :duration => [4, :day], :repetitions => 2 do |p|
+              p.activity "Rituximab", 4
+              p.activity "Alcohol", 4
+            end
+          end
+        end
+      end
     end
     
-    #create period and link to studies
-    @periods = [
-      @period1 = PscTest::Fixtures.createPeriod("Period1", 2, 4, 2),
-      @period2 = PscTest::Fixtures.createPeriod("Period2", 2, 3, 1)
-    ]
-    @periods.each do |p|
-      application_context['periodDao'].save(p) 
+    # The released version of ECOG170
+    @ecog170 = create_study 'ECOG170' do |s|
+      s.planned_calendar do |cal|
+        cal.epoch "Followup" do |e|
+          e.study_segment "B" do |a|
+            a.period "P2", :start_day => 2, :duration => [3, :day], :repetitions => 1 do |p|
+              p.activity "Lab Test", 1
+              p.activity "Physical Test", 2
+            end
+          end
+        end
+      end
     end
-    @study1.plannedCalendar.epochs.first.studySegments.first.addPeriod(@period1)
-    @study2.plannedCalendar.epochs.first.studySegments.first.addPeriod(@period2)
-  
-    #create planned activity and link to studies
-    @source = PscTest::Fixtures.createSource("Malaria")
-    application_context['sourceDao'].save(@source)
-    @activityType = PscTest::Fixtures.createActivityType("Malaria Treatment")
-    application_context['activityTypeDao'].save(@activityType)
-    @activity = PscTest::Fixtures.createActivity("Initial Diagnosis", "diag1", @source, @activityType, "Stage 1 diagnosis for malaria")
-    application_context['activityDao'].save(@activity)
-    @planned_activities = [
-       @planned_activity1 = PscTest::Fixtures.createPlannedActivity(@activity, 4),
-       @planned_activity2 = PscTest::Fixtures.createPlannedActivity(@activity, 2)
-    ]
-    @planned_activities.each do |pa|
-      application_context['plannedActivityDao'].save(pa)
-    end
-    @period1.addPlannedActivity(@planned_activity1)
-    @period2.addPlannedActivity(@planned_activity2)
-   
-    @studies.each do |s|
-       application_context['studyService'].save(s)
-    end
-    
     #create a studysites
     @studySites = [       
-       @studySite1 = PscTest::Fixtures.createStudySite(@study1, @site),
-       @studySite2 = PscTest::Fixtures.createStudySite(@study2, @site)
+       @studySite1 = PscTest::Fixtures.createStudySite(@nu480, @site),
+       @studySite2 = PscTest::Fixtures.createStudySite(@ecog170, @site)
     ] 
     @studySites.each do |ss|
        application_context['studySiteDao'].save(ss)
@@ -61,8 +42,8 @@ describe "/subjects/{subject-identifier}/schedules" do
     
     #approve an existing amendment
     @approve_date = PscTest.createDate(2008, 12, 20)
-    @studySite1.approveAmendment(@amendment1, @approve_date)
-    @studySite2.approveAmendment(@amendment2, @approve_date)
+    @studySite1.approveAmendment(@nu480.amendment, @approve_date)
+    @studySite2.approveAmendment( @ecog170.amendment, @approve_date)
     application_context['studySiteDao'].save(@studySite1)
     application_context['studySiteDao'].save(@studySite2)    
     
@@ -70,59 +51,235 @@ describe "/subjects/{subject-identifier}/schedules" do
     @subject = PscTest::Fixtures.createSubject("ID001", "Alan", "Boyarski", PscTest.createDate(1983, 3, 23)) 
     
     #Assign studies to the subject coordinator
-    application_context['templateService'].assignTemplateToSubjectCoordinator(@study1, @site, erin)
-    application_context['templateService'].assignTemplateToSubjectCoordinator(@study2, @site, erin)
+    application_context['templateService'].assignTemplateToSubjectCoordinator(@nu480, @site, erin)
+    application_context['templateService'].assignTemplateToSubjectCoordinator(@ecog170, @site, erin)
             
     #create a study subject assignment
-    @studySegment1 = @study1.plannedCalendar.epochs.first.studySegments.first
-    @studySegment2 = @study2.plannedCalendar.epochs.first.studySegments.first
+    @studySegment1 = @nu480.plannedCalendar.epochs.first.studySegments.first
+    @studySegment2 = @ecog170.plannedCalendar.epochs.first.studySegments.first
     @studySubjectAssignment1 = application_context['subjectService'].assignSubject(@subject, @studySite1, @studySegment1, PscTest.createDate(2008, 12, 26) , "SS001", erin)
     application_context['studySubjectAssignmentDao'].save( @studySubjectAssignment1)
     @studySubjectAssignment2 = application_context['subjectService'].assignSubject(@subject, @studySite2, @studySegment2, PscTest.createDate(2008, 12, 28) , "SS002", erin)
     application_context['studySubjectAssignmentDao'].save( @studySubjectAssignment2)
-    
-    @scheduled_activity1 = @studySubjectAssignment1.scheduledCalendar.scheduledStudySegments.first.activities.first
-    @scheduled_activity2 = @studySubjectAssignment2.scheduledCalendar.scheduledStudySegments.first.activities.first
-    rescue Exception => e
-        application_context['databaseInitializer'].afterEach
-        raise e
-    end
   end
+  
   describe "GET" do
         
-    it "allows to get schedule for subject in xml representation to an authorized user" do
-      get "/subjects/ID001/schedules", :as => :erin
-      response.status_code.should == 200
-      response.status_message.should == "OK"
-      response.content_type.should == 'text/xml'
-      response.xml_attributes("subject-assignment", "site-name").should include("TestSite")
-      response.xml_attributes("subject-assignment", "study-name").should include("NU480")
-      response.xml_attributes("subject-assignment", "study-name").should include("ECOG170")
+    describe "xml" do
+      before do
+        get "/subjects/ID001/schedules", :as => :erin
+      end
+
+      it "is successful" do
+        response.should be_success
+      end
+
+      it "is XML" do
+        response.content_type.should == 'text/xml'
+      end
+
+      it "has the right number of activities" do
+        response.xml_elements('//scheduled-activity').should have(6).activities
+      end
+    end
+
+    describe "json" do
+      before do
+        get "/subjects/ID001/schedules.json", :as => :erin
+      end
       
+      it "is sucessful" do
+        response.should be_success
+      end
+      
+      it "is json" do
+        response.content_type.should == 'application/json'
+      end
+      
+      it "contains the right number of activities" do
+        response.json["days"].inject(0) { |sum, (_, day)| sum + day["activities"].size }.should == 6
+      end
+      
+      describe "the structure" do
+        describe "[days]" do
+          before do
+            @days = response.json["days"]
+          end
+          
+          it "has the correct days" do
+            @days.keys.sort.should == %w(2008-12-28 2008-12-29 2009-01-02)
+          end
+          
+          describe "[2008-12-29][activities][0]" do
+            before do
+              @activity = @days['2008-12-29']['activities'][0]
+            end
+
+            it "refers to the study" do
+              @activity["study"].should == "ECOG170"
+            end
+
+            it "refers to the segment" do
+              @activity["study_segment"].should == "Followup"
+            end
+
+            it "refers to the plan day" do
+              @activity["plan_day"].should == "3"
+            end
+
+            describe "[current_state]" do
+              it "has the name" do
+                @activity["current_state"]["name"].should == "scheduled"
+              end
+
+              it "has the date" do
+                @activity["current_state"]["date"].should == "2008-12-29"
+              end
+            end
+
+            describe "[activity]" do
+              it "has the name" do
+                @activity["activity"]["name"].should == "Physical Test"
+              end
+
+              it "has the type" do
+                @activity["activity"]["type"].should == "Other"
+              end
+            end
+
+            describe "[state_history]" do
+              it "has one item" do
+                @activity["state_history"].should have(1).item
+              end
+
+              it "has the current state" do
+                @activity["state_history"][0]["name"].should == "scheduled"
+                @activity["state_history"][0]["date"].should == "2008-12-29"
+              end
+            end
+          end
+        end
+        describe "[study_segments]" do
+          before do
+            response.json["study_segments"].should have(2).segment
+            @segment = response.json["study_segments"][0]
+          end
+
+          it "includes the name" do
+            @segment['name'].should == "Treatment"
+          end
+
+          describe "[range]" do
+            it "includes the start date" do
+              @segment["range"]["start_date"].should == "2008-12-26"
+            end
+            it "includes the stop date" do
+              @segment["range"]["stop_date"].should == "2009-01-02"
+            end
+          end
+
+          describe "[planned]" do
+            describe "[segment]" do
+              it "has the name" do
+                @segment["planned"]["segment"]["name"].should == "A"
+              end
+
+              it "has the ID" do
+                @segment["planned"]["segment"]["id"].should == @studySegment1.grid_id
+              end
+            end
+
+            describe "[epoch]" do
+              it "has the name" do
+                @segment["planned"]["epoch"]["name"].should == "Treatment"
+              end
+
+              it "has the ID" do
+                @segment["planned"]["epoch"]["id"].should == @studySegment1.epoch.grid_id
+              end
+            end
+
+            describe "[study]" do
+              it "has the assigned ident" do
+                @segment["planned"]["study"]["assigned_identifier"].should == "NU480"
+              end
+            end
+          end
+        end
+      end
     end
     
-    it "allows to get schedule for subject in JSON representation to an authorized user" do
-      get "/subjects/ID001/schedules.json", :as => :erin
-      response.status_code.should == 200
-      response.status_message.should == "OK"
-      response.content_type.should == 'application/json'
-      response.json["days"]["2008-12-29"]["activities"][0]["study"].should include("NU480")
-      response.json["days"]["2008-12-29"]["activities"][0]["study_segment"].should include("Treatment: segment_A")
-      response.json["days"]["2008-12-29"]["activities"][1]["study"].should include("ECOG170")
-      response.json["days"]["2008-12-29"]["activities"][1]["study_segment"].should include("FollowUp: segment_A")
-      response.json["days"]["2008-12-29"]["activities"][0]["plan_day"].should == 4
-      response.json["days"]["2008-12-29"]["activities"][0]["current_state"]["name"].should include("scheduled")
-      response.json["days"]["2008-12-29"]["activities"][0]["activity"]["name"].should include("Initial Diagnosis")
-      response.json["days"]["2008-12-29"]["activities"][0]["activity"]["type"].should include("Malaria Treatment")
-      response.json["study_segments"][0]["name"].should include("Treatment: segment_A")
-      response.json["study_segments"][0]["planned"]["segment"]["name"].should include("segment_A")
-      response.json["study_segments"][0]["planned"]["epoch"]["name"].should include("Treatment")
-      response.json["study_segments"][0]["planned"]["study"]["assigned_identifier"].should include("NU480")
-      response.json["study_segments"][1]["name"].should include("FollowUp: segment_A")
-      response.json["study_segments"][1]["planned"]["segment"]["name"].should include("segment_A")
-      response.json["study_segments"][1]["planned"]["epoch"]["name"].should include("FollowUp")
-      response.json["study_segments"][1]["planned"]["study"]["assigned_identifier"].should include("ECOG170")
+    describe "ics" do
+      before do
+        get "/subjects/ID001/schedules.ics", :as => :erin
+      end
       
+      it "is successful" do
+        response.should be_success
+      end
+
+      it "is ICS calendar" do
+        response.content_type.should == 'text/calendar'
+      end
+
+      it "has the right number of events" do
+        response.calendar.events.size.should == 6
+      end
+      
+      it "has right calendar properties" do
+        response.calendar.calscale.to_s.should =="GREGORIAN"
+        response.calendar.prodid.to_s.should == "-//Events Calendar//iCal4j 1.0//EN"
+        response.calendar.ip_method.to_s.should == "PUBLISH"
+      end
+      
+      describe "events" do
+        before do
+          @event = response.calendar.events[0]
+          @scheduled_activity = @studySubjectAssignment2.scheduledCalendar.scheduledStudySegments.first.activities.first
+        end
+        
+        it 'has the UID property' do
+          @event.uid.should == @scheduled_activity.grid_id
+        end 
+        
+        it 'has the TRANSP property' do
+          @event.transp.should == "TRANSPARENT"
+        end
+        
+        it 'has the Dtstart property' do
+          @event.dtstart.to_s.should == "2008-12-28"
+        end
+        
+        it 'has the Summary property' do
+          @event.summary.should == "ECOG170/Lab Test"
+        end
+        
+        describe "Description" do
+          it "includes subject name" do
+            @event.description.should =~ /Subject:Alan Boyarski/
+          end
+          
+          it "includes study name" do
+            @event.description.should =~ /Study:ECOG170/ 
+          end
+          
+          it "includes segment name" do
+            @event.description.should =~ /Study Segment:Followup/ 
+          end
+          
+          it "includes activity name" do
+            @event.description.should =~ /Activity:Lab Test/ 
+          end
+          
+          it "includes study plan day" do
+            @event.description.should =~ /Study plan day:Day 2/ 
+          end
+        end
+        
+        it 'has the URL property' do
+          @event.url.to_s.should =~ %r{/pages/cal/scheduleActivity\?event=#{@scheduled_activity.grid_id}$}
+        end
+      end
     end
     
     it "doesn't give schedule for invalid subject" do
@@ -130,43 +287,6 @@ describe "/subjects/{subject-identifier}/schedules" do
       response.status_code.should == 404
       response.status_message.should == "Not Found"
     end
-    
   end
   
-  describe "/activities POST" do
-    
-    it "updates the scheduled activity states for batch activities" do
-      @JSONentity = "{#{@scheduled_activity1.gridId} : { state : scheduled, reason : Delay by two days , date : 2009-12-30 },
-                      #{@scheduled_activity2.gridId} : { state : canceled, reason : Just canceled , date : 2009-12-29 }}"
-      post "/subjects/ID001/schedules/activities", @JSONentity,
-        :as => :erin , 'Content-Type' => 'application/json'
-      response.status_code.should == 207
-      response.json[@scheduled_activity1.gridId]["Status"] == 201
-      response.json[@scheduled_activity2.gridId]["Status"] == 201
-      response.json[@scheduled_activity1.gridId]["Location"] =~ %r{api/v1/studies/NU480/schedules/#{@studySubjectAssignment1.gridId}/activities/#{@scheduled_activity1.gridId}$}
-      response.json[@scheduled_activity2.gridId]["Location"] =~ %r{api/v1/studies/ECOG170/schedules/#{@studySubjectAssignment2.gridId}/activities/#{@scheduled_activity2.gridId}$}
-    end
-    
-    it "updates scheduled activity state for one activity and send 400 - Bad Request for another activity with incorrect state in request" do
-      @JSONentity = "{#{@scheduled_activity1.gridId} : { state : scheduled, reason : Delay by two days , date : 2009-12-30 },
-                      #{@scheduled_activity2.gridId} : { state : canceledd, reason : Just canceled , date : 2009-12-29 }}"
-      post "/subjects/ID001/schedules/activities", @JSONentity,
-            :as => :erin , 'Content-Type' => 'application/json'
-      response.status_code.should == 207
-      response.json[@scheduled_activity1.gridId]["Status"] == 201
-      response.json[@scheduled_activity2.gridId]["Status"] == 400
-      response.json[@scheduled_activity1.gridId]["Location"] =~ %r{api/v1/studies/NU480/schedules/#{@studySubjectAssignment1.gridId}/activities/#{@scheduled_activity1.gridId}$}
-    end
-              
-    it "updates scheduled activity state for one activity and send 400 - Bad Request for another activity with incorrect date in request" do
-      @JSONentity = "{#{@scheduled_activity1.gridId} : { state : scheduled, reason : Delay by two days , date : 2009-12-30 },
-                      #{@scheduled_activity2.gridId} : { state : canceled, reason : Just canceled , date : 2009 }}"
-      post "/subjects/ID001/schedules/activities", @JSONentity,
-            :as => :erin , 'Content-Type' => 'application/json'
-      response.status_code.should == 207
-      response.json[@scheduled_activity1.gridId]["Status"] == 201
-      response.json[@scheduled_activity2.gridId]["Status"] == 400
-      response.json[@scheduled_activity1.gridId]["Location"] =~ %r{api/v1/studies/NU480/schedules/#{@studySubjectAssignment1.gridId}/activities/#{@scheduled_activity1.gridId}$}
-    end
-  end
 end
