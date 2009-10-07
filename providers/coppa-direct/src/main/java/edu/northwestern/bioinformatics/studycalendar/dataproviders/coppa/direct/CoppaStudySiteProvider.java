9411dcb9055d3a75b476172d4be7cbf7488be19d
John Dzak <j-dzak@northwestern.edu>
2009-10-07 17:41:45 +0000

    Added test for getting associated study sites when no results are returned. Feature #774.

--- providers/coppa-direct/src/main/java/edu/northwestern/bioinformatics/studycalendar/dataproviders/coppa/direct/CoppaStudySiteProvider.java
+++ providers/coppa-direct/src/main/java/edu/northwestern/bioinformatics/studycalendar/dataproviders/coppa/direct/CoppaStudySiteProvider.java
@@ -3,17 +3,42 @@ package edu.northwestern.bioinformatics.studycalendar.dataproviders.coppa.direct
 import edu.northwestern.bioinformatics.studycalendar.domain.Site;
 import edu.northwestern.bioinformatics.studycalendar.domain.Study;
 import edu.northwestern.bioinformatics.studycalendar.domain.StudySite;
+import gov.nih.nci.coppa.services.pa.Id;
 import gov.nih.nci.coppa.services.pa.studysiteservice.client.StudySiteServiceClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CoppaStudySiteProvider implements edu.northwestern.bioinformatics.studycalendar.dataproviders.api.StudySiteProvider{
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     private StudySiteServiceClient client;
 
     public List<List<StudySite>> getAssociatedSites(List<Study> studies) {
-        List<List<StudySite>> results = new ArrayList<List<StudySite>>();
+        List<List<StudySite>> results = new ArrayList<List<StudySite>>(studies.size());
+
+        for (Study study : studies) {
+            String extension = study.getSecondaryIdentifierValue("extension");
+            gov.nih.nci.coppa.services.pa.StudySite[] studySites = getByStudyProtocol(extension);
+            if (studySites == null || studySites.length == 0) {
+                results.add(null);
+            }
+        }
         return results;
+    }
+
+    private gov.nih.nci.coppa.services.pa.StudySite[] getByStudyProtocol(String extension) {
+        Id id = new Id();
+        id.setExtension(extension);
+        try {
+            return client.getByStudyProtocol(id);
+        } catch (RemoteException e) {
+            log.error("COPPA study site search failed", e);
+            return new gov.nih.nci.coppa.services.pa.StudySite[0];
+        }
     }
 
     public List<List<StudySite>> getAssociatedStudies(List<Site> sites) {
--- providers/coppa-direct/src/test/java/edu/northwestern/bioinformatics/studycalendar/dataproviders/coppa/direct/CoppaStudySiteProviderTest.java
+++ providers/coppa-direct/src/test/java/edu/northwestern/bioinformatics/studycalendar/dataproviders/coppa/direct/CoppaStudySiteProviderTest.java
@@ -1,6 +1,7 @@
 package edu.northwestern.bioinformatics.studycalendar.dataproviders.coppa.direct;
 
 import edu.northwestern.bioinformatics.studycalendar.domain.Study;
+import edu.northwestern.bioinformatics.studycalendar.domain.StudySecondaryIdentifier;
 import edu.northwestern.bioinformatics.studycalendar.domain.StudySite;
 import gov.nih.nci.cabig.ctms.testing.MockRegistry;
 import gov.nih.nci.coppa.services.pa.Id;
@@ -8,8 +9,10 @@ import gov.nih.nci.coppa.services.pa.studysiteservice.client.StudySiteServiceCli
 import junit.framework.TestCase;
 import org.easymock.EasyMock;
 import static org.easymock.EasyMock.expect;
+import org.iso._21090.II;
 
 import java.util.ArrayList;
+import static java.util.Arrays.asList;
 import java.util.List;
 
 /**
@@ -31,11 +34,45 @@ public class CoppaStudySiteProviderTest extends TestCase {
 
     public void testGetAssociatedSitesWithEmptyStudyList() throws Exception {
         expect(client.getByStudyProtocol((Id) EasyMock.notNull())).andReturn(
-                new gov.nih.nci.coppa.services.pa.StudySite[0]
+            new gov.nih.nci.coppa.services.pa.StudySite[0]
         );
 
         List<List<StudySite>> actual = provider.getAssociatedSites(new ArrayList<Study>());
 
         assertEquals("Wrong results size", 0, actual.size());
+    }
+
+    public void testGetAssociatedSitesWithNoResults() throws Exception {
+        expect(client.getByStudyProtocol((Id) EasyMock.notNull())).andReturn(
+            new gov.nih.nci.coppa.services.pa.StudySite[0]
+        );
+
+        List<List<StudySite>> actual = provider.getAssociatedSites(asList(
+            pscStudy("Ext A")
+        ));
+
+        assertEquals("Wrong results size", 1, actual.size());
+        assertNull("Wrong element", actual.get(0));
+    }
+
+    /////////////// Helper Methods
+    private gov.nih.nci.coppa.services.pa.StudySite coppaStudySite(String extension) {
+        gov.nih.nci.coppa.services.pa.StudySite studySite =
+            new gov.nih.nci.coppa.services.pa.StudySite();
+        II ii = new II();
+        ii.setExtension(extension);
+        studySite.setIdentifier(ii);
+        return studySite;
+    }
+
+    private Study pscStudy(String extensionSecondaryIdentifier) {
+        Study study = new Study();
+
+        StudySecondaryIdentifier i = new StudySecondaryIdentifier ();
+        i.setType("extension");
+        i.setValue(extensionSecondaryIdentifier);
+        study.addSecondaryIdentifier(i);
+
+        return study;
     }
 }
