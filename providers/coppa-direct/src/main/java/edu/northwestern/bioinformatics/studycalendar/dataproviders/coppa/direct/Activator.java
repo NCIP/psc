3f4d884910b6f6c7aedff0a8a420a7f20d2a2a1a
Rhett Sutphin <r-sutphin@northwestern.edu>
2009-10-14 21:40:31 +0000

    Register services for COPPA study & study site providers.  #773 and #774.

--- providers/coppa-direct/src/main/java/edu/northwestern/bioinformatics/studycalendar/dataproviders/coppa/direct/Activator.java
+++ providers/coppa-direct/src/main/java/edu/northwestern/bioinformatics/studycalendar/dataproviders/coppa/direct/Activator.java
@@ -1,6 +1,8 @@
 package edu.northwestern.bioinformatics.studycalendar.dataproviders.coppa.direct;
 
 import edu.northwestern.bioinformatics.studycalendar.dataproviders.api.SiteProvider;
+import edu.northwestern.bioinformatics.studycalendar.dataproviders.api.StudyProvider;
+import edu.northwestern.bioinformatics.studycalendar.dataproviders.api.StudySiteProvider;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
@@ -10,6 +12,10 @@ import org.osgi.framework.BundleContext;
 public class Activator implements BundleActivator {
     public void start(BundleContext bundleContext) throws Exception {
         bundleContext.registerService(SiteProvider.class.getName(), new CoppaSiteProvider(), null);
+        bundleContext.registerService(StudyProvider.class.getName(),
+            new CoppaStudyProvider(), null);
+        bundleContext.registerService(StudySiteProvider.class.getName(),
+            new CoppaStudySiteProvider(), null);
     }
 
     public void stop(BundleContext bundleContext) throws Exception {
