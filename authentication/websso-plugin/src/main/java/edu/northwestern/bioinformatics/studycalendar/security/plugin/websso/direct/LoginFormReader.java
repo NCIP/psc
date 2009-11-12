583a7e76585e22f1861afa5b86f5a0e1fa16e2da
Rhett Sutphin <r-sutphin@northwestern.edu>
2009-11-12 19:29:30 +0000

    Special CAS direct implementation for WebSSO, since it requires additional parameters above a username and password.  Fixes #816.

---
+++ authentication/websso-plugin/src/main/java/edu/northwestern/bioinformatics/studycalendar/security/plugin/websso/direct/LoginFormReader.java
@@ -0,0 +1,67 @@
+package edu.northwestern.bioinformatics.studycalendar.security.plugin.websso.direct;
+
+import edu.northwestern.bioinformatics.studycalendar.security.plugin.cas.direct.CasDirectException;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author Rhett Sutphin
+ */
+public class LoginFormReader extends edu.northwestern.bioinformatics.studycalendar.security.plugin.cas.direct.LoginFormReader {
+    public LoginFormReader(String body) {
+        super(body);
+    }
+
+    /**
+     * Extracts a mapping from visible names to authentication service URLs.  Derived from
+     * the SELECT which is labeled "organization" in the WebSSO UI.
+     *
+     * @return
+     */
+    public Map<String, String> getAuthenticationServices() {
+        NodeList selects = getDocument().getElementsByTagName("select");
+        for (int i = 0; i < selects.getLength(); i++) {
+            Element select = (Element) selects.item(i);
+            if ("authenticationServiceURL".equals(select.getAttribute("id"))) {
+                return extractAuthenticationServiceMap(select);
+            }
+        }
+        failWithNoAuthenticationServices();
+        return null; // unreachable
+    }
+
+    private Map<String, String> extractAuthenticationServiceMap(Element authenticationServiceUrlSelect) {
+        Map<String, String> orgs = new LinkedHashMap<String, String>();
+        NodeList options = authenticationServiceUrlSelect.getElementsByTagName("option");
+        for (int i = 0; i < options.getLength(); i++) {
+            Element option = (Element) options.item(i);
+            String key = option.getTextContent();
+            String value = option.getAttribute("value");
+            if (!"-".equals(value)) {
+                orgs.put(key, value);
+            }
+        }
+        if (orgs.size() == 0) failWithNoAuthenticationServices();
+        return orgs;
+    }
+
+    public boolean hasUsernameAndPasswordFields() {
+        boolean hasUsername = false, hasPassword = false;
+
+        NodeList inputs = getDocument().getElementsByTagName("input");
+        for (int i = 0; i < inputs.getLength(); i++) {
+            Element input = (Element) inputs.item(i);
+            hasUsername = hasUsername || "username".equals(input.getAttribute("name"));
+            hasPassword = hasPassword || "password".equals(input.getAttribute("name"));
+        }
+
+        return hasUsername && hasPassword;
+    }
+
+    private void failWithNoAuthenticationServices() {
+        throw new CasDirectException("The WebSSO login page does not include a list of authenticationServiceURLs.  Cannot use direct login.");
+    }
+}
---
+++ authentication/websso-plugin/src/test/java/edu/northwestern/bioinformatics/studycalendar/security/plugin/websso/direct/WebSSODirectAuthenticationProviderTest.java
@@ -0,0 +1,147 @@
+package edu.northwestern.bioinformatics.studycalendar.security.plugin.websso.direct;
+
+import edu.northwestern.bioinformatics.studycalendar.domain.Role;
+import edu.northwestern.bioinformatics.studycalendar.security.plugin.AuthenticationTestCase;
+import edu.northwestern.bioinformatics.studycalendar.security.plugin.cas.direct.CasDirectException;
+import edu.northwestern.bioinformatics.studycalendar.security.plugin.cas.direct.CasDirectUsernamePasswordAuthenticationToken;
+import static gov.nih.nci.cabig.ctms.testing.MoreJUnitAssertions.assertContains;
+import org.acegisecurity.Authentication;
+import org.acegisecurity.AuthenticationException;
+import org.acegisecurity.BadCredentialsException;
+import static org.easymock.EasyMock.expect;
+
+import java.io.IOException;
+
+/**
+ * Note that this class only includes tests for differences between
+ * {@link edu.northwestern.bioinformatics.studycalendar.security.plugin.websso.direct.WebSSODirectAuthenticationProvider}
+ * and {@link
+ *
+ * @author Rhett Sutphin
+ */
+public class WebSSODirectAuthenticationProviderTest extends AuthenticationTestCase {
+    private static final String CREDENTIAL_PROVIDER = ">Foo Provider";
+    private static final String ORGANIZATION = "Dorian";
+    private static final String USERNAME = "joe";
+    private static final String QUALIFIED_USERNAME =
+        CREDENTIAL_PROVIDER + '\\' + ORGANIZATION + '\\' + USERNAME;
+    private static final String PASSWORD = "eoj";
+    private static final String LT = "the-ticket";
+
+    private WebSSODirectAuthenticationProvider provider;
+
+    private DirectLoginHttpFacade loginFacade;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        loginFacade = registerMockFor(DirectLoginHttpFacade.class);
+
+        provider = new WebSSODirectAuthenticationProvider() {
+            @Override
+            protected DirectLoginHttpFacade createLoginFacade() {
+                return loginFacade;
+            }
+        };
+        provider.setUserDetailsService(userDetailsService);
+
+        userDetailsService.addUser(USERNAME, Role.SUBJECT_COORDINATOR);
+    }
+
+    public void testUsernameOnlyPrincipalResultsInNoAuthentication() throws Exception {
+        try {
+            doAuthenticate(USERNAME);
+            fail("Exception not thrown");
+        } catch (BadCredentialsException bce) {
+            assertEquals("The principal must be of the form credential-provider\\authentication-service-name\\username", bce.getMessage());
+        }
+    }
+
+    public void testTooManyEntriesInPrincipalResultsInNoAuthentication() throws Exception {
+        try {
+            doAuthenticate("something\\else\\again\\and-again");
+            fail("Exception not thrown");
+        } catch (BadCredentialsException bce) {
+            assertEquals("The principal must be of the form credential-provider\\authentication-service-name\\username", bce.getMessage());
+        }
+    }
+
+    public void testBadLoginFormResultsInNoAuthentication() throws Exception {
+        /* expect */ loginFacade.start();
+        expect(loginFacade.selectCredentialProvider(CREDENTIAL_PROVIDER)).andThrow(new CasDirectException("Bad form"));
+
+        try {
+            doAuthenticate();
+            fail("Exception not thrown");
+        } catch (AuthenticationException ae) {
+            assertContains("Wrong exception message", ae.getMessage(), "Direct CAS login failed");
+            assertContains("Wrong exception message", ae.getMessage(), "Bad form");
+        }
+    }
+
+    public void testUnavailableLoginFormResultsInNoAuthentication() throws Exception {
+        /* expect */ loginFacade.start();
+        expect(loginFacade.selectCredentialProvider(CREDENTIAL_PROVIDER)).andThrow(new IOException("Bad connection"));
+
+        try {
+            doAuthenticate();
+            fail("Exception not thrown");
+        } catch (AuthenticationException ae) {
+            assertContains("Wrong exception message", ae.getMessage(), "Direct CAS login failed");
+            assertContains("Wrong exception message", ae.getMessage(), "Bad connection");
+        }
+    }
+
+    public void testFailedPostResultsInNoAuthentication() throws Exception {
+        /* expect */ loginFacade.start();
+        expect(loginFacade.selectCredentialProvider(CREDENTIAL_PROVIDER)).
+            andReturn("<input name='lt' value='the-ticket'/><select id='authenticationServiceURL'><option value='http://dorian!/'>Dorian</select>");
+        /* expect */ loginFacade.selectAuthenticationService("http://dorian!/");
+        expect(loginFacade.postCredentials(USERNAME, PASSWORD)).andReturn(false);
+
+        try {
+            doAuthenticate();
+            fail("Exception not thrown");
+        } catch (BadCredentialsException bce) {
+            assertEquals("Credentials are invalid according to direct CAS login", bce.getMessage());
+        }
+    }
+
+    public void testUnknownAuthenticationServiceFailsAuthentication() throws Exception {
+        /* expect */ loginFacade.start();
+        expect(loginFacade.selectCredentialProvider(CREDENTIAL_PROVIDER)).
+            andReturn("<input name='lt' value='the-ticket'/><select id='authenticationServiceURL'><option value='http://dorian!/'>Not Dorian</select>");
+
+        try {
+            doAuthenticate();
+            fail("Exception not thrown");
+        } catch (BadCredentialsException bce) {
+            assertEquals("The WebSSO server does not know of an authentication service named \"Dorian\"", bce.getMessage());
+        }
+    }
+    
+    public void testSuccessfulPostResultsInAuthentication() throws Exception {
+        /* expect */ loginFacade.start();
+        expect(loginFacade.selectCredentialProvider(CREDENTIAL_PROVIDER)).
+            andReturn("<input name='lt' value='the-ticket'/><select id='authenticationServiceURL'><option value='http://dorian!/'>Dorian</select>");
+        /* expect */ loginFacade.selectAuthenticationService("http://dorian!/");
+
+        expect(loginFacade.postCredentials(USERNAME, PASSWORD)).andReturn(true);
+
+        Authentication actual = doAuthenticate();
+        assertNotNull(actual);
+        assertTrue(actual.isAuthenticated());
+    }
+
+    private Authentication doAuthenticate() {
+        return doAuthenticate(QUALIFIED_USERNAME);
+    }
+
+    private Authentication doAuthenticate(String qualifiedUsername) {
+        replayMocks();
+        Authentication actual = provider.authenticate(
+            new CasDirectUsernamePasswordAuthenticationToken(qualifiedUsername, PASSWORD));
+        verifyMocks();
+        return actual;
+    }
+}
