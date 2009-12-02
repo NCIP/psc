f701955ef1e87d04d30ba83b6b5cca4cdba33411
John Dzak <j-dzak@northwestern.edu>
2009-12-02 00:39:53 +0000

    Changed the coppa study site consumer to take a study, instead of a study site, as an argument to refresh. This should make it usable. :) Still need to add refresh throttling, searching on the site sitde, and lots of cleanup/refactoring. Task #817...chug..chug..chug

--- core/src/main/java/edu/northwestern/bioinformatics/studycalendar/service/dataproviders/StudySiteConsumer.java
+++ core/src/main/java/edu/northwestern/bioinformatics/studycalendar/service/dataproviders/StudySiteConsumer.java
@@ -5,59 +5,184 @@ import edu.northwestern.bioinformatics.studycalendar.domain.Study;
 import edu.northwestern.bioinformatics.studycalendar.domain.StudySite;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import static java.util.Arrays.asList;
 import java.util.List;
+import java.util.Map;
 
-public class StudySiteConsumer extends AbstractConsumer<StudySite, StudySiteProvider> {
+public class StudySiteConsumer extends AbstractConsumer {
     @Override protected Class<StudySiteProvider> providerType() { return StudySiteProvider.class; }
 
-    public StudySite refresh(StudySite in) {
-        return refresh(Arrays.asList(in)).get(0);
+    public List<StudySite> refresh(Study in) {
+        return refresh(asList(in)).get(0);
     }
 
-    public List<StudySite> refresh(List<StudySite> in) {
-        return new Refresh().execute(in);
+    public List<List<StudySite>> refresh(List<Study> in) {
+        return new StudySpecificRefresh().execute(in);
     }
 
-    private class Refresh extends AbstractRefresh {
-        @Override
-        protected List<StudySite> loadNewVersions(StudySiteProvider provider, List<StudySite> targetStudySites) {
-            List<StudySite> results = new ArrayList<StudySite>(targetStudySites.size());
+    private class StudySpecificRefresh {    
+        protected List<List<StudySite>> loadNewVersions(StudySiteProvider provider, List<Study> targetedStudy) {
+            return provider.getAssociatedSites(targetedStudy);
+        }
+        
+        protected void updateInstanceInPlace(StudySite current, StudySite newVersion) {
+            current.setLastRefresh(newVersion.getLastRefresh());
+        }
 
-            List<Study> targetStudies = new ArrayList <Study>();
-            for (StudySite targetStudySite : targetStudySites) {
-                targetStudies.add(targetStudySite.getStudy());
-            }
 
-            // Get StudySites from the Provider
-            List<List<StudySite>> provided = provider.getAssociatedSites(targetStudies);
+        // TODO: Refactor Me, everything below this line is a duplication of AbstractConsumer
+        private Map<String, StudySiteProvider> providers;
 
-            // Lets validate each Study/Site association still exists.... Ready... Go!!
-            for (StudySite targetStudySite : targetStudySites) {
-                int i = targetStudySites.indexOf(targetStudySite);
+        protected StudySpecificRefresh() {
+            providers = getProviders();
+        }
 
-                // Both the target StudySites and the provided StudySites should correspond by index (see StudySiteProvider Contract)
-                for (StudySite providedStudySite : provided.get(i)) {
+        @SuppressWarnings({ "unchecked" })
+        public List<List<StudySite>> execute(List<Study> in) {
+            List<List<StudySite>> results = new ArrayList<List<StudySite>>();
+            for (StudySiteProvider provider : providers.values()) {
+                List<List<StudySite>> fromProvider = loadNewVersions(provider, in);
 
-                    // Since we already searched the provider by Study, to make sure
-                    // the StudySite still exists, all we have to do is check that the site
-                    // has the correct site.
-                    if (providedStudySite.getSite().equals(targetStudySite.getSite())) {
-                        results.add(i, targetStudySite);        // StudySite still exists!!!
-                        break;
-                    }
+                for (int i = 0; i < in.size(); i++) {
+                    Study study = in.get(i);
+                    List<StudySite> currentStudySites = in.get(i).getStudySites();
+                    List<StudySite> fromProviderStudySites = fromProvider.get(i);
+                    provisionInstances(fromProviderStudySites, provider);
+                    associateStudy(fromProviderStudySites, study);
+                    List<StudySite> merged = union(currentStudySites, fromProviderStudySites);
+                    results.add(merged);
                 }
+
             }
+
             return results;
         }
 
-        @Override
-        protected void updateInstanceInPlace(StudySite current, StudySite newVersion) {
-            if (newVersion == null) {
-                current = null;
-            } else {
-                current.setLastRefresh(newVersion.getLastRefresh());
+        private List<StudySite> union(List<StudySite> first, List<StudySite> second) {
+            List<StudySite> merged = new ArrayList<StudySite>(first);
+            for (StudySite ss : second) {
+                if (!merged.contains(ss)) {
+                    merged.add(ss);
+                }
+            }
+            return merged;
+        }
+
+        private void associateStudy(List<StudySite> fromProviderStudySites, Study study) {
+            for (StudySite ss : fromProviderStudySites) {
+                ss.setStudy(study);
             }
         }
+
+
+        protected void provisionInstances(List<StudySite> in, StudySiteProvider provider) {
+         for (StudySite studySite : in) {
+             provisionInstance(studySite, provider);
+         }
+     }
+            /**
+     * Generic search implementation which subclasses may expose if appropriate.
+     * @see edu.northwestern.bioinformatics.studycalendar.dataproviders.api.SearchingProvider
+     */
+//    @SuppressWarnings({ "unchecked" })
+//    protected List<D> doSearch(String partialName) {
+//        if (!SearchingProvider.class.isAssignableFrom(providerType())) {
+//            throw new StudyCalendarError("%s is not a %s.  Search won't work.",
+//                providerType().getSimpleName(), SearchingProvider.class.getSimpleName());
+//        }
+//
+//        List<P> providers = getOsgiLayerTools().getServices(providerType());
+//        List<D> found = new ArrayList<D>();
+//        for (P provider : providers) {
+//            List<D> providerMatches = ((SearchingProvider<D>) provider).search(partialName);
+//            if (providerMatches != null) {
+//                for (D match : providerMatches) {
+//                    found.add(provisionInstance(match, provider));
+//                }
+//            }
+//        }
+//        return found;
+//    }
+//        private void updateAssociated(List<Study> in) {
+//            Map<String, List<Study>> toUpdate = findToUpdate(in);
+//            for (String providerToken : toUpdate.keySet()) {
+//                refreshFromProvider(providers.get(providerToken), toUpdate.get(providerToken));
+//            }
+//        }
+
+//        private Map<String, List<Study>> findToUpdate(List<Study> source) {
+//            Map<String, List<Study>> toUpdate = new HashMap<String, List<Study>>();
+//
+//            Timestamp now = getNowFactory().getNowTimestamp();
+//            for (Study item : source) {
+//                String providerName = item.getProvider();
+//                if (providerName == null) continue;
+//                if (shouldRefresh(item, now)) {
+//                    if (!toUpdate.containsKey(providerName)) {
+//                        toUpdate.put(providerName, new ArrayList<Study>());
+//                    }
+//                    toUpdate.get(providerName).add(item);
+//                }
+//            }
+//
+//            return toUpdate;
+//        }
+//
+//        private boolean shouldRefresh(Study providable, Timestamp now) {
+//            StudySiteProvider dataProvider = providers.get(providable.getProvider());
+//            if (!(dataProvider instanceof RefreshableProvider)) return false;
+//            Integer interval = ((RefreshableProvider) dataProvider).getRefreshInterval();
+//            if (interval == null || interval < 0) return false;
+//            interval = 1000 * interval; // convert to ms
+//            long lastRefresh = providable.getLastRefresh() == null ? 0 : providable.getLastRefresh().getTime();
+//            long timeSince = now.getTime() - lastRefresh;
+//            log.debug("{}ms since last refresh", timeSince);
+//            if (timeSince < interval) {
+//                log.debug(" - within refresh interval {}ms; skipping", interval);
+//                return false;
+//            } else {
+//                log.debug(" - greater than refresh interval {}ms; refreshing", interval);
+//                return true;
+//            }
+//        }
+
+//        protected void refreshFromProvider(StudySiteProvider provider, List<Study> currentVersions) {
+//            List<StudySite> newVersions;
+//            try {
+//                newVersions = loadNewVersions(provider, currentVersions);
+//            } catch (RuntimeException re) {
+//                log.error("Refreshing " + currentVersions.size() + ' ' +
+//                    providerType().getSimpleName() + " instance(s) from " + provider.providerToken() +
+//                    "failed", re);
+//                log.debug("Specifically, the provider was trying to refresh {}", currentVersions);
+//                return;
+//            }
+//
+//            if (newVersions == null) {
+//                log.error(
+//                    "Provider {} violated protocol by returning null.  Ignoring.",
+//                    provider.providerToken());
+//            } else if (newVersions.size() != currentVersions.size()) {
+//                log.error(
+//                    "Provider {} violated protocol returning the wrong number of results ({} when expecting {}).  Ignoring.",
+//                    new Object[] { provider.providerToken(), newVersions.size(), currentVersions.size() });
+//                newVersions = null;
+//            }
+//
+//            if (newVersions != null) {
+//                updateInstances(currentVersions, newVersions, provider);
+//            }
+//        }
+//
+//        private void updateInstances(List<Study> currentVersions, List<D> newVersions, StudySiteProvider provider) {
+//            for (ListIterator<D> lit = currentVersions.listIterator(); lit.hasNext();) {
+//                D current = lit.next();
+//                D newVersion = newVersions.get(lit.previousIndex());
+//                if (newVersion != null) {
+//                    provisionInstance(newVersion, provider);
+//                    updateInstanceInPlace(current, newVersion);
+//                }
+//            }
+//        }
     }
 }
--- core/src/test/java/edu/northwestern/bioinformatics/studycalendar/service/dataproviders/StudySiteConsumerTest.java
+++ core/src/test/java/edu/northwestern/bioinformatics/studycalendar/service/dataproviders/StudySiteConsumerTest.java
@@ -1,11 +1,11 @@
 package edu.northwestern.bioinformatics.studycalendar.service.dataproviders;
 
+import edu.northwestern.bioinformatics.studycalendar.core.Fixtures;
 import edu.northwestern.bioinformatics.studycalendar.core.StudyCalendarTestCase;
 import edu.northwestern.bioinformatics.studycalendar.core.osgi.OsgiLayerTools;
 import edu.northwestern.bioinformatics.studycalendar.dataproviders.api.RefreshableProvider;
 import edu.northwestern.bioinformatics.studycalendar.dataproviders.api.StudySiteProvider;
 import static edu.northwestern.bioinformatics.studycalendar.domain.Fixtures.createSite;
-import static edu.northwestern.bioinformatics.studycalendar.domain.Fixtures.createStudySite;
 import edu.northwestern.bioinformatics.studycalendar.domain.Site;
 import edu.northwestern.bioinformatics.studycalendar.domain.Study;
 import edu.northwestern.bioinformatics.studycalendar.domain.StudySite;
@@ -16,6 +16,7 @@ import static org.easymock.EasyMock.expect;
 import java.sql.Timestamp;
 import static java.util.Arrays.asList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.List;
 
 public class StudySiteConsumerTest extends StudyCalendarTestCase {
@@ -29,9 +30,8 @@ public class StudySiteConsumerTest extends StudyCalendarTestCase {
     private List<StudySiteProvider> providers;
 
     private Site nu;
+    private Site uic;
     private Study nu123;
-    private StudySite fromA;
-
 
     @Override
     protected void setUp() throws Exception {
@@ -50,36 +50,86 @@ public class StudySiteConsumerTest extends StudyCalendarTestCase {
         expect(providerA.providerToken()).andStubReturn("alpha");
         expect(providerA.getRefreshInterval()).andStubReturn(20 * 60);
 
+        nu = createSite("NU");
+        uic = createSite("UIC");
+
         nu123 = new Study();
         nu123.setAssignedIdentifier("NU123");
-        nu = createSite("Northwestern");
-        fromA = createStudySite(nu123, nu);
-        fromA.setProvider("alpha");
-        fromA.setLastRefresh(INITIAL_REFRESH);
+        nu123.setProvider("alpha");
+        nu123.setLastRefresh(INITIAL_REFRESH);
 
         expect(tools.getServices(StudySiteProvider.class)).andReturn(providers);
     }
 
-    public void testStudySiteExistsOnRefresh() {
-        Site uic = createSite("UIC");
+    public void testNewStudySiteAddedOnRefresh() {
+        associate(nu123, nu);
 
         expect(providerA.getAssociatedSites(asList(nu123))).andReturn(asList(asList(
-            createStudySite(nu123, uic),
-            createStudySite(nu123, nu)
+            createBasicStudySite(null, nu),
+            createBasicStudySite(null, uic)
         )));
 
         replayMocks();
-        StudySite result = consumer.refresh(fromA);
+        List<StudySite> results = consumer.refresh(nu123);
         verifyMocks();
 
-        assertNotNull("StudySite should still exist", result);
-        assertEquals("Wrong Study", "NU123", result.getStudy().getAssignedIdentifier());
-        assertEquals("Wrong Study", "Northwestern", result.getSite().getName());
+        assertEquals("Wrong Number of Sites", 2, results.size());
+
+        assertEquals("Wrong Site", "NU", results.get(0).getSite().getName());
+        assertEquals("Wrong Site", "UIC", results.get(1).getSite().getName());
     }
 
-//    public void testStudySitePrunedOnRefresh() {
-//        assertTrue(false);
-//    }
+    public void testNoDuplicateStudySitesOnRefresh() {
+        associate(nu123, nu);
 
+        expect(providerA.getAssociatedSites(asList(nu123))).andReturn(asList(asList(
+            createBasicStudySite(null, nu)
+        )));
+
+        replayMocks();
+        List<StudySite> results = consumer.refresh(nu123);
+        verifyMocks();
+
+        assertEquals("Wrong Number of Sites", 1, results.size());
+        assertEquals("Wrong Site", "NU", results.get(0).getSite().getName());
+    }
+
+    public void testDeletedStudySiteExistsOnRefresh() {
+        associate(nu123, nu);
+
+        expect(providerA.getAssociatedSites(asList(nu123))).andReturn(asList(Collections.<StudySite>emptyList()));
+
+        replayMocks();
+        List<StudySite> results = consumer.refresh(nu123);
+        verifyMocks();
+
+        assertEquals("Wrong Number of Sites", 1, results.size());
+        assertEquals("Wrong Site", "NU", results.get(0).getSite().getName());
+    }
+
+
+
+    ///// Helper Methods
+
+    private StudySite associate(Study study, Site site) {
+        StudySite ss = Fixtures.createStudySite(study, site);
+        ss.setStudy(study);
+        ss.setSite(site);
+        return ss;
+    }
+
+    
+            //        fromA.setProvider("alpha");
+//        fromA.setLastRefresh(INITIAL_REFRESH);
+
+
+    public static StudySite createBasicStudySite(Study study, Site site) {
+        StudySite studySite = new StudySite();
+        if (study != null) studySite.setStudy(study);
+        if (site != null) studySite.setSite(site);
+        return studySite;
+    }
+
+    ///// Stub interface
     private interface RefreshableStudySiteProvider extends StudySiteProvider, RefreshableProvider { }
 }
