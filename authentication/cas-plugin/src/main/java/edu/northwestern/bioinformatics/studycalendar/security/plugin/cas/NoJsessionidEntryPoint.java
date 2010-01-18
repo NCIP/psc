b1ae9cd7776e84c63db07771abf3f3bafc8b6bc9
Rhett Sutphin <r-sutphin@northwestern.edu>
2010-01-18 14:23:59 +0000

    Port r4764: Prevent the CAS plugin from appending JSESSIONID to the URL.  Fixes #899.

---
+++ authentication/cas-plugin/src/main/java/edu/northwestern/bioinformatics/studycalendar/security/plugin/cas/NoJsessionidEntryPoint.java
@@ -0,0 +1,53 @@
+package edu.northwestern.bioinformatics.studycalendar.security.plugin.cas;
+
+import org.acegisecurity.AuthenticationException;
+import org.acegisecurity.ui.cas.CasProcessingFilterEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.Proxy;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * This class wraps {@link CasProcessingFilterEntryPoint} to prevent it from asking the container to
+ * append JSESSIONID to the service URL.  This behavior is configurable in Spring Security 2 so
+ * we should be able to remove this class when we upgrade.
+ *
+ * @see http://jira.springframework.org/browse/SEC-306
+ * @author Rhett Sutphin
+ */
+public class NoJsessionidEntryPoint extends CasProcessingFilterEntryPoint {
+    @Override
+    public void commence(
+        ServletRequest servletRequest, ServletResponse servletResponse, AuthenticationException e
+    ) throws IOException, ServletException {
+        super.commence(servletRequest, wrap((HttpServletResponse) servletResponse), e);
+    }
+
+    private ServletResponse wrap(HttpServletResponse originalResponse) {
+        return (ServletResponse) Proxy.newProxyInstance(
+            originalResponse.getClass().getClassLoader(),
+            new Class[] { HttpServletResponse.class },
+            new NoEncodeHandler(originalResponse));
+    }
+
+    private class NoEncodeHandler implements InvocationHandler {
+        private HttpServletResponse originalResponse;
+
+        public NoEncodeHandler(HttpServletResponse originalResponse) {
+            this.originalResponse = originalResponse;
+        }
+
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            if ("encodeURL".equals(method.getName()) && args.length == 1) {
+                return args[0];
+            } else {
+                return method.invoke(originalResponse, args);
+            }
+        }
+    }
+}
---
+++ authentication/cas-plugin/src/test/java/edu/northwestern/bioinformatics/studycalendar/security/plugin/cas/NoJsessionidEntryPointTest.java
@@ -0,0 +1,60 @@
+package edu.northwestern.bioinformatics.studycalendar.security.plugin.cas;
+
+import junit.framework.TestCase;
+import org.acegisecurity.ui.cas.ServiceProperties;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.net.URLEncoder;
+
+/**
+ * @author Rhett Sutphin
+ */
+public class NoJsessionidEntryPointTest extends TestCase {
+    private static final String LOGIN_URL = "https://security-server/cas";
+    private static final String SERVICE = "http://localhost/psc-tests";
+
+    private NoJsessionidEntryPoint entryPoint;
+    private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        request = new MockHttpServletRequest();
+        response = new SessionAppendingMockHttpServletResponse();
+
+        entryPoint = new NoJsessionidEntryPoint();
+        entryPoint.setLoginUrl(LOGIN_URL);
+        ServiceProperties sp = new ServiceProperties();
+        sp.setService(SERVICE);
+        entryPoint.setServiceProperties(sp);
+    }
+
+    public void testRedirectedUrlDoesNotIncludeJsessionid() throws Exception {
+        commence();
+
+        assertFalse("jsessionid present in response URL: " + response.getRedirectedUrl(),
+            response.getRedirectedUrl().contains("jsessionid"));
+    }
+
+    public void testRedirectedUrlDoesIncludeService() throws Exception {
+        commence();
+
+        assertTrue("service not present in response URL: " + response.getRedirectedUrl(),
+            response.getRedirectedUrl().contains("service=" + URLEncoder.encode(SERVICE, "UTF-8")));
+    }
+
+    private void commence() throws IOException, ServletException {
+        entryPoint.commence(request, response, null);
+    }
+
+    private static class SessionAppendingMockHttpServletResponse extends MockHttpServletResponse {
+        @Override
+        public String encodeURL(String s) {
+            return s + ";jsessionid=foo";
+        }
+    }
+}
