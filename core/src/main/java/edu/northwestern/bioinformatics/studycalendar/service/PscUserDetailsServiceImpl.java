d698836bf62580bc2a4588b32849b1ae26958429
Rhett Sutphin <r-sutphin@northwestern.edu>
2010-02-23 22:10:58 +0000

    Port r4844: Use explicit transaction demarcation to force rollbacks in PscUserDetailsService since spring/hibernate weren't getting the hint from the read-only transaction annotation.  Fixes #925.

--- core/src/main/java/edu/northwestern/bioinformatics/studycalendar/service/PscUserDetailsServiceImpl.java
+++ core/src/main/java/edu/northwestern/bioinformatics/studycalendar/service/PscUserDetailsServiceImpl.java
@@ -4,24 +4,52 @@ import edu.northwestern.bioinformatics.studycalendar.domain.User;
 import edu.northwestern.bioinformatics.studycalendar.security.acegi.PscUserDetailsService;
 import org.acegisecurity.DisabledException;
 import org.acegisecurity.userdetails.UsernameNotFoundException;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.dao.DataAccessException;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 /**
  * Implementation of Acegi's {@UserDetailsService} for PSC.
  */
-@Transactional(readOnly = true)
 public class PscUserDetailsServiceImpl implements PscUserDetailsService {
     private UserService userService;
+    private PlatformTransactionManager transactionManager;
 
     public User loadUserByUsername(String userName) throws UsernameNotFoundException, DataAccessException, DisabledException {
+        // This explicit transaction demarcation shouldn't be necessary
+        // However, annotating with @Transactional(readOnly=true) was not stopping hibernate from flushing.
+        TransactionStatus transactionStatus = transactionManager.getTransaction(readOnlyTransactionDef());
+        try {
+            return actuallyLoadUser(userName);
+        } finally {
+            transactionManager.rollback(transactionStatus);
+        }
+    }
+
+    private User actuallyLoadUser(String userName) {
         User user = userService.getUserByName(userName);
         if (user == null) throw new UsernameNotFoundException("Unknown user " + userName);
         if (!user.getActiveFlag()) throw new DisabledException("User is disabled " +userName);
         return user;
     }
 
+    private DefaultTransactionDefinition readOnlyTransactionDef() {
+        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+        def.setReadOnly(true);
+        return def;
+    }
+
+    ////// CONFIGURATION
+
+    @Required
     public void setUserService(UserService userService) {
         this.userService = userService;
+    }
+
+    @Required
+    public void setTransactionManager(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
     }
 }
---
+++ core/src/test/java/edu/northwestern/bioinformatics/studycalendar/service/PscUserDetailsServiceIntegratedTest.java
@@ -0,0 +1,48 @@
+package edu.northwestern.bioinformatics.studycalendar.service;
+
+import edu.northwestern.bioinformatics.studycalendar.core.DaoTestCase;
+import edu.northwestern.bioinformatics.studycalendar.dao.SiteDao;
+import edu.northwestern.bioinformatics.studycalendar.domain.Role;
+import edu.northwestern.bioinformatics.studycalendar.domain.Site;
+import edu.northwestern.bioinformatics.studycalendar.domain.User;
+import edu.northwestern.bioinformatics.studycalendar.security.acegi.PscUserDetailsService;
+
+/**
+ * @author Rhett Sutphin
+ */
+public class PscUserDetailsServiceIntegratedTest extends DaoTestCase {
+    private PscUserDetailsService service;
+    private SiteDao siteDao;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        service = (PscUserDetailsService) getApplicationContext().getBean("pscUserDetailsService");
+        siteDao = (SiteDao) getApplicationContext().getBean("siteDao");
+    }
+
+    public void testUserLoading() throws Exception {
+        User loaded = service.loadUserByUsername("Joey");
+        assertNotNull(loaded);
+    }
+
+    public void testUserModificationsAreNotAutomaticallyPersisted() throws Exception {
+        int originalId;
+        String originalName;
+        {
+            User loaded = service.loadUserByUsername("Joey");
+
+            Site someSite = loaded.getUserRole(Role.SUBJECT_COORDINATOR).getSites().iterator().next();
+            originalId = someSite.getId();
+            originalName = someSite.getName();
+            someSite.setName("Something else");
+        }
+
+        interruptSession();
+
+        {
+            Site reloaded = siteDao.getById(originalId);
+            assertEquals("Name changed", originalName, reloaded.getName());
+        }
+    }
+}
