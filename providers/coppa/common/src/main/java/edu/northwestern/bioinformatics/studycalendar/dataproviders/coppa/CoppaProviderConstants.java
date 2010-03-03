5bb7436c8f7699934719418b6a4605a0eb3c8fdc
John Dzak <j-dzak@northwestern.edu>
2010-03-03 21:10:53 +0000

    Change CoppaStudyProvider to use the correct secondary identifier when searching for COPPA studies. Bug #936.

--- providers/coppa/common/src/main/java/edu/northwestern/bioinformatics/studycalendar/dataproviders/coppa/CoppaProviderConstants.java
+++ providers/coppa/common/src/main/java/edu/northwestern/bioinformatics/studycalendar/dataproviders/coppa/CoppaProviderConstants.java
@@ -11,4 +11,7 @@ public interface CoppaProviderConstants {
      * into which the COPPA <tt>StudyProtocol</tt> assigned identifier is set.
      */
     String COPPA_STUDY_IDENTIFIER_TYPE = "COPPA Identifier";
+    String COPPA_STUDY_PUBLIC_TITLE_TYPE = "COPPA Public Title";
+    String COPPA_STUDY_OFFICIAL_TITLE_TYPE = "COPPA Official Title";
+    String COPPA_LEAD_ORGANIZATION_IDENTIFIER_TYPE= "COPPA Lead Organization Identifier";
 }
