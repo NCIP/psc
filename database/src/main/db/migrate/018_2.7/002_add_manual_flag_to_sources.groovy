1102819ef54e6045a830310ea391efddd3588435
Jalpa Patel <jalpa-patel@northwestern.edu>
2010-04-09 19:23:34 +0000

    Update migration script to set flag to true for fixing the migration failure on oracle. #957.

--- database/src/main/db/migrate/018_2.7/002_add_manual_flag_to_sources.groovy
+++ database/src/main/db/migrate/018_2.7/002_add_manual_flag_to_sources.groovy
@@ -2,7 +2,7 @@ class AddManualFlagToSources extends edu.northwestern.bioinformatics.bering.Migr
     void up() {
         addColumn("sources", "manual_flag", "boolean");
         execute("ALTER TABLE sources ADD CONSTRAINT un_one_manual_source UNIQUE (manual_flag)")
-        execute("update sources set manual_flag = true where name = 'PSC - Manual Activity Creation' ");
+        execute("update sources set manual_flag = 'TRUE' where name = 'PSC - Manual Activity Creation' ");
     }
 
     void down() {
