f88b248e586d6121c8615cc814cf6fa22dd80814
Rhett Sutphin <r-sutphin@northwestern.edu>
2010-04-12 16:10:08 +0000

    Correct initial value for oracle.  #957.

--- database/src/main/db/migrate/018_2.7/002_add_manual_flag_to_sources.groovy
+++ database/src/main/db/migrate/018_2.7/002_add_manual_flag_to_sources.groovy
@@ -1,13 +1,16 @@
 class AddManualFlagToSources extends edu.northwestern.bioinformatics.bering.Migration {
     void up() {
         addColumn("sources", "manual_flag", "boolean");
-        execute("ALTER TABLE sources ADD CONSTRAINT un_one_manual_source UNIQUE (manual_flag)")
-        execute("update sources set manual_flag = 'TRUE' where name = 'PSC - Manual Activity Creation' ");
+        execute("ALTER TABLE sources ADD CONSTRAINT un_one_manual_source UNIQUE (manual_flag)");
+        if (databaseMatches("oracle")) {
+            execute("UPDATE sources SET manual_flag=1 WHERE name='PSC - Manual Activity Creation'");
+        } else {
+            execute("UPDATE sources set manual_flag=true WHERE name='PSC - Manual Activity Creation'");
+        }
     }
 
     void down() {
         execute("ALTER TABLE sources DROP CONSTRAINT un_one_manual_source")
         removeColumn("sources", "manual_flag");
-
     }
 }
