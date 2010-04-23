e14641bb76553cf4297a8d2cf00e326c0a223fca
John Dzak <j-dzak@northwestern.edu>
2010-04-23 15:39:10 +0000

    Adding tests for populating the study details on the purge study page.

--- web/js-spec/src/spec/javascript/admin/purge-study/purge-study_spec.js
+++ web/js-spec/src/spec/javascript/admin/purge-study/purge-study_spec.js
@@ -16,30 +16,31 @@ Screw.Unit(function () {
     var requestedAjaxResponses;
 
 
-    describe("psc.admin.PurgeStudy", function () {
+    describe("psc.admin.ps", function () {
       before(function () {
-
           SC.relativeUri = function(arg) {
             return arg;
           }
 
-          SC.asyncRequest = function(uri, options) {
-            var resp = {
-              responseJSON: {
-                studies: [
-                  {id: -99, assigned_identifier: 'NCT-123'}
-                ]
-              }
-            }
-            options.onSuccess(resp)
-          }
-
           jQuery(document).ready(function() {
-              psc.admin.PurgeStudy.init();
+//              psc.admin.PurgeStudy.init();
           })
       });
 
-      describe("search", function () {
+      describe("StudyAutocompleter", function () {
+        before(function () {
+            SC.asyncRequest = function(uri, options) {
+                var resp = {
+                    responseJSON: {
+                        studies: [
+                          {id: -99, assigned_identifier: 'NCT-123'}
+                        ]
+                    }
+                }
+                options.onSuccess(resp)
+            }
+        });
+
         it("returns a list of matching studies", function () {
             $('studies-autocompleter-input').value = 'NCT';
 
@@ -52,6 +53,106 @@ Screw.Unit(function () {
 
         });
       });
+
+      describe("StudyDetails", function () {
+          var studySnapshot;
+
+          before(function () {
+              var studySnapshotText =  
+                    "<study xmlns='http://bioinformatics.northwestern.edu/ns/psc' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' assigned-identifier='NCI-999' last-modified-date='2010-02-23T15:23:50.867Z' provider='alpha' >" +
+                    "  <long-title>I am the loooong title.</long-title>\n" +
+                    "  <amendment name='[Original]' date='2010-02-22' mandatory='true' released-date='2010-02-23T15:23:50.867Z'/>\n" +
+                    "  <amendment name='AMEND-1' date='2010-02-23' mandatory='true' released-date='2010-02-24T15:23:50.867Z'/>\n" +
+                    "  <development-amendment name='DEVO' date='2010-02-24' mandatory='true' released-date='2010-02-25T15:23:50.867Z'/>\n" +
+                    "</study>";
+//var studySnapshotText =
+//"<?xml version='1.0' encoding='UTF-8'?>\n" +
+//"<study xmlns='http://bioinformatics.northwestern.edu/ns/psc' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' assigned-identifier='NCT00003641' last-modified-date='2010-02-23T15:23:50.867Z' provider='mock - NOT FOR PRODUCTION' xsi:schemaLocation='http://bioinformatics.northwestern.edu/ns/psc http://bioinformatics.northwestern.edu/ns/psc/psc.xsd'>\n" +
+//"  <long-title>Phase III Randomized Study of Four Weeks of High Dose Interferon Alfa-2b in Stage TN ,TN, TN, and T, N (Microscopic) Melanoma</long-title>\n" +
+//"  <secondary-identifier type='nct' value='NCT00003641'/>\n" +
+//"  <secondary-identifier type='org_study' value='CDR0000066727'/>\n" +
+//"  <secondary-identifier type='secondary' value='CALGB-500103'/>\n" +
+//"  <secondary-identifier type='secondary' value='CAN-NCIC-ME10'/>\n" +
+//"  <secondary-identifier type='secondary' value='COG-E1697'/>\n" +
+//"  <secondary-identifier type='secondary' value='ECOG-1697'/>\n" +
+//"  <secondary-identifier type='secondary' value='SWOG-E1697'/>\n" +
+//"  <planned-calendar id='312cc213-1721-4d1d-8715-655181cfcb57'/>\n" +
+//"  <amendment name='[Original]' date='2010-02-22' mandatory='true' released-date='2010-02-23T15:23:50.867Z'>\n" +
+//"    <planned-calendar-delta id='5dc0779f-2917-44ab-b44b-097fc9597758' node-id='312cc213-1721-4d1d-8715-655181cfcb57'>\n" +
+//"      <add id='e57779cf-bf4c-41d4-83c0-6f3af91f87f4' index='0'>\n" +
+//"        <epoch id='94a698d9-73de-4acd-adda-ecfa39f16305' name='Treatment'>\n" +
+//"          <study-segment id='908ca8d4-2bb6-4e79-8e1d-9df5dd06b706' name='A'>\n" +
+//"            <period id='4ea1bb41-5463-46e2-b3d2-ecaeefd38081' repetitions='1' start-day='1' duration-quantity='1' duration-unit='day'>\n" +
+//"              <planned-activity id='d9046094-bda8-404f-9f00-22cb22d08aba' day='1'>\n" +
+//"                <activity name='Bone Marrow Aspirate' code='961' type='Disease Measure' source='Northwestern University'/>\n" +
+//"              </planned-activity>\n" +
+//"            </period>\n" +
+//"          </study-segment>\n" +
+//"        </epoch>\n" +
+//"      </add>\n" +
+//"    </planned-calendar-delta>\n" +
+//"  </amendment>\n" +
+//"</study>";
+
+//              var studySnapshotText = "<?xml version='1.0' encoding='UTF-8'?>\n<study assigned-identifier='NCI-999' xmlns='http://bioinformatics.northwestern.edu/ns/psc' xsi:schemaLocation='http://bioinformatics.northwestern.edu/ns/psc http://bioinformatics.northwestern.edu/ns/psc/psc.xsd'></study>";
+//              var studySnapshotText = "<study assigned-identifier=\"NCT00003641\" last-modified-date=\"2010-02-23T15:23:50.867Z\" provider=\"mock - NOT FOR PRODUCTION\" xsi:schemaLocation=\"http://bioinformatics.northwestern.edu/ns/psc http://bioinformatics.northwestern.edu/ns/psc/psc.xsd\"></study>";
+              studySnapshot = XML.parse(studySnapshotText);
+          });
+
+          it ("generates a JSON object from a study snaphot XML", function() {
+              var result = psc.admin.ps.StudyDetails.generateStudyJSON(studySnapshot);
+              expect(result['provider']).to(equal, "alpha");
+              expect(result['assigned-identifier']).to(equal, "NCI-999");
+              expect(result['long-title']).to(equal, "I am the loooong title.");
+
+              expect(result['amendment'].length).to(equal, 2)
+
+              expect(result['amendment'][0]['name']).to(equal, '[Original]')
+              expect(result['amendment'][0]['released-date']).to(equal, '2010-02-23T15:23:50.867Z')
+
+              expect(result['amendment'][1]['name']).to(equal, 'AMEND-1')
+              expect(result['amendment'][1]['released-date']).to(equal, '2010-02-24T15:23:50.867Z')
+
+              expect(result['development-amendment'].length).to(equal, 1)
+
+              expect(result['development-amendment'][0]['name']).to(equal, 'DEVO')
+              expect(result['development-amendment'][0]['released-date']).to(equal, '2010-02-25T15:23:50.867Z')
+          });
+
+          it("populates the study details fields", function () {
+              expect(jQuery('#study-long-title').html()).to(equal, 'Loading...')
+              expect(jQuery('#study-assigned-identifier').html()).to(equal, 'Loading...')
+              expect(jQuery('#study-amendment-count').html()).to(equal, 'Loading...')
+
+              psc.admin.ps.StudyDetails.populateTitleAndIdentifier({'assigned-identifier': 'NCI-999', 'long-title':'Long title.', amendment:['[Original]', 'First']});
+
+              expect(jQuery('#study-long-title').html()).to(equal, 'Long title.')
+              expect(jQuery('#study-assigned-identifier').html()).to(equal, 'NCI-999')
+              expect(jQuery('#study-amendment-count').html()).to(equal, 1)
+          });
+
+          it("clears the study details fields", function() {
+              jQuery('#study-assigned-identifier').html("A")
+              jQuery('#study-long-title').html("B")
+              jQuery('#study-amendment-count').html("C")
+
+              psc.admin.ps.StudyDetails.clearDetails();
+
+              expect(jQuery('#study-long-title').html()).to(equal, 'Loading...')
+              expect(jQuery('#study-assigned-identifier').html()).to(equal, 'Loading...')
+              expect(jQuery('#study-amendment-count').html()).to(equal, 'Loading...')
+          });
+
+          it("populates the study site details", function() {
+              expect(jQuery('#associated-sites').html()).to(equal, 'Loading...');
+
+              psc.admin.ps.StudyDetails.populateSiteAssociations({})
+
+//              expect
+              
+          });
+      });
+
     });
 
   })(jQuery, $);
--- web/src/main/webapp/js/admin/purge-study/purge-study.js
+++ web/src/main/webapp/js/admin/purge-study/purge-study.js
@@ -1,82 +1,155 @@
-psc.namespace("admin");
+psc.namespace("admin.ps");
 
-psc.admin.PurgeStudy = (function ($) {
+psc.admin.ps.StudyAutocompleter = (function ($) {
+    var resourceUri = "/api/v1/studies";
 
-  StudyAutocompleter = (function ($) {
-      var resourceUri = "/api/v1/studies";
+    var acInputIdentifier = "studies-autocompleter-input";
+    var acResultsIdentifier = "studies-autocompleter-div";
 
-      var acInputIdentifier = "studies-autocompleter-input";
-      var acResultsIdentifier = "studies-autocompleter-div";
+    function studyAutocompleterChoiceProcessing(callback) {
+        var searchString = $F(acInputIdentifier )
+        if (searchString == "Search for study") {
+            searchString = ""
+        }
 
-      function studyAutocompleterChoiceProcessing(callback) {
-          var searchString = $F(acInputIdentifier )
-          if (searchString == "Search for study") {
-              searchString = ""
-          }
+        var uri = SC.relativeUri(resourceUri)
+        if (searchString.blank()) {
+            return;
+        }
 
-          var uri = SC.relativeUri(resourceUri)
-          if (searchString.blank()) {
-              return;
-          }
+        var params = {};
+        if (!searchString.blank()) {
+            params.q = searchString;
+        }
 
-          var params = {};
-          if (!searchString.blank()) {
-              params.q = searchString;
-          }
+        SC.asyncRequest(uri+".json", {
+            method: "GET", parameters: params,
+            onSuccess: function(response) {
+                callback(response.responseJSON.studies)
+            }
+        })
+    }
 
-          SC.asyncRequest(uri+".json", {
-              method: "GET", parameters: params,
-              onSuccess: function(response) {
-                  callback(response.responseJSON.studies)
-              }
-          })
-      }
+    function createAutocompleter() {
+        new SC.FunctionalAutocompleter(
+                acInputIdentifier, acResultsIdentifier, studyAutocompleterChoices, {
+            afterUpdateElement: function(input, selected) {
+                input.value = "";
+                input.focus();
+                $('#' + acResultsIdentifier).hide()
 
-      function createAutocompleter() {
-         new SC.FunctionalAutocompleter(
-             acInputIdentifier, acResultsIdentifier, studyAutocompleterChoices, {
-                 afterUpdateElement: function(input, selected) {
-                     input.value = "";
-                     input.focus();
-                     $('#' + acResultsIdentifier).hide()
+                $('#' + acInputIdentifier).trigger("autocompleter-study-selected", {study:{assigned_identifier: selected.innerHTML, id: selected.id}});
+            }
+        });
+    }
 
-                     alert(selected.id);
-                 }
-             }
-         );
-      }
+    function studyAutocompleterChoices(str, callback) {
+        studyAutocompleterChoiceProcessing(function(data) {
+            var lis = data.map(function(study) {
+                var id = study.id
+                var name = study.assigned_identifier
+                var listItem = "<li id='"  + id + "'>" + name + "</li>";
+                return listItem
+            }).join("\n");
 
-      function studyAutocompleterChoices(str, callback) {
-              studyAutocompleterChoiceProcessing(function(data) {
-              var lis = data.map(function(study) {
-                        var id = study.id
-                        var name = study.assigned_identifier
-                        var listItem = "<li id='"  + id + "'>" + name + "</li>";
-                        return listItem
-              }).join("\n");
+            callback("<ul>\n" + lis + "\n</ul>");
+        });
+    }
 
-              callback("<ul>\n" + lis + "\n</ul>");
+    return {
+        init: function() {
+            $(document).ready(function() {
+                createAutocompleter();
             });
-      }
+        }
+    };
+})(jQuery);
 
-      function populateStudyDetails(studyId) {
-          
-      }
+psc.admin.ps.StudyDetails = (function ($) {
+    var acInputIdentifier = "studies-autocompleter-input";
+    var studyResourceUri = "/api/v1/studies/{assigned-identifier}/template"
+    var studySitesResourceUri = "api/v1/studies/{study-identifier}/sites";  // Have to creates
 
-      return {
-          init: function() {
-              createAutocompleter();
-          }
-      };
-  })($);
+    function createStudyInputListener() {
+        $('#' + acInputIdentifier).bind("autocompleter-study-selected", updateStudyDetails(event))
+    }
+
+    function updateStudyDetails(event) {
+        clearDetails();
+
+        if (event == null || event.data == null || event.data.study == null) {
+            return;
+        }
+
+        getStudyDetails(event.data.study, populateStudyDetails);
+    }
+
+    function getStudyDetails(study, callback) {
+        if (study == null || study.assigned_identifier == null || study.assigned_identifier.blank()) {
+            return;
+        }
+
+        showDetails();
+
+        var assignedIdentifier = event.study.assigned_identifier;
+
+        var uri = SC.relativeUri(
+                studyResourceUri .replace("\{assigned-identifier\}", assignedIdentifier)
+                )
+
+        $.get(uri, function(data) {
+            // callback(generateStudyJSON(data));
+        })
 
 
-  return {
-      init: function() {
-          $(document).ready(function() {
-                StudyAutocompleter.init();
-          });
-      }
-  };
+    }
 
+    return {
+        init: function() {
+            $(document).ready(function() {
+                createStudyInputListener();
+            });
+
+        },
+
+        generateStudyJSON: function(xml) {
+            var studies = SC.objectifyXml("study", xml, function(elt, study) {
+                var longTitles = SC.objectifyXml("long-title", xml, function(elt, holder) {
+                    if (elt.childNodes[0] != null) {
+                        study['long-title'] = elt.childNodes[0].nodeValue; // The text node
+                    }
+                });
+
+                study['amendment'] = SC.objectifyXml("amendment", xml);
+                study['development-amendment'] = SC.objectifyXml("development-amendment", xml);
+            });
+
+            return studies[0];
+        },
+
+        populateTitleAndIdentifier: function(study) {
+            $('#study-assigned-identifier').html(study['assigned-identifier'])
+            $('#study-long-title').html(study['long-title'])
+
+            var amendmentCount = study['amendment'].length;
+            if (amendmentCount > 0) {
+                amendmentCount = amendmentCount - 1; // Don't count [Original] Amendment
+            }
+
+            $('#study-amendment-count').html(amendmentCount);
+        },
+
+        hideyDetails: function() {
+            $('#study-details').hide();
+        },
+        showDetails: function() {
+            $('#study-details').hide();
+        },
+
+        clearDetails: function() {
+            $('#study-assigned-identifier').html("Loading...");
+            $('#study-long-title').html("Loading...");
+            $('#study-amendment-count').html("Loading...");
+        }
+    }
 })(jQuery);
