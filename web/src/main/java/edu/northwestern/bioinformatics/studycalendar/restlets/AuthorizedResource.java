4ee4b9635790715e0e1362570c370a53082cc614
Rhett Sutphin <r-sutphin@northwestern.edu>
2010-07-13 23:11:55 +0000

    Doc typo.

--- web/src/main/java/edu/northwestern/bioinformatics/studycalendar/restlets/AuthorizedResource.java
+++ web/src/main/java/edu/northwestern/bioinformatics/studycalendar/restlets/AuthorizedResource.java
@@ -14,7 +14,7 @@ public interface AuthorizedResource {
     /**
      * Returns a collection of {@link ResourceAuthorization}s for
      * the specified method on this resource.  If it returns an empty collection,
-     * no roles/scopes are permitted to perform the method.. If it returns null, any
+     * no roles/scopes are permitted to perform the method. If it returns null, any
      * role/scope is permitted.
      * <p>
      * Implementors are not required to ensure that it returns the correct value
