1cca96ecdaa24e99ef6242a1e7319e77cb3bea5a
Rhett Sutphin <r-sutphin@northwestern.edu>
2010-08-02 21:48:32 +0000

    Flip condition where we set the end date -- it should be for users that are inactive.  Recloses #1054.

--- database/src/main/db/migrate/019_2.8/003_update_csm_user_name_and_end_date.groovy
+++ database/src/main/db/migrate/019_2.8/003_update_csm_user_name_and_end_date.groovy
@@ -4,22 +4,18 @@ class UpdateCsmUserNameAndEndDate extends edu.northwestern.bioinformatics.bering
             execute("UPDATE csm_user SET first_name=(SELECT (CASE WHEN u.first_name IS NULL THEN csm_user.first_name ELSE u.first_name END) FROM users u WHERE u.csm_user_id=csm_user.user_id)");
             execute("UPDATE csm_user SET last_name=(SELECT (CASE WHEN u.last_name IS NULL THEN csm_user.last_name ELSE u.last_name END) FROM users u WHERE u.csm_user_id=csm_user.user_id)");
 
-            if (databaseMatches('hsqldb')) {
-                execute("UPDATE csm_user cu SET end_date = CURRENT_TIMESTAMP WHERE user_id in (SELECT csm_user_id FROM users WHERE active_flag = 't')");
-            } else {
-                execute("UPDATE csm_user cu SET end_date = CURRENT_TIMESTAMP WHERE user_id in (SELECT csm_user_id FROM users WHERE active_flag = 1)");
-            }
+            execute("UPDATE csm_user cu SET end_date = CURRENT_TIMESTAMP WHERE user_id in (SELECT csm_user_id FROM users WHERE active_flag = 0)");
         } else {
             execute("UPDATE csm_user cu1 SET first_name=temp.first_name FROM (SELECT u.csm_user_id, u.first_name FROM users u, csm_user cu WHERE cu.user_id = u.csm_user_id AND (u.first_name is not null AND u.first_name!='')) temp WHERE temp.csm_user_id=cu1.user_id");
             execute("UPDATE csm_user cu1 SET last_name=temp.last_name FROM (SELECT u.csm_user_id, u.last_name FROM users u, csm_user cu WHERE cu.user_id = u.csm_user_id AND (u.last_name is not null AND u.last_name!='')) temp WHERE temp.csm_user_id=cu1.user_id");
 
-            execute("UPDATE csm_user cu SET end_date = CURRENT_TIMESTAMP WHERE user_id in (SELECT csm_user_id FROM users WHERE active_flag = 't')");
+            execute("UPDATE csm_user cu SET end_date = CURRENT_TIMESTAMP WHERE user_id in (SELECT csm_user_id FROM users WHERE active_flag = false)");
         }
     }
 
     void down() {
-        execute("update csm_user set first_name='.' where first_name != '.'")
-        execute("update csm_user set last_name='.' where last_name != '.'")
-        execute("update csm_user set end_date=null where end_date is not null")
+        execute("update csm_user set first_name='.'")
+        execute("update csm_user set last_name='.'")
+        execute("update csm_user set end_date = null")
     }
 }
