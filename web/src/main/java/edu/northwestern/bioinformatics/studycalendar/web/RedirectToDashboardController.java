4bba2ec547fd94345ce6b19e0eb98884c29240f1
Rhett Sutphin <r-sutphin@northwestern.edu>
2010-08-09 16:58:26 +0000

    Remove definitely unnecessary dashboard-related bits.  #1105.

--- web/src/main/java/edu/northwestern/bioinformatics/studycalendar/web/RedirectToDashboardController.java
+++ web/src/main/java/edu/northwestern/bioinformatics/studycalendar/web/RedirectToDashboardController.java
@@ -1,35 +1,25 @@
 package edu.northwestern.bioinformatics.studycalendar.web;
 
-import edu.northwestern.bioinformatics.studycalendar.dao.ScheduledActivityDao;
-import edu.northwestern.bioinformatics.studycalendar.dao.UserDao;
-import edu.northwestern.bioinformatics.studycalendar.domain.*;
+import edu.northwestern.bioinformatics.studycalendar.domain.Role;
 import edu.northwestern.bioinformatics.studycalendar.web.accesscontrol.PscAuthorizedHandler;
 import edu.northwestern.bioinformatics.studycalendar.web.accesscontrol.ResourceAuthorization;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
-import org.springframework.web.servlet.view.RedirectView;
-import org.springframework.beans.factory.annotation.Required;
-import org.acegisecurity.context.SecurityContext;
-import org.acegisecurity.context.SecurityContextHolder;
 import org.acegisecurity.Authentication;
 import org.acegisecurity.GrantedAuthority;
+import org.acegisecurity.context.SecurityContext;
+import org.acegisecurity.context.SecurityContextHolder;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.ArrayList;
 import java.util.Map;
 
-import static edu.northwestern.bioinformatics.studycalendar.security.authorization.PscRole.SYSTEM_ADMINISTRATOR;
-import static edu.northwestern.bioinformatics.studycalendar.security.authorization.PscRole.STUDY_SUBJECT_CALENDAR_MANAGER;
-import static edu.northwestern.bioinformatics.studycalendar.security.authorization.PscRole.STUDY_TEAM_ADMINISTRATOR;
-
+import static edu.northwestern.bioinformatics.studycalendar.security.authorization.PscRole.*;
 
 public class RedirectToDashboardController extends PscAbstractController implements PscAuthorizedHandler {
-
     //todo - need to verify
     public Collection<ResourceAuthorization> authorizations(String httpMethod, Map<String, String[]> queryParameters) {
         return ResourceAuthorization.createCollection(SYSTEM_ADMINISTRATOR, STUDY_SUBJECT_CALENDAR_MANAGER, STUDY_TEAM_ADMINISTRATOR);
@@ -49,11 +39,11 @@ public class RedirectToDashboardController extends PscAbstractController impleme
         } else if (authorityList.contains(Role.SITE_COORDINATOR.toString())){
            return new ModelAndView(new RedirectView("dashboard/siteCoordinator"));
         } else if (authorityList.contains(Role.SYSTEM_ADMINISTRATOR.toString())){
-           return new ModelAndView(new RedirectView("dashboard/systemAdmin"));
+           return new ModelAndView("redirectToAdministration");
         } else if (authorityList.contains(Role.STUDY_ADMIN.toString())){
-           return new ModelAndView(new RedirectView("dashboard/studyAdmin"));
+           return new ModelAndView("redirectToStudyList");
         } else if (authorityList.contains(Role.STUDY_COORDINATOR.toString())){
-           return new ModelAndView(new RedirectView("dashboard/studyCoordinator"));
+           return new ModelAndView("redirectToStudyList");
         }
         return null;
     }
