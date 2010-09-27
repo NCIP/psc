d0903261720ade795f97527a5620517596c2afb9
Rhett Sutphin <r-sutphin@northwestern.edu>
2010-09-27 19:49:10 +0000

    Undeprecate this test.  #1073.
    It was originally for testing a particularly legacy situation, but it doesn't hurt to keep it around.

--- core/src/test/java/edu/northwestern/bioinformatics/studycalendar/service/PscUserServiceIntegratedTest.java
+++ core/src/test/java/edu/northwestern/bioinformatics/studycalendar/service/PscUserServiceIntegratedTest.java
@@ -1,15 +1,11 @@
 package edu.northwestern.bioinformatics.studycalendar.service;
 
 import edu.northwestern.bioinformatics.studycalendar.core.DaoTestCase;
-import edu.northwestern.bioinformatics.studycalendar.dao.SiteDao;
 import edu.northwestern.bioinformatics.studycalendar.security.authorization.PscUser;
-
-import java.util.List;
 
 /**
  * @author Rhett Sutphin
  */
-@Deprecated
 public class PscUserServiceIntegratedTest extends DaoTestCase {
     private PscUserService service;
 
