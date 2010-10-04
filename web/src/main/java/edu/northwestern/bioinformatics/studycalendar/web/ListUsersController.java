144307bd149982b5858698d4b28a270f991d934b
Rhett Sutphin <r-sutphin@northwestern.edu>
2010-10-04 21:16:07 +0000

    Simplify controller since the page is all dynamic now.  #1129.

--- web/src/main/java/edu/northwestern/bioinformatics/studycalendar/web/ListUsersController.java
+++ web/src/main/java/edu/northwestern/bioinformatics/studycalendar/web/ListUsersController.java
@@ -1,9 +1,7 @@
 package edu.northwestern.bioinformatics.studycalendar.web;
 
-import edu.northwestern.bioinformatics.studycalendar.service.PscUserService;
 import edu.northwestern.bioinformatics.studycalendar.web.accesscontrol.PscAuthorizedHandler;
 import edu.northwestern.bioinformatics.studycalendar.web.accesscontrol.ResourceAuthorization;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,23 +11,13 @@ import java.util.Map;
 
 import static edu.northwestern.bioinformatics.studycalendar.security.authorization.PscRole.*;
 
-public class ListUsersController extends PscSimpleFormController implements PscAuthorizedHandler {
-    private PscUserService pscUserService;
-
-    @Override
+public class ListUsersController extends PscAbstractController implements PscAuthorizedHandler {
     public Collection<ResourceAuthorization> authorizations(String httpMethod, Map<String, String[]> queryParameters) {
         return ResourceAuthorization.createCollection(SYSTEM_ADMINISTRATOR, USER_ADMINISTRATOR);
     }
 
     @Override
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return new ModelAndView("admin/users", "users", pscUserService.getAllUsers());
-    }
-
-    ////// CONFIGURATION
-
-    @Required
-    public void setPscUserService(PscUserService pscUserService) {
-        this.pscUserService = pscUserService;
+        return new ModelAndView("admin/users");
     }
 }
