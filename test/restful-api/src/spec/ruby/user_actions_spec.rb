90ecffef2c2284994fd7f23c257f9ae674a1c7c8
John Dzak <j-dzak@northwestern.edu>
2010-12-17 21:36:07 +0000

    Starting with failing integration spec.

---
+++ test/restful-api/src/spec/ruby/user_actions_spec.rb
@@ -0,0 +1,23 @@
+describe "/user-actions" do
+  describe "POST" do
+
+    def user_action(description, context)
+      <<-JSON
+        {
+          description: "Delayed 45 activities for Jo Carlson by 6 days",
+          context: "http://fake.us/api/v1/subjects/0000001/schedules"
+        }
+      JSON
+    end
+
+    it "should successfully create a user action" do
+      action = user_action("Delayed 45 activities for Jo Carlson by 6 days", "http://fake.us/api/v1/subjects/0000001/schedules")
+
+      post "/user-actions/", action, 'Content-Type' => 'application/json'
+      response.status_code.should == 201
+
+      # response.status_message.should == "Created"
+      # response.meta['location'].should =~ %r{api/v1/studies/NU480/schedules/#{@studySubjectAssignment.gridId}/activities/#{@scheduled_activity.gridId}$}
+    end
+  end
+end
