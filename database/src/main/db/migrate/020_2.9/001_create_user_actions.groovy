293458c7b0857f2186f5527d8f849f53afc6b027
John Dzak <j-dzak@northwestern.edu>
2010-12-17 21:36:23 +0000

    Changed UserAction to not have a fk constraint for user and updated the user_id field to be csm_user_id. Feature #1281.

--- database/src/main/db/migrate/020_2.9/001_create_user_actions.groovy
+++ database/src/main/db/migrate/020_2.9/001_create_user_actions.groovy
@@ -2,14 +2,13 @@ class CreateUserActions extends edu.northwestern.bioinformatics.bering.Migration
     void up() {
         createTable("user_actions") { t ->
             t.addVersionColumn()
-            t.addColumn("user_id", "integer", nullable: false)
+            t.addColumn("csm_user_id", "integer", nullable: false)
             t.addColumn("uri", "string", nullable:false)
             t.addColumn("description", "string", nullable:false)
             t.addColumn("action_type", "string", nullable:false)
             t.addColumn("undone", "boolean", nullable:false, defaultValue:false)
             t.addColumn("grid_id", "string", limit: 255)
         }
-        execute("ALTER TABLE user_actions ADD CONSTRAINT fk_user_actions_users FOREIGN KEY (user_id) REFERENCES users");
     }
 
     void down() {
--- domain/src/main/java/edu/northwestern/bioinformatics/studycalendar/domain/UserAction.java
+++ domain/src/main/java/edu/northwestern/bioinformatics/studycalendar/domain/UserAction.java
@@ -1,11 +1,14 @@
 package edu.northwestern.bioinformatics.studycalendar.domain;
 
+import edu.northwestern.bioinformatics.studycalendar.StudyCalendarSystemException;
 import gov.nih.nci.cabig.ctms.domain.AbstractMutableDomainObject;
+import gov.nih.nci.security.authorization.domainobjects.User;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "user_actions")
@@ -14,6 +17,8 @@ import javax.persistence.Table;
 )
 public class UserAction extends AbstractMutableDomainObject {
     private String description;
+    private Integer csmUserId;
+    private User resolvedUser;
 
     public String getDescription() {
         return description;
@@ -21,5 +26,41 @@ public class UserAction extends AbstractMutableDomainObject {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public Integer getCsmUserId() {
+        return csmUserId;
+    }
+
+    public void setCsmUserId(Integer csmUserId) {
+        this.csmUserId = csmUserId;
+    }
+
+
+    /*
+     TODO: I would prefer that this field's type be PscUser.  However, that would introduce
+     a dependency on psc:authorization from psc:domain.  psc:authorization's legacy mode support
+     forces a dependency on psc:domain, so the reverse dependency isn't possible until legacy
+     mode can be completely removed.
+    */
+
+    @Transient
+    public gov.nih.nci.security.authorization.domainobjects.User getUser() {
+        if (resolvedUser != null) {
+            return resolvedUser;
+        } else if (getCsmUserId() == null) {
+            return null;
+        } else {
+            throw new StudyCalendarSystemException("The actual manager user object has not been externally resolved for this assignment");
+        }
+    }
+
+    public void setUser(gov.nih.nci.security.authorization.domainobjects.User csmUser) {
+        this.resolvedUser = csmUser;
+        if (csmUser != null) {
+            setCsmUserId(csmUser.getUserId().intValue());
+        } else {
+            setCsmUserId(null);
+        }
     }
 }
