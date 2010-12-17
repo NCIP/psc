6035a570c24f32ffa9ee616fb787a7435d615160
John Dzak <j-dzak@northwestern.edu>
2010-12-17 21:36:20 +0000

    Removing 201 test from unit test since it will be tested in the integration test. Feature #1281.

--- web/src/test/java/edu/northwestern/bioinformatics/studycalendar/restlets/UserActionsResourceTest.java
+++ web/src/test/java/edu/northwestern/bioinformatics/studycalendar/restlets/UserActionsResourceTest.java
@@ -1,23 +1,6 @@
 package edu.northwestern.bioinformatics.studycalendar.restlets;
 
-import edu.northwestern.bioinformatics.studycalendar.security.authorization.PscUser;
-import edu.northwestern.bioinformatics.studycalendar.service.PscUserService;
-import org.json.JSONObject;
-import org.restlet.data.Status;
-import org.restlet.ext.json.JsonRepresentation;
-
 public class UserActionsResourceTest extends AuthorizedResourceTestCase<UserActionsResource> {
-    private PscUserService pscUserService;
-    private static final String USERNAME = "TestUser";
-    private PscUser user;
-
-//    public void setUp() throws Exception {
-//        super.setUp();
-//        pscUserService = registerMockFor(PscUserService.class);
-//        user = createPscUser(USERNAME);
-//        request.getAttributes().put(UriTemplateParameters.USERNAME.attributeName(), USERNAME);
-//    }
-
     @Override
     @SuppressWarnings({ "unchecked" })
     protected UserActionsResource createAuthorizedResource() {
@@ -27,11 +10,4 @@ public class UserActionsResourceTest extends AuthorizedResourceTestCase<UserActi
     public void testPostAllowed() throws Exception {
         assertAllowedMethods("POST");
     }
-
-    public void test201WhenAddingUserAction() throws Exception {
-        request.setEntity(new JsonRepresentation(new JSONObject()));
-        doPost();
-        assertResponseStatus(Status.SUCCESS_CREATED);
-    }
-
 }
