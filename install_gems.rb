4fc38d585fa1e36f7faf76931a0d8efc32eca8bd
Rhett Sutphin <r-sutphin@northwestern.edu>
2011-01-10 03:54:20 +0000

    Provide a separate script to install the gems PSC depends on.  Done with #1091.

---
+++ install_gems.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+####
+# Installs the gems that PSC's build depends on, as expressed in build.yaml.
+
+require 'yaml'
+
+require 'rubygems'
+require 'rubygems/dependency_installer'
+
+BUILD_YAML = File.expand_path("../build.yaml", __FILE__)
+
+YAML.load(File.read(BUILD_YAML))["gems"].
+  collect { |gem_entry| gem_entry.split(/\s+/, 2) }.
+  each do |gem, version|
+    if Gem.available?(gem, version)
+      $stderr.puts "Already installed #{[gem, version].compact.join(' ')}"
+    else
+      inst = Gem::DependencyInstaller.new
+      inst.install gem, version
+
+      inst.installed_gems.each do |spec|
+        $stderr.puts "Successfully installed #{spec.full_name}"
+      end
+    end
+  end
