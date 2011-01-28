252eccac9ed1b9e43304997ae6286f8aabc117cc
Jalpa Patel <jalpa-patel@northwestern.edu>
2011-01-28 18:35:25 +0000

    Implement DeletableDomainObjectDao for notification dao. #1314.

--- core/src/main/java/edu/northwestern/bioinformatics/studycalendar/dao/NotificationDao.java
+++ core/src/main/java/edu/northwestern/bioinformatics/studycalendar/dao/NotificationDao.java
@@ -2,10 +2,11 @@ package edu.northwestern.bioinformatics.studycalendar.dao;
 
 import edu.northwestern.bioinformatics.studycalendar.domain.Notification;
 
+import java.util.List;
 /**
  * @author Saruabh Agrawal
  */
-public class NotificationDao extends StudyCalendarMutableDomainObjectDao<Notification> {
+public class NotificationDao extends StudyCalendarMutableDomainObjectDao<Notification> implements DeletableDomainObjectDao<Notification> {
     @Override
     public Class<Notification> domainClass() {
         return Notification.class;
@@ -13,5 +14,18 @@ public class NotificationDao extends StudyCalendarMutableDomainObjectDao<Notific
 
     public void save(final Notification notification) {
 
+    }
+
+    /**
+    * Deletes notification
+    *
+    * @param  notification the activity to delete
+    */
+    public void delete(Notification notification) {
+        getHibernateTemplate().delete(notification);
+    }
+
+    public void deleteAll(List<Notification> t) {
+        getHibernateTemplate().deleteAll(t);
     }
 }
