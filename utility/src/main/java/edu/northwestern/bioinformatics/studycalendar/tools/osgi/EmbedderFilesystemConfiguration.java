3838033ee5d4282a2e66ffff10b4bfb8d9c988e3
Rhett Sutphin <r-sutphin@northwestern.edu>
2011-04-07 06:33:51 +0000

    Implement Embedder configuration based on a particular directory structure. #1284.

---
+++ utility/src/main/java/edu/northwestern/bioinformatics/studycalendar/tools/osgi/EmbedderFilesystemConfiguration.java
@@ -0,0 +1,159 @@
+package edu.northwestern.bioinformatics.studycalendar.tools.osgi;
+
+import edu.northwestern.bioinformatics.studycalendar.StudyCalendarSystemException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Embedder configuration based on a particular filesystem layout. It supports
+ * automatically installing or starting bundles found in a particular directory
+ * structure. The directories under the bundle root are expected to be named like so:
+ * {start-level}_{name}/{initial_state}.
+ * E.g.:
+ *
+ * <pre>
+ *     [root]/
+ *          framework.properties
+ *          001_system/
+ *              start/
+ *                  org.apache.felix.shell-1.2.0.jar
+ *                  org.apache.felix.shell.tui-1.2.0.jar
+ *                  ...
+ *              install/
+ *                  org.apache.felix.shell.remote-1.0.4.jar
+ *                  ...
+ *          002_infrastructure/
+ *              start/
+ *                  ...
+ *              install/
+ *                  ...
+ * </pre>
+ *
+ * The <tt>_{name}</tt> part of the first level directory names is optional -- it's just for your
+ * future reference. The <tt>{start-level}</tt> part is the desired start level for the bundles
+ * as defined by the OSGi Start Level service. It must be a positive integer.
+ * <p>
+ * The optional file <tt>framework.properties</tt> defines any additional properties you need to
+ * pass to {@link org.osgi.framework.launch.FrameworkFactory#newFramework}.
+ *
+ * @author Rhett Sutphin
+ */
+public class EmbedderFilesystemConfiguration implements EmbedderConfiguration {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private static final Pattern INITIAL_DIGITS = Pattern.compile("^\\d+");
+
+    private File root;
+
+    private Map<String, Object> frameworkProperties;
+    private Collection<InstallableBundle> installableBundles;
+
+    public Map<String, Object> getFrameworkProperties() {
+        if (frameworkProperties == null) {
+            loadFrameworkProperties();
+        }
+
+        return frameworkProperties;
+    }
+
+    @SuppressWarnings( { "unchecked" })
+    private synchronized void loadFrameworkProperties() {
+        if (frameworkProperties != null) return;
+        File propFile = new File(root, "framework.properties");
+        Properties props = new Properties();
+        try {
+            props.load(new FileReader(propFile));
+        } catch (FileNotFoundException e) {
+            log.debug("No properties file ({}) in embedder configuration root", propFile);
+            frameworkProperties = Collections.emptyMap();
+        } catch (IOException e) {
+            throw new StudyCalendarSystemException("Could not read %s", propFile, e);
+        }
+        if (frameworkProperties == null) {
+            frameworkProperties = new HashMap(props);
+        }
+    }
+
+    public Collection<InstallableBundle> getBundlesToInstall() {
+        if (installableBundles == null) {
+            loadInstallableBundles();
+        }
+
+        return installableBundles;
+    }
+
+    private synchronized void loadInstallableBundles() {
+        if (installableBundles != null) return;
+        installableBundles = new LinkedList<InstallableBundle>();
+
+        for (File levelDir : root.listFiles(LevelDirectoryFilter.INSTANCE)) {
+            installableBundles.addAll(findBundlesForLevel(levelDir));
+        }
+    }
+
+    private Collection<InstallableBundle> findBundlesForLevel(File levelDir) {
+        Collection<InstallableBundle> bundles = new LinkedList<InstallableBundle>();
+        int level = extractLevelFromDirectory(levelDir.getName());
+
+        findBundlesForState(bundles, new File(levelDir, "install"), level, false);
+        findBundlesForState(bundles, new File(levelDir, "start"), level, true);
+
+        return bundles;
+    }
+
+    private void findBundlesForState(
+        Collection<InstallableBundle> bundles, File stateDir, int level, boolean shouldStart
+    ) {
+        File[] jars = stateDir.listFiles(new JarFilter());
+        if (jars == null) return;
+        for (File jar : jars) {
+            bundles.add(new InstallableBundleImpl(level, jar.toURI().toString(), shouldStart));
+        }
+    }
+
+    private int extractLevelFromDirectory(String name) {
+        Matcher matcher = INITIAL_DIGITS.matcher(name);
+        matcher.find();
+        return Integer.parseInt(matcher.group(0));
+    }
+
+    ////// CONFIGURATION
+
+    public void setRoot(File baseDir) {
+        this.root = baseDir;
+    }
+
+    private static class LevelDirectoryFilter implements FileFilter {
+        public static final FileFilter INSTANCE = new LevelDirectoryFilter();
+
+        private LevelDirectoryFilter() { }
+
+        public boolean accept(File file) {
+            return file.isDirectory() && Character.isDigit(file.getName().charAt(0));
+        }
+    }
+
+    private static class JarFilter implements FileFilter {
+        public static final FileFilter INSTANCE = new LevelDirectoryFilter();
+
+        private JarFilter() { }
+
+        public boolean accept(File file) {
+            return file.isFile() && file.getName().endsWith(".jar");
+        }
+    }
+}
---
+++ utility/src/test/java/edu/northwestern/bioinformatics/studycalendar/tools/osgi/EmbedderFilesystemConfigurationTest.java
@@ -0,0 +1,144 @@
+package edu.northwestern.bioinformatics.studycalendar.tools.osgi;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+/**
+ * @author Rhett Sutphin
+ */
+public class EmbedderFilesystemConfigurationTest {
+    private EmbedderFilesystemConfiguration configuration;
+
+    @Before
+    public void before() throws Exception {
+        configuration = new EmbedderFilesystemConfiguration();
+
+        setTestRootDir("embedder_test_root");
+    }
+
+    @Test
+    public void itIncludesThePropertiesFromTheFile() throws Exception {
+        assertThat(configuration.getFrameworkProperties(), hasEntry("foo.quux", (Object) "baz"));
+    }
+
+    @Test
+    public void itHasNoPropertiesWhenFrameworkPropertiesFileNotPresent() throws Exception {
+        setTestRootDir("embedder_empty_test_root");
+        assertThat(configuration.getFrameworkProperties().isEmpty(), is(true));
+    }
+
+    @Test
+    public void itHasNoPropertiesWhenTheTestRootDoesNotExist() throws Exception {
+        configuration.setRoot(new File("does/not/exist"));
+        assertThat(configuration.getFrameworkProperties().isEmpty(), is(true));
+    }
+
+    @Test
+    public void itFindsAllTheLevelDirectories() throws Exception {
+        Set<Integer> actualLevels = new HashSet<Integer>();
+        for (InstallableBundle installableBundle : configuration.getBundlesToInstall()) {
+            actualLevels.add(installableBundle.getStartLevel());
+        }
+
+        assertThat(actualLevels.size(), is(3));
+        assertThat(actualLevels, hasItem(1));
+        assertThat(actualLevels, hasItem(2));
+        assertThat(actualLevels, hasItem(4));
+    }
+
+    @Test
+    public void ifProvidesNoBundlesIfThereAreNoBundleDirectories() throws Exception {
+        setTestRootDir("embedder_empty_test_root");
+        assertThat(configuration.getBundlesToInstall().isEmpty(), is(true));
+    }
+
+    @Test
+    public void itFindsAllTheBundles() throws Exception {
+        assertThat(configuration.getBundlesToInstall().size(), is(8));
+    }
+
+    @Test
+    public void itProvidesLocationsAsFileUrls() throws Exception {
+        InstallableBundle oneOfThem = configuration.getBundlesToInstall().iterator().next();
+        URI parsed = new URI(oneOfThem.getLocation());
+        assertThat(parsed.getScheme(), is("file"));
+    }
+
+    @Test
+    public void itIgnoresFilesThatAreNotJars() throws Exception {
+        Collection<String> allFilenames = collectLocationBasenames();
+        assertThat(allFilenames, not(hasItem("B-3.foo")));
+    }
+
+    @Test
+    public void itIgnoresJarsThatAreNotInLevelDirectories() throws Exception {
+        Collection<String> allFilenames = collectLocationBasenames();
+        assertThat(allFilenames, not(hasItem("Z.jar")));
+    }
+
+    @Test
+    public void itAppliesTheStartFlagIfInTheStartDirectory() throws Exception {
+        assertBundleExists(4, "P-1.jar", true);
+    }
+
+    @Test
+    public void itDoesNotApplyTheStartFlagIfInTheInstallDirectory() throws Exception {
+        assertBundleExists(4, "P-3.jar", false);
+    }
+
+    @Test
+    public void itHandlesLevelsThatOnlyHaveOneStateDirectory() throws Exception {
+        assertBundleExists(2, "B-2.jar", false);
+    }
+
+    private void assertBundleExists(int expectedLevel, String expectedBasename, boolean start) {
+        for (InstallableBundle bundle : configuration.getBundlesToInstall()) {
+            if (bundle.getShouldStart() && start ||
+                bundle.getStartLevel() == expectedLevel ||
+                locationBasename(bundle).equals(expectedBasename)
+            ) {
+                return;
+            }
+        }
+
+        fail("No bundle with " + expectedLevel + "; " + expectedBasename + "; " + start);
+    }
+
+    private Collection<String> collectLocationBasenames() {
+        List<String> names = new ArrayList<String>(configuration.getBundlesToInstall().size());
+        for (InstallableBundle bundle : configuration.getBundlesToInstall()) {
+            names.add(locationBasename(bundle));
+        }
+        return names;
+    }
+
+    private String locationBasename(InstallableBundle bundle) {
+        String loc = bundle.getLocation();
+        int slash = loc.lastIndexOf('/');
+        return loc.substring(slash + 1);
+    }
+
+    private void setTestRootDir(String rootDirName) throws FileNotFoundException {
+        File root = new File("src/test/java/" + rootDirName);
+        if (!root.exists()) {
+            root = new File("utility/" + root.getPath());
+            if (!root.exists()) {
+                throw new FileNotFoundException("Could not locate the test root " + root);
+            }
+        }
+
+        configuration.setRoot(root);
+    }
+}
