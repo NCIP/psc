ff96ce4578935efffc97482c7d1e2ec1444a1c82
Rhett Sutphin <r-sutphin@northwestern.edu>
2011-05-02 20:27:39 +0000

    Correct study-site-link attribute names and values. #1441.

--- core/src/main/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/StudySiteXmlSerializer.java
+++ core/src/main/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/StudySiteXmlSerializer.java
@@ -4,30 +4,33 @@ import edu.northwestern.bioinformatics.studycalendar.domain.Site;
 import edu.northwestern.bioinformatics.studycalendar.domain.Study;
 import edu.northwestern.bioinformatics.studycalendar.domain.StudySite;
 import edu.northwestern.bioinformatics.studycalendar.xml.AbstractStudyCalendarXmlSerializer;
-import static edu.northwestern.bioinformatics.studycalendar.xml.XsdAttribute.*;
 import edu.northwestern.bioinformatics.studycalendar.xml.XsdElement;
 import org.dom4j.Element;
+
+import static edu.northwestern.bioinformatics.studycalendar.xml.XsdAttribute.*;
 
 /**
  * @author John Dzak
  */
 public class StudySiteXmlSerializer extends AbstractStudyCalendarXmlSerializer<StudySite> {
-
+    @Override
     public Element createElement(StudySite studySite) {
         Element elt = XsdElement.STUDY_SITE_LINK.create();
-        STUDY_SITE_STUDY_NAME.addTo(elt, studySite.getStudy().getName());
-        STUDY_SITE_SITE_NAME.addTo(elt, studySite.getSite().getName());
+        STUDY_SITE_STUDY_IDENTIFIER.addTo(elt, studySite.getStudy().getName());
+        STUDY_SITE_SITE_IDENTIFIER.addTo(elt, studySite.getSite().getName());
         return elt;
     }
 
+    @Override
     public StudySite readElement(Element element) {
-        String studyName = element.attributeValue("study-name");
-        String siteName = element.attributeValue("site-name");
+        String studyIdent = STUDY_SITE_STUDY_IDENTIFIER.from(element);
+        String siteIdent = STUDY_SITE_SITE_IDENTIFIER.from(element);
             
         Study study = new Study();
-        study.setAssignedIdentifier(studyName);
+        study.setAssignedIdentifier(studyIdent);
         Site site = new Site();
-        site.setName(siteName);
+        site.setAssignedIdentifier(siteIdent);
+
         StudySite studySite = new StudySite();
         studySite.setStudy(study);
         studySite.setSite(site);
