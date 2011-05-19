7bb9b374bc5d5524c3d7602639dcd78981bd8c49
Rhett Sutphin <r-sutphin@northwestern.edu>
2011-05-19 20:13:04 +0000

    Port r6853: Add /system-version resource to echo the current version. Closes #1458.

---
+++ test/restful-api/src/spec/ruby/system_version_spec.rb
@@ -0,0 +1,16 @@
+describe '/system-version' do
+  it 'is not available when not authenticated' do
+    get '/system-version'
+    response.status_code.should == 401
+  end
+
+  it 'is available to any authenticated user' do
+    get '/system-version', :as => 'carla'
+    response.status_code.should == 200
+  end
+
+  it 'includes the version number in the entity' do
+    get '/system-version', :as => 'carla'
+    response.json['psc_version'].should == application_context['buildInfo'].versionNumber
+  end
+end
---
+++ web/src/test/java/edu/northwestern/bioinformatics/studycalendar/restlets/SystemVersionResourceTest.java
@@ -0,0 +1,42 @@
+package edu.northwestern.bioinformatics.studycalendar.restlets;
+
+import edu.northwestern.bioinformatics.studycalendar.security.authorization.PscRole;
+import gov.nih.nci.cabig.ctms.tools.BuildInfo;
+import org.json.JSONObject;
+import org.restlet.data.Method;
+
+/**
+ * @author Rhett Sutphin
+ */
+public class SystemVersionResourceTest extends AuthorizedResourceTestCase<SystemVersionResource> {
+    private BuildInfo buildInfo;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        buildInfo = new BuildInfo();
+        buildInfo.setVersionNumber("2.3.5.FOO");
+    }
+
+    @Override
+    protected SystemVersionResource createAuthorizedResource() {
+        SystemVersionResource r = new SystemVersionResource();
+        r.setBuildInfo(buildInfo);
+        return r;
+    }
+
+    public void testAuthorizedForAnyoneAuthenticated() throws Exception {
+        assertRolesAllowedForMethod(Method.GET, PscRole.values());
+    }
+
+    public void testIsForGetOnly() throws Exception {
+        assertAllowedMethods("GET");
+    }
+
+    public void testEntityContainsVersionNumber() throws Exception {
+        doGet();
+
+        JSONObject actual = new JSONObject(response.getEntity().getText());
+        assertEquals("2.3.5.FOO", actual.optString("psc_version"));
+    }
+}
