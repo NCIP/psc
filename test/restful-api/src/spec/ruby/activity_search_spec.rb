bdabb719a6ba4561c0ea7e1e7841af1d76137a98
John Dzak <j-dzak@northwestern.edu>
2011-08-08 15:27:01 +0000

    DRY up repetetive specs for the /activities and /activities/{source-name} resources. Part of #1363.

--- test/restful-api/src/spec/ruby/activity_search_spec.rb
+++ test/restful-api/src/spec/ruby/activity_search_spec.rb
@@ -5,300 +5,196 @@ START = [
   "Bone Scan", "Breast exam"
 ]
 
+module ActivitySearch
+  module HelperMethods
+    module Xml
+      def response_activity_count
+        response.xml_elements('//activity').size
+      end
+
+      def activity_names
+        @response_activity_names ||= response.xml_attributes("activity", "name")
+      end
+
+      def activity_types
+        response.xml_attributes("activity", "type")
+      end
+    end
+    module Json
+      def activity_names
+        response.json['activities'].collect{|a| a['activity_name']}
+      end
+
+      def activity_types
+        response.json['activities'].collect{|a| a['activity_type']}
+      end
+
+      def response_activity_count
+        activity_names.count
+      end
+    end
+  end
+
+
+  module Examples
+    class << self
+      def included(mod)
+        mod.class_eval do
+          def url
+            self.class.url
+          end
+          raise "A class method named url must be defined in order to include these examples" unless url
+
+
+          if url =~ /.*\.json$/
+            include ActivitySearch::HelperMethods::Json
+          else
+            include ActivitySearch::HelperMethods::Xml
+          end
+
+          def activities_with(s)
+            "#{url}?#{s}"
+          end
+
+          it "limits to single activity type activity type=" do
+            get activities_with('type=Intervention'), :as => :alice
+            response.status_code.should == 200
+            response_activity_count.should == 246
+            activity_types.compact.uniq.should have(1).kind
+          end
+
+          it "does not match partial activity types with type=" do
+            get activities_with('type=Oth'), :as => :alice
+            response.status_code.should == 400
+            response.entity.should include("Unknown activity type: Oth")
+          end
+
+          it "searches activity names from q=" do
+            get activities_with('q=CT%3A'), :as => :alice
+            response.status_code.should == 200
+            response_activity_count.should == 5
+            activity_names.should include("CT: Abdomen")
+            activity_names.should include("CT: Chest")
+            activity_names.should include("CT: head")
+            activity_names.should include("CT: Other")
+            activity_names.should include("CT: Pelvis")
+          end
+
+          it "searches activity codes from q=" do
+            get activities_with('q=788'), :as => :alice
+            response.status_code.should == 200
+            response_activity_count.should == 1
+            activity_names.first.should == "T3"
+          end
+
+          it "obeys type= and q= simultaneously" do
+            get activities_with('q=bone&type=Lab+Test'), :as => :alice
+            response.status_code.should == 200
+            response_activity_count.should == 3
+            activity_names.should include("Bone Marrow Biopsy")
+            activity_names.should include("Bone Marrow Cultures")
+            activity_names.should include("serum bone alkaline phosphatase")
+            activity_types.compact.uniq.should have(1).kind
+          end
+
+          it "searches single source activities with activity type=" do
+            get activities_with('type=Intervention'), :as => :alice
+            response.status_code.should == 200
+            response_activity_count.should == 246
+            activity_types.compact.uniq.should have(1).kind
+          end
+
+          it "limits the number of activities with limit=" do
+            get activities_with('limit=5'), :as => :alice
+            response.status_code.should == 200
+            response_activity_count.should == 5
+          end
+
+          it "offsets the activity list with offset=" do
+            get activities_with('offset=2'), :as => :alice
+            response.status_code.should == 200
+            activity_names.should_not include( START[0] )
+            activity_names.should_not include( START[1] )
+          end
+
+          it "limits and offsets the activity list with limit= & offset=" do
+            get activities_with('limit=50&offset=2'), :as => :alice
+            response.status_code.should == 200
+            response_activity_count.should == 50
+            activity_names.should_not include( START[0] )
+            activity_names.should_not include( START[1] )
+          end
+
+          it "sorts the activities by activity type with sort=" do
+            get activities_with('sort=activity_type&order=asc'), :as => :alice
+            response.status_code.should == 200
+            activity_types[0].should == "Disease Measure"
+          end
+
+          it "sorts the activities by activity name with sort=" do
+            get activities_with('sort=activity_name'), :as => :alice
+            response.status_code.should == 200
+            activity_names[0].should == "11-Deoxycortisol (Compound S)"
+          end
+
+          it "controls the sort order with order=" do
+            get activities_with('sort=activity_name&order=desc'), :as => :alice
+            response.status_code.should == 200
+            activity_names[0].should == "Zoladex"
+          end
+        end
+      end
+    end
+  end
+end
+
 describe "/activities" do
   describe "GET" do
+    def self.url
+      '/activities'
+    end
+    include ActivitySearch::Examples
+
     it "returns all sources without parameters" do
       get '/activities', :as => :alice
       response.status_code.should == 200
       response.xml_elements("//source").should have(1).elements
       response.xml_attributes("source", "name").should include("Northwestern University")
     end
-
-    def response_activity_count
-      response.xml_elements('//activity').size
-    end
-
-    def activity_names
-      @response_activity_names ||= response.xml_attributes("activity", "name")
-    end
-
-    def activity_types
-      response.xml_attributes("activity", "type")
-    end
-
-    it "limits to a single activity type with type=" do
-      get '/activities?type=Other', :as => :alice
-      response.status_code.should == 200
-      activity_types.uniq.should have(1).kind
-    end
-
-    it "does not match partial activity types with type=" do
-      get '/activities?type=Oth', :as => :alice
-      response.status_code.should == 400
-      response.entity.should include("Unknown activity type: Oth")
-    end
-
-    it "searches activity names from q=" do
-      get '/activities?q=CT%3A', :as => :alice
-      response.status_code.should == 200
-      response_activity_count.should == 5
-      activity_names.should include("CT: Abdomen")
-      activity_names.should include("CT: Chest")
-      activity_names.should include("CT: head")
-      activity_names.should include("CT: Other")
-      activity_names.should include("CT: Pelvis")
-    end
-
-    it "searches activity codes from q=" do
-      get '/activities?q=788', :as => :alice
-      response.status_code.should == 200
-      response_activity_count.should == 1
-      activity_names.first.should == "T3"
-    end
-
-    it "limits the number of activities with limit=" do
-      get '/activities?limit=5', :as => :alice
-      response.status_code.should == 200
-      response_activity_count.should == 5
-    end
-
-    it "offsets the activity list with offset=" do
-      get '/activities?offset=2', :as => :alice
-      response.status_code.should == 200
-      activity_names.should_not include( START[0] )
-      activity_names.should_not include( START[1] )
-    end
-
-    it "sorts the activities by activity type with sort=" do
-      get '/activities?sort=activity_type&order=asc', :as => :alice
-      response.status_code.should == 200
-      activity_types[0].should == "Disease Measure"
-    end
-
-    it "controls the sort order with order=" do
-      get '/activities?sort=activity_name&order=desc', :as => :alice
-      response.status_code.should == 200
-      activity_names[0].should == "Zoladex"
-    end
   end
 end
 
 describe "/activities.json" do
-  def activity_names
-    response.json['activities'].collect{|a| a['activity_name']}
-  end
-
-  def activity_types
-    response.json['activities'].collect{|a| a['activity_type']}
-  end
-
-  def response_activity_count
-    activity_names.count
-  end
-
   describe "GET" do
-    it "limits to a single activity type with type=" do
-      get '/activities.json?type=Other', :as => :alice
-      response.status_code.should == 200
-      activity_types.compact.uniq.should have(1).kind
+    def self.url
+      '/activities.json'
     end
-
-    it "does not match partial activity types with type=" do
-      get '/activities.json?type=Oth', :as => :alice
-      response.status_code.should == 400
-      response.entity.should include("Unknown activity type: Oth")
-    end
-
-    it "searches activity names from q=" do
-      get '/activities.json?q=CT%3A', :as => :alice
-      response.status_code.should == 200
-      response_activity_count.should == 5
-      activity_names.should include("CT: Abdomen")
-      activity_names.should include("CT: Chest")
-      activity_names.should include("CT: head")
-      activity_names.should include("CT: Other")
-      activity_names.should include("CT: Pelvis")
-    end
+    include ActivitySearch::Examples
 
     it "returns all activities" do
       get '/activities.json', :as => :alice
       response.status_code.should == 200
       response_activity_count.should == 1042
     end
-
-    it "limits the number of activities with limit=" do
-      get '/activities.json?limit=5', :as => :alice
-      response.status_code.should == 200
-      response_activity_count.should == 5
-      activity_names.sort.should == START
-    end
-
-    it "offsets the activity list with offset=" do
-      get '/activities.json?offset=2', :as => :alice
-      response.status_code.should == 200
-      activity_names.should_not include( START[0] )
-      activity_names.should_not include( START[1] )
-    end
-
-    it "limits and offsets the activity list with limit= & offset=" do
-      get '/activities.json?limit=50&offset=2', :as => :alice
-      response.status_code.should == 200
-      response_activity_count.should == 50
-      activity_names.should_not include( START[0] )
-      activity_names.should_not include( START[1] )
-    end
-
-    it "searches activity codes from q=" do
-      get '/activities.json?q=788', :as => :alice
-      response.status_code.should == 200
-      response_activity_count.should == 1
-      activity_names.first.should == "T3"
-    end
-
-    it "sorts the activities by activity type with sort=" do
-      get '/activities.json?sort=activity_type&order=asc', :as => :alice
-      response.status_code.should == 200
-      activity_types[0].should == "Disease Measure"
-    end
-
-    it "controls the sort order with order=" do
-      get '/activities.json?sort=activity_name&order=desc', :as => :alice
-      response.status_code.should == 200
-      activity_names[0].should == "Zoladex"
-    end
   end
 end
 
 describe "/activities/{activity-source-name}" do
   describe "GET" do
-    def response_activity_count
-      response.xml_elements('//activity').size
+    def self.url
+      '/activities/Northwestern%20University'
     end
-
-    def activity_names
-      @response_activity_names ||= response.xml_attributes("activity", "name")
-    end
-
-    def activity_types
-      response.xml_attributes("activity", "type")
-    end
-
-    it "obeys type= and q= simultaneously" do
-      get '/activities?q=bone&type=Lab+Test', :as => :alice
-      response.status_code.should == 200
-      response_activity_count.should == 3
-      activity_names.should include("Bone Marrow Biopsy")
-      activity_names.should include("Bone Marrow Cultures")
-      activity_names.should include("serum bone alkaline phosphatase")
-    end
-
-    it "searches single source activities with activity type=" do
-      get'/activities/Northwestern%20University?type=Intervention', :as => :alice
-      response.status_code.should == 200
-      response_activity_count.should == 246
-      response.xml_attributes("activity", "type").uniq.should have(1).kind
-    end
-
-    it "searches single source activities with q= and type=" do
-      get'/activities/Northwestern%20University?q=HLA&type=Lab+Test',:as => :alice
-      response.status_code.should == 200
-      response_activity_count.should == 3
-      response.xml_attributes("activity","type").uniq.should have(1).kind
-    end
-
-    it "searches single source activities and limits results with limit=" do
-      get'/activities/Northwestern%20University?limit=10', :as => :alice
-      response.status_code.should == 200
-      response_activity_count.should == 10
-    end
-
-    it "offsets the activity list with offset=" do
-      get '/activities/Northwestern%20University?offset=2', :as => :alice
-      response.status_code.should == 200
-      activity_names.should_not include( START[0] )
-      activity_names.should_not include( START[1] )
-    end
-
-    it "sorts the activities by activity type with sort=" do
-      get '/activities?sort=activity_type&order=asc', :as => :alice
-      response.status_code.should == 200
-      activity_types[0].should == "Disease Measure"
-    end
-
-    it "controls the sort order with order=" do
-      get '/activities?sort=activity_name&order=desc', :as => :alice
-      response.status_code.should == 200
-      activity_names[0].should == "Zoladex"
-    end
+    include ActivitySearch::Examples
   end
 end
 
 describe "/activities/{activity-source-name}.json" do
   describe "GET" do
-    def activity_names
-      response.json['activities'].collect{|a| a['activity_name']}
+    def self.url
+      '/activities/Northwestern%20University'
     end
-
-    def response_activity_count
-      activity_names.count
-    end
-
-    def activity_types
-      response.json['activities'].collect{|a| a['activity_type']}
-    end
-
-    it "obeys type= and q= simultaneously" do
-      get '/activities.json?q=bone&type=Lab+Test', :as => :alice
-      response.status_code.should == 200
-      response_activity_count.should == 3
-      activity_names.should include("Bone Marrow Biopsy")
-      activity_names.should include("Bone Marrow Cultures")
-      activity_names.should include("serum bone alkaline phosphatase")
-    end
-
-    it "searches single source activities with activity type=" do
-      get'/activities/Northwestern%20University.json?type=Intervention', :as => :alice
-      response.status_code.should == 200
-      response_activity_count.should == 246
-      activity_types.compact.uniq.should have(1).kind
-    end
-
-    it "searches single source activities with q= and type=" do
-      get'/activities/Northwestern%20University.json?q=HLA&type=Lab+Test',:as => :alice
-      response.status_code.should == 200
-      response_activity_count.should == 3
-      activity_types.compact.uniq.should have(1).kind
-    end
-
-    it "searches single source activities and limits results with limit=" do
-      get'/activities/Northwestern%20University.json?limit=10', :as => :alice
-      response.status_code.should == 200
-      response_activity_count.should == 10
-    end
-
-    it "offsets the activity list with offset=" do
-      get '/activities/Northwestern%20University.json?offset=2', :as => :alice
-      response.status_code.should == 200
-      activity_names.should_not include( START[0] )
-      activity_names.should_not include( START[1] )
-    end
-
-    it "sorts the activities by activity name with sort=" do
-      get '/activities/Northwestern%20University.json?sort=activity_name', :as => :alice
-      response.status_code.should == 200
-      activity_names[0].should == "11-Deoxycortisol (Compound S)"
-    end
-
-    it "sorts the activities by activity type with sort=" do
-      get '/activities/Northwestern%20University.json?sort=activity_type&order=asc', :as => :alice
-      response.status_code.should == 200
-      activity_types[0].should == "Disease Measure"
-    end
-
-    it "controls the sort order with order=" do
-      get '/activities/Northwestern%20University.json?sort=activity_name&order=desc', :as => :alice
-      response.status_code.should == 200
-      activity_names[0].should == "Zoladex"
-    end
+    include ActivitySearch::Examples
   end
 end
+
+
