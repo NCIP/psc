c46751d093cff08fb543da73b20f61f31b4ce01e
Rhett Sutphin <r-sutphin@northwestern.edu>
2011-09-26 06:32:15 +0000

    Whitespace. Done with #1568.

--- core/src/main/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/PlannedActivityLabelXmlSerializer.java
+++ core/src/main/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/PlannedActivityLabelXmlSerializer.java
@@ -1,23 +1,26 @@
 package edu.northwestern.bioinformatics.studycalendar.xml.writers;
 
-import edu.northwestern.bioinformatics.studycalendar.xml.AbstractStudyCalendarXmlSerializer;
-import edu.northwestern.bioinformatics.studycalendar.xml.XsdElement;
-import edu.northwestern.bioinformatics.studycalendar.xml.XsdAttribute;
 import edu.northwestern.bioinformatics.studycalendar.domain.PlannedActivityLabel;
-import org.dom4j.Element;
+import edu.northwestern.bioinformatics.studycalendar.xml.AbstractStudyCalendarXmlSerializer;
+import edu.northwestern.bioinformatics.studycalendar.xml.XsdAttribute;
+import edu.northwestern.bioinformatics.studycalendar.xml.XsdElement;
 import org.apache.commons.lang.StringUtils;
+import org.dom4j.Element;
 
 /**
  * @author Jalpa Patel
  */
-public class PlannedActivityLabelXmlSerializer  extends AbstractStudyCalendarXmlSerializer<PlannedActivityLabel> {
+public class PlannedActivityLabelXmlSerializer
+    extends AbstractStudyCalendarXmlSerializer<PlannedActivityLabel>
+{
 
     @Override
     public Element createElement(PlannedActivityLabel paLabel) {
         Element paLabelElt = XsdElement.PLANNED_ACTIVITY_LABEL.create();
-        XsdAttribute.LABEL_NAME.addTo(paLabelElt,paLabel.getLabel());
-        if(paLabel.getRepetitionNumber()!=null)
-        XsdAttribute.LABEL_REP_NUM.addTo(paLabelElt,paLabel.getRepetitionNumber());
+        XsdAttribute.LABEL_NAME.addTo(paLabelElt, paLabel.getLabel());
+        if (paLabel.getRepetitionNumber() != null) {
+            XsdAttribute.LABEL_REP_NUM.addTo(paLabelElt, paLabel.getRepetitionNumber());
+        }
         return paLabelElt;
     }
 
@@ -25,12 +28,13 @@ public class PlannedActivityLabelXmlSerializer  extends AbstractStudyCalendarXml
     public PlannedActivityLabel readElement(Element element) {
         PlannedActivityLabel label = new PlannedActivityLabel();
         label.setLabel(XsdAttribute.LABEL_NAME.from(element));
-        if(XsdAttribute.LABEL_REP_NUM.from(element)!=null)
-        label.setRepetitionNumber(Integer.parseInt(XsdAttribute.LABEL_REP_NUM.from(element)));
+        if (XsdAttribute.LABEL_REP_NUM.from(element) != null) {
+            label.setRepetitionNumber(Integer.parseInt(XsdAttribute.LABEL_REP_NUM.from(element)));
+        }
         return label;
-     }
+    }
 
-     public boolean validateElement(PlannedActivityLabel paLabel, Element element) {
+    public boolean validateElement(PlannedActivityLabel paLabel, Element element) {
         boolean valid = true;
         if (element == null && paLabel == null) {
             return true;
@@ -38,7 +42,7 @@ public class PlannedActivityLabelXmlSerializer  extends AbstractStudyCalendarXml
             return false;
         } else if (!StringUtils.equals(paLabel.getLabel(), XsdAttribute.LABEL_NAME.from(element))) {
             valid = false;
-        }  else if (!StringUtils.equals(String.valueOf(paLabel.getRepetitionNumber()), XsdAttribute.LABEL_REP_NUM.from(element))) {
+        } else if (!StringUtils.equals(String.valueOf(paLabel.getRepetitionNumber()), XsdAttribute.LABEL_REP_NUM.from(element))) {
             valid = false;
         }
         return valid;
