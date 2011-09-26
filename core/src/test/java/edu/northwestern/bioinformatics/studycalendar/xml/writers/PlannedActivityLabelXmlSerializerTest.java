b6be74c35b847b47e0de21fddafb69c830f03bc1
Rhett Sutphin <r-sutphin@northwestern.edu>
2011-09-26 21:48:47 +0000

    Port r6994: Whitespace. Done with #1568.

--- core/src/test/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/PlannedActivityLabelXmlSerializerTest.java
+++ core/src/test/java/edu/northwestern/bioinformatics/studycalendar/xml/writers/PlannedActivityLabelXmlSerializerTest.java
@@ -1,50 +1,48 @@
 package edu.northwestern.bioinformatics.studycalendar.xml.writers;
 
+import edu.northwestern.bioinformatics.studycalendar.core.Fixtures;
 import edu.northwestern.bioinformatics.studycalendar.core.StudyCalendarXmlTestCase;
 import edu.northwestern.bioinformatics.studycalendar.domain.PlannedActivityLabel;
-import edu.northwestern.bioinformatics.studycalendar.core.Fixtures;
 import edu.northwestern.bioinformatics.studycalendar.xml.XsdElement;
-import static edu.northwestern.bioinformatics.studycalendar.xml.XsdAttribute.*;
 import org.dom4j.Element;
+
+import static edu.northwestern.bioinformatics.studycalendar.xml.XsdAttribute.*;
 
 /**
  * @author Jalpa Patel
  */
-public class PlannedActivityLabelXmlSerializerTest  extends StudyCalendarXmlTestCase {
+public class PlannedActivityLabelXmlSerializerTest extends StudyCalendarXmlTestCase {
     private PlannedActivityLabel paLabel;
     private PlannedActivityLabelXmlSerializer plannedActivityLabelXmlSerializer;
 
     @Override
     protected void setUp() throws Exception {
-           super.setUp();
-           paLabel =  Fixtures.createPlannedActivityLabel("testlabel",2);
-           plannedActivityLabelXmlSerializer = new PlannedActivityLabelXmlSerializer();
-       }
+        super.setUp();
+        paLabel = Fixtures.createPlannedActivityLabel("testlabel", 2);
+        plannedActivityLabelXmlSerializer = new PlannedActivityLabelXmlSerializer();
+    }
 
     public void testValidateElement() throws Exception {
-        Element actual =  plannedActivityLabelXmlSerializer.createElement(paLabel);
-        assertFalse( plannedActivityLabelXmlSerializer.validateElement(null, actual));
+        Element actual = plannedActivityLabelXmlSerializer.createElement(paLabel);
+        assertFalse(plannedActivityLabelXmlSerializer.validateElement(null, actual));
 
-        PlannedActivityLabel paLabel2 = Fixtures.createPlannedActivityLabel("testlabel",2);
-        assertTrue( plannedActivityLabelXmlSerializer.validateElement(paLabel2, actual));
+        PlannedActivityLabel paLabel2 = Fixtures.createPlannedActivityLabel("testlabel", 2);
+        assertTrue(plannedActivityLabelXmlSerializer.validateElement(paLabel2, actual));
         paLabel2.setLabel("testlabel1");
-        assertFalse( plannedActivityLabelXmlSerializer.validateElement(paLabel2, actual));
+        assertFalse(plannedActivityLabelXmlSerializer.validateElement(paLabel2, actual));
 
-        PlannedActivityLabel paLabel1 = Fixtures.createPlannedActivityLabel("testlabel",2);
-        assertTrue( plannedActivityLabelXmlSerializer.validateElement(paLabel1, actual));
+        PlannedActivityLabel paLabel1 = Fixtures.createPlannedActivityLabel("testlabel", 2);
+        assertTrue(plannedActivityLabelXmlSerializer.validateElement(paLabel1, actual));
         paLabel1.setRepetitionNumber(3);
-        assertFalse( plannedActivityLabelXmlSerializer.validateElement(paLabel1, actual));
-      }
+        assertFalse(plannedActivityLabelXmlSerializer.validateElement(paLabel1, actual));
+    }
 
-     public void testCreateElement() throws Exception {
-        Element actual =   plannedActivityLabelXmlSerializer.createElement(paLabel);
+    public void testCreateElement() throws Exception {
+        Element actual = plannedActivityLabelXmlSerializer.createElement(paLabel);
         assertEquals("Wrong element name", XsdElement.PLANNED_ACTIVITY_LABEL.xmlName(), actual.getName());
         assertEquals("Should have no children", 0, actual.elements().size());
         assertEquals("Wrong Label", paLabel.getLabel(),
-                LABEL_NAME.from(actual));
-        assertEquals("Wrong Repetition Number", paLabel.getRepetitionNumber().toString(),LABEL_REP_NUM.from(actual));
-
+            LABEL_NAME.from(actual));
+        assertEquals("Wrong Repetition Number", paLabel.getRepetitionNumber().toString(), LABEL_REP_NUM.from(actual));
     }
-
-
 }
