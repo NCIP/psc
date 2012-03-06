6c2038ec568199cb26782c6a6c6f94ca0f88267a
Jalpa Patel <jalpa-patel@northwestern.edu>
2012-03-06 21:57:47 +0000

    Port r7115: Added tests for application context of the grid services. #1874.

---
+++ grid/registration-consumer/test/src/java/edu/northwestern/bioinformatics/studycalendar/grid/ApplicationContextInitializationTest.java
@@ -0,0 +1,15 @@
+package edu.northwestern.bioinformatics.studycalendar.grid;
+
+import junit.framework.TestCase;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Jalpa Patel
+ */
+public class ApplicationContextInitializationTest extends TestCase {
+    public void testApplicationContextLoads() throws Exception {
+        ApplicationContext loaded = new ClassPathXmlApplicationContext("applicationContext-grid.xml");
+        assertTrue("No beans loaded", loaded.getBeanDefinitionCount() > 0);
+    }
+}
